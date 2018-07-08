b226bba93b4669c8e09c8e9ac371fe93722a21db
Łukasz Sentkiewicz <sirmims@gmail.com>
2018-07-08 12:36:24 +0200

    wip
    Signed-off-by: Łukasz Sentkiewicz <sirmims@gmail.com>

--- src/groupBy.test.ts
+++ src/groupBy.test.ts
@@ -23,3 +23,9 @@ describe('data last', () => {
     ).toEqual(expected);
   });
 });
+
+declare module './groupBy' {
+  export interface groupBy {
+    indexed: () => string;
+  }
+}
--- src/groupBy.ts
+++ src/groupBy.ts
@@ -1,16 +1,37 @@
+import { purry } from './purry';
+
+/**
+ * Splits a collection into sets, grouped by the result of running each value through `fn`.
+ * @param items the items to group
+ * @param fn the grouping function
+ * @signature
+ *    R.groupBy(array, fn)
+ * @example
+ *    R.groupBy(['one', 'two', 'three'], x => x.length) => {3: ['one', 'two'], 5: ['three']}
+ * @data_first
+ * @category Array
+ */
 export function groupBy<T>(
   items: T[],
   fn: (item: T) => any
 ): Record<string, T[]>;
+
 export function groupBy<T>(
   fn: (item: T) => any
 ): (array: T[]) => Record<string, T[]>;
 
-export function groupBy(arg1: any, arg2?: any): any {
-  if (arguments.length === 1) {
-    return (data: any) => _groupBy(data, arg1);
-  }
-  return _groupBy(arg1, arg2);
+/**
+ * Splits a collection into sets, grouped by the result of running each value through `fn`.
+ * @param fn the grouping function
+ * @signature
+ *    R.groupBy(array, fn)
+ * @example
+ *    R.pipe(['one', 'two', 'three'], R.groupBy(x => x.length)) => {3: ['one', 'two'], 5: ['three']}
+ * @data_last
+ * @category Array
+ */
+export function groupBy() {
+  return purry(_groupBy, arguments);
 }
 
 function _groupBy<T>(array: T[], fn: (item: T) => any) {
@@ -23,4 +44,8 @@ function _groupBy<T>(array: T[], fn: (item: T) => any) {
     ret[key].push(item);
   });
   return ret;
+}
+
+declare module './groupBy' {
+  namespace groupBy { export var indexed: () => 1; }
 }
