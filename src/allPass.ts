cb4fae9d33f5aa9637918322528f74ab07800bea
Jan Andersson <jan.andersson@gmail.com>
2019-09-22 22:54:28 +0200

    Add allPass

---
+++ src/allPass.ts
@@ -0,0 +1,44 @@
+import { purry } from './purry';
+
+/**
+ * Determines whether all predicates returns true for the input data.
+ * @param data The input data for predicates.
+ * @param fns The list of predicates.
+ * @signature
+ *    R.allPass(data, fns)
+ * @example
+ *    const isDivisibleBy3 = (x: number) = x % 3 === 0
+ *    const isDivisibleBy4 = (x: number) = x % 4 === 0
+ *    const fns = [isDivisibleBy3, isDivisibleBy4]
+ *    R.allPass(12, fns) // => true
+ *    R.allPass(8, fns) // => false
+ * @data_first
+ * @category Array
+ */
+export function allPass<T>(data: T, fns: Array<(data: T) => boolean>): boolean;
+
+/**
+ * Determines whether all predicates returns true for the input data.
+ * @param fns The list of predicates.
+ * @signature
+ *    R.allPass(fns)(data)
+ * @example
+ *    const isDivisibleBy3 = (x: number) = x % 3 === 0
+ *    const isDivisibleBy4 = (x: number) = x % 4 === 0
+ *    const fns = [isDivisibleBy3, isDivisibleBy4]
+ *    R.allPass(fns)(12) // => true
+ *    R.allPass(fns)(8) // => false
+ * @data_last
+ * @category Array
+ */
+export function allPass<T>(
+  fns: Array<(data: T) => boolean>
+): (data: T) => boolean;
+
+export function allPass() {
+  return purry(_allPass, arguments);
+}
+
+function _allPass(data: any, fns: Array<(data: any) => boolean>) {
+  return fns.every(fn => fn(data));
+}
