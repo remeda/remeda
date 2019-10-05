fa6234019af93912ed9d6bde13203490131b8ea2
Jan Andersson <jan.andersson@gmail.com>
2019-10-05 11:44:33 +0200

    Add allPass (#24)

---
+++ src/allPass.test.ts
@@ -0,0 +1,19 @@
+import { allPass } from './allPass';
+
+const fns = [(x: number) => x % 3 === 0, (x: number) => x % 4 === 0];
+
+describe('data first', () => {
+  test('allPass', () => {
+    expect(allPass(12, fns)).toEqual(true);
+    expect(allPass(4, fns)).toEqual(false);
+    expect(allPass(3, fns)).toEqual(false);
+  });
+});
+
+describe('data last', () => {
+  test('allPass', () => {
+    expect(allPass(fns)(12)).toEqual(true);
+    expect(allPass(fns)(4)).toEqual(false);
+    expect(allPass(fns)(3)).toEqual(false);
+  });
+});
