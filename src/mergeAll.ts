307b2baab5f898fef9c3766a219d515895f30b2f
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2020-03-23 09:44:51 +0100

    add missing overload to mergeAll
    #43

--- src/mergeAll.ts
+++ src/mergeAll.ts
@@ -14,6 +14,7 @@ export function mergeAll<A, B, C, D>(array: [A, B, C, D]): A & B & C & D;
 export function mergeAll<A, B, C, D, E>(
   array: [A, B, C, D, E]
 ): A & B & C & D & E;
+export function mergeAll(array: object[]): object;
 
 export function mergeAll(items: any[]) {
   return items.reduce((acc, x) => ({ ...acc, ...x }), {});
