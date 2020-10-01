44bc854335d0290d43e63804a04127b001f3f2a8
Steev Sachs <steev.sachs@gmail.com>
2020-10-01 09:12:56 -0400

    [remeda/remeda#81] Do not return nonexistent source properties from `pick` * Upgraded jest and @types/jest to pick up `toStrictEqual`

--- src/pick.ts
+++ src/pick.ts
@@ -37,7 +37,9 @@ function _pick(object: any, names: string[]) {
     return {};
   }
   return names.reduce((acc, name) => {
-    acc[name] = object[name];
+    if (object.hasOwnProperty(name)) {
+      acc[name] = object[name];
+    }
     return acc;
   }, {} as any);
 }
