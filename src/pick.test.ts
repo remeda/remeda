cfecd4e0722d946b1d6cb520a8a49ea68a41f8fe
steevsachs <steev.sachs@gmail.com>
2020-11-03 04:03:55 -0500

    [remeda/remeda#81] Do not return nonexistent source properties from `pick` (#98)
    * Upgraded jest and @types/jest to pick up `toStrictEqual`

--- src/pick.test.ts
+++ src/pick.test.ts
@@ -5,7 +5,7 @@ import { concat } from './concat';
 describe('data first', () => {
   test('it should pick props', () => {
     const result = pick({ a: 1, b: 2, c: 3, d: 4 }, ['a', 'd']);
-    expect(result).toEqual({ a: 1, d: 4 });
+    expect(result).toStrictEqual({ a: 1, d: 4 });
   });
   test('allow undefined or null', () => {
     expect(pick(undefined as any, ['foo'])).toEqual({});
