92587cec969f9f5cabf07d260159343dd5ffcba8
Dominik Dorfmeister <office@dorfmeister.cc>
2021-06-15 18:00:05 +0200

    use our own flatMap because apparently, .flatMap doesn't exist

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -3,6 +3,7 @@ import marked from 'marked';
 import prettier from 'prettier';
 
 import data from '../out.json';
+import { flatMap } from '../src/flatMap';
 
 export interface MethodDoc {
   tag: string;
@@ -35,94 +36,89 @@ function getReturnType(signature) {
   return 'Object';
 }
 
-const ret = data.children
-  .flatMap((method: any) =>
-    method.children
-      ? method.children
-          .filter(
-            (item: any) =>
-              (item.kindString === 'Function' ||
-                item.kindString === 'Module') &&
-              item.signatures &&
-              item.flags.isExported
-          )
-          .map(target => {
-            if (!target) {
-              return;
-            }
-            console.log('processing', target.name);
-            const signatures = target.signatures.filter(s => s.comment);
-            if (!signatures.length) {
-              return null;
-            }
-            const comment = signatures[0].comment;
-            return {
-              name: target.name,
-              category: '',
-              description: marked(
-                (comment.shortText + '\n' + (comment.text || '')).trim(),
-                { breaks: true }
-              ),
-              methods: signatures.map(signature => {
-                const tags =
-                  signature.comment.tags || target.comment.tags || [];
-                const isDataFirst = tags.find(
-                  item => item.tag === 'data_first'
-                );
-                const isDataLast = tags.find(item => item.tag === 'data_last');
-                const getTag = name =>
-                  tags
-                    .filter(item => item.tag === name)
-                    .map(item => item.text.trim())
-                    .join('\n');
-                const hasTag = name => !!tags.find(item => item.tag === name);
+const ret = flatMap(data.children, (method: any) =>
+  method.children
+    ? method.children
+        .filter(
+          (item: any) =>
+            (item.kindString === 'Function' || item.kindString === 'Module') &&
+            item.signatures &&
+            item.flags.isExported
+        )
+        .map(target => {
+          if (!target) {
+            return;
+          }
+          console.log('processing', target.name);
+          const signatures = target.signatures.filter(s => s.comment);
+          if (!signatures.length) {
+            return null;
+          }
+          const comment = signatures[0].comment;
+          return {
+            name: target.name,
+            category: '',
+            description: marked(
+              (comment.shortText + '\n' + (comment.text || '')).trim(),
+              { breaks: true }
+            ),
+            methods: signatures.map(signature => {
+              const tags = signature.comment.tags || target.comment.tags || [];
+              const isDataFirst = tags.find(item => item.tag === 'data_first');
+              const isDataLast = tags.find(item => item.tag === 'data_last');
+              const getTag = name =>
+                tags
+                  .filter(item => item.tag === name)
+                  .map(item => item.text.trim())
+                  .join('\n');
+              const hasTag = name => !!tags.find(item => item.tag === name);
 
-                function getExample() {
-                  let str = getTag('example');
-                  if (str) {
-                    return prettier.format(str, {
-                      semi: false,
-                      singleQuote: true,
-                      parser: 'typescript',
-                    });
-                  }
-                  str = getTag('example-raw');
-                  return str
-                    .split('\n')
-                    .map(str => str.replace(/^   /, ''))
-                    .join('\n');
-                }
-
-                const parameters = signature.parameters || [];
-                return {
-                  tag: isDataFirst
-                    ? 'Data First'
-                    : isDataLast
-                    ? 'Data Last'
-                    : null,
-                  signature: prettier.format(getTag('signature'), {
+              function getExample() {
+                let str = getTag('example');
+                if (str) {
+                  return prettier.format(str, {
                     semi: false,
                     singleQuote: true,
                     parser: 'typescript',
-                  }),
-                  category: getTag('category'),
-                  indexed: hasTag('indexed'),
-                  pipeable: hasTag('pipeable'),
-                  example: getExample(),
-                  args: parameters.map((item: any) => ({
-                    name: item.name,
-                    description: item.comment && item.comment.text,
-                  })),
-                  returns: {
-                    name: getReturnType(signature),
-                    description: getTag('returns'),
-                  },
-                };
-              }),
-            };
-          })
-      : []
-  )
+                  });
+                }
+                str = getTag('example-raw');
+                return str
+                  .split('\n')
+                  .map(str => str.replace(/^   /, ''))
+                  .join('\n');
+              }
+
+              const parameters = signature.parameters || [];
+              return {
+                tag: isDataFirst
+                  ? 'Data First'
+                  : isDataLast
+                  ? 'Data Last'
+                  : null,
+                signature: prettier.format(getTag('signature'), {
+                  semi: false,
+                  singleQuote: true,
+                  parser: 'typescript',
+                }),
+                category: getTag('category'),
+                indexed: hasTag('indexed'),
+                pipeable: hasTag('pipeable'),
+                example: getExample(),
+                args: parameters.map((item: any) => ({
+                  name: item.name,
+                  description: item.comment && item.comment.text,
+                })),
+                returns: {
+                  name: getReturnType(signature),
+                  description: getTag('returns'),
+                },
+              };
+            }),
+          };
+        })
+    : []
+)
   .filter(item => item)
   .map(item => {
     item.category = item.methods[0].category;
