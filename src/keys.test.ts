034ef64c2aa9561f2b621091fff3b258e4b2b986
Ren <44821537+rarenatoe@users.noreply.github.com>
2021-07-28 09:44:09 -0500

    feat: values and keys functions (#137)

---
+++ src/keys.test.ts
@@ -0,0 +1,11 @@
+import { keys } from './keys';
+
+describe('Test for keys', () => {
+  it('should return keys of array', () => {
+    expect(keys(['x', 'y', 'z'])).toEqual(['0', '1', '2']);
+  });
+
+  it('should return keys of object', () => {
+    expect(keys({ a: 'x', b: 'y', c: 'z' })).toEqual(['a', 'b', 'c']);
+  });
+});
