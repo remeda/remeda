59a371667a455c6e7ee2cb4f67359f6b08b7b372
Dominik Dorfmeister <office@dorfmeister.cc>
2022-05-20 09:54:15 +0200

    feat(last): improve types (#171)
    if a verified non-empty array is passed as input, the result value cannot be potentially undefined

--- src/last.test.ts
+++ src/last.test.ts
@@ -1,9 +1,19 @@
 import { last } from './last';
+import { AssertEqual, NonEmptyArray } from 'src/_types';
 
-test('should return last', () => {
-  expect(last([1, 2, 3] as const)).toEqual(3);
-});
+describe('last', () => {
+  test('should return last', () => {
+    expect(last([1, 2, 3] as const)).toEqual(3);
+  });
 
-test('empty array', () => {
-  expect(last([] as const)).toEqual(undefined);
+  test('empty array', () => {
+    expect(last([] as const)).toEqual(undefined);
+  });
+
+  test('should not return undefined for non empty arrays', () => {
+    const input: NonEmptyArray<number> = [1, 2, 3];
+    const data = last(input);
+    const result: AssertEqual<typeof data, number> = true;
+    expect(result).toEqual(true);
+  });
 });
