f454062f36f54376705135663ff4db7d1141ccf4
Dominik Dorfmeister <office@dorfmeister.cc>
2022-05-25 15:25:56 +0200

    Feature/173 last pipe (#174)
    * chore: update typescript dev dependency
    
    * fix: make last work in pipes
    
    just like first works; however, you need to invoke it with `last()` in pipes

--- src/last.test.ts
+++ src/last.test.ts
@@ -1,19 +1,47 @@
 import { last } from './last';
+import { pipe } from './pipe';
 import { AssertEqual, NonEmptyArray } from './_types';
 
 describe('last', () => {
-  test('should return last', () => {
-    expect(last([1, 2, 3] as const)).toEqual(3);
+  describe('data first', () => {
+    test('should return last', () => {
+      expect(last([1, 2, 3] as const)).toEqual(3);
+    });
+
+    test('empty array', () => {
+      expect(last([] as const)).toEqual(undefined);
+    });
   });
 
-  test('empty array', () => {
-    expect(last([] as const)).toEqual(undefined);
+  describe('data last', () => {
+    test('should return last', () => {
+      expect(last([1, 2, 3] as const)).toEqual(3);
+    });
+
+    test('empty array', () => {
+      expect(last([] as const)).toEqual(undefined);
+    });
   });
 
-  test('should not return undefined for non empty arrays', () => {
-    const input: NonEmptyArray<number> = [1, 2, 3];
-    const data = last(input);
-    const result: AssertEqual<typeof data, number> = true;
-    expect(result).toEqual(true);
+  describe('types', () => {
+    test('should return T | undefined for Array input', () => {
+      const input: Array<number> = [1, 2, 3];
+      const actual = last(input);
+      const result: AssertEqual<typeof actual, number | undefined> = true;
+      expect(result).toEqual(true);
+    });
+
+    test('should not return undefined for non empty arrays', () => {
+      const input: NonEmptyArray<number> = [1, 2, 3];
+      const data = last(input);
+      const result: AssertEqual<typeof data, number> = true;
+      expect(result).toEqual(true);
+    });
+
+    test('should infer type in pipes', () => {
+      const data = pipe('this is a text', data => data.split(''), last());
+      const result: AssertEqual<typeof data, string | undefined> = true;
+      expect(result).toEqual(true);
+    });
   });
 });
--- src/last.ts
+++ src/last.ts
@@ -1,17 +1,47 @@
 import { NonEmptyArray } from './_types';
+import { purry } from './purry';
 
 /**
  * Gets the last element of `array`.
+ * Note: In `pipe`, use `last()` form instead of `last`. Otherwise, the inferred type is lost.
  * @param array the array
  * @signature
  *    R.last(array)
  * @example
  *    R.last([1, 2, 3]) // => 3
  *    R.last([]) // => undefined
+ *    R.pipe(
+ *      [1, 2, 4, 8, 16],
+ *      R.filter(x => x > 3),
+ *      R.last(),
+ *      x => x + 1
+ *    ); // => 17
+ *
  * @category Array
+ * @pipeable
  */
 export function last<T>(array: NonEmptyArray<T>): T;
 export function last<T>(array: ReadonlyArray<T>): T | undefined;
-export function last(array: ReadonlyArray<unknown> | NonEmptyArray<unknown>) {
+export function last<T>(): (array: ReadonlyArray<T>) => T | undefined;
+export function last() {
+  return purry(_last, arguments, last.lazy);
+}
+
+function _last<T>(array: T[]) {
   return array[array.length - 1];
+}
+
+export namespace last {
+  export function lazy<T>() {
+    return (value: T) => {
+      return {
+        done: true,
+        hasNext: true,
+        next: value,
+      };
+    };
+  }
+  export namespace lazy {
+    export const single = true;
+  }
 }
