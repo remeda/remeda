31539a1ed3cb76872c0f3195c42ed10cb1bb7230
Dominik Dorfmeister <office@dorfmeister.cc>
2022-05-25 09:04:49 +0200

    fix: make last work in pipes
    just like first works; however, you need to invoke it with `last()` in pipes

--- src/reverse.test.ts
+++ src/reverse.test.ts
@@ -25,7 +25,10 @@ describe('data first', () => {
     test('variadic tuples', () => {
       const input: [number, ...Array<string>] = [1, 'two', 'three'];
       const actual = reverse(input);
-      const result: AssertEqual<typeof actual, (string | number)[]> = true;
+      const result: AssertEqual<
+        typeof actual,
+        [...Array<string>, number]
+      > = true;
       expect(result).toEqual(true);
     });
   });
@@ -54,7 +57,10 @@ describe('data last', () => {
     test('variadic tuples', () => {
       const input: [number, ...Array<string>] = [1, 'two', 'three'];
       const actual = pipe(input, reverse());
-      const result: AssertEqual<typeof actual, (string | number)[]> = true;
+      const result: AssertEqual<
+        typeof actual,
+        [...Array<string>, number]
+      > = true;
       expect(result).toEqual(true);
     });
   });
