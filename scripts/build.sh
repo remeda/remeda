1329fce8fb4ad8ed756f24a5e3732510f042e806
Dominik Dorfmeister <office@dorfmeister.cc>
2022-07-28 15:36:19 +0200

    chore: replace build.sh with npm script
    so that codesandbox can pick it up

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-rm -rf ./dist
-mkdir dist
-yarn run tsc --declaration true --declarationMap true --module esnext --outDir "./dist/es"
-yarn run tsc --declaration true --declarationMap true --module commonjs --outDir "./dist/commonjs"
