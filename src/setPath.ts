aa6cfacdc2d5bd22b367b6b681aa2d9809f5d103
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-11-06 13:59:54 -0500

    feat: add string path support to setPath

--- src/setPath.ts
+++ src/setPath.ts
@@ -1,6 +1,13 @@
 import { F } from 'ts-toolbelt';
 import { purry } from './purry';
-import { Path, SupportsValueAtPath, ValueAtPath } from './_paths';
+import {
+  Path,
+  PathString,
+  StringToPath,
+  stringToPathArray,
+  SupportsValueAtPath,
+  ValueAtPath,
+} from './_paths';
 import { Key } from './_types';
 
 export function setPath<T, TPath extends Key[] & Path<T>>(
@@ -9,22 +16,45 @@ export function setPath<T, TPath extends Key[] & Path<T>>(
   value: ValueAtPath<T, TPath>
 ): T;
 
+export function setPath<T, TPath extends PathString<T>>(
+  object: T,
+  path: TPath,
+  value: ValueAtPath<T, StringToPath<TPath>>
+): T;
+
 export function setPath<TPath extends Key[], Value>(
   path: F.Narrow<TPath>,
   value: Value
 ): <Obj>(object: SupportsValueAtPath<Obj, TPath, Value>) => Obj;
 
+export function setPath<TPath extends string, Value>(
+  path: F.Narrow<TPath>,
+  value: Value
+): <Obj>(object: SupportsValueAtPath<Obj, StringToPath<TPath>, Value>) => Obj;
+
 export function setPath() {
   return purry(_setPath, arguments);
 }
 
-export function _setPath(object: any, path: any[], defaultValue: any): any {
+function _setPath(object: any, path: any[] | string, defaultValue: any) {
+  return _setPathNormalized(
+    object,
+    Array.isArray(path) ? path : stringToPathArray(path),
+    defaultValue
+  );
+}
+
+function _setPathNormalized(
+  object: any,
+  path: any[] | string,
+  defaultValue: any
+): any {
   if (path.length === 0) return defaultValue;
 
   if (Array.isArray(object)) {
     return object.map((item, index) => {
       if (index === path[0]) {
-        return _setPath(item, path.slice(1), defaultValue);
+        return _setPathNormalized(item, path.slice(1), defaultValue);
       }
       return item;
     });
@@ -32,6 +62,6 @@ export function _setPath(object: any, path: any[], defaultValue: any): any {
 
   return {
     ...object,
-    [path[0]]: _setPath(object[path[0]], path.slice(1), defaultValue),
+    [path[0]]: _setPathNormalized(object[path[0]], path.slice(1), defaultValue),
   };
 }
