7641ab1a07c8ab03ea3f780c75ff58985a047cf0
Santi Albo <santialbo@gmail.com>
2022-11-12 08:58:31 +0100

    feat: add overload to partition with type guard support (#221)

--- src/partition.test.ts
+++ src/partition.test.ts
@@ -1,5 +1,6 @@
 import { partition } from './partition';
 import { pipe } from './pipe';
+import { AssertEqual } from './_types';
 
 const array = [
   { a: 1, b: 1 },
@@ -19,6 +20,18 @@ const expected = [
 describe('data first', () => {
   test('partition', () => {
     expect(partition(array, x => x.a === 1)).toEqual(expected);
+  });
+  test('partition with type guard', () => {
+    const isNumber = function (value: any): value is number {
+      return typeof value === "number"
+    };
+    const actual = partition([1, "a", 2, "b"], isNumber);
+    expect(actual).toEqual([[1, 2], ["a", "b"]]);
+    const result: AssertEqual<
+      typeof actual,
+      [number[], string[]]
+    > = true;
+    expect(result).toBe(true);
   });
   test('partition.indexed', () => {
     expect(partition.indexed(array, (_, index) => index !== 2)).toEqual(
