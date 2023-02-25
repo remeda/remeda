d525f41e33bab143fdf5c4c7781c1ed1c9ef4ae4
Eran Hirsch <eranhirsch@gmail.com>
2023-02-25 16:45:13 +0200

    strict toPairs

--- src/toPairs.ts
+++ src/toPairs.ts
@@ -3,10 +3,37 @@
  * @param object
  * @signature
  *    R.toPairs(object)
+ *    R.toPairs.strict(object)
  * @example
  *    R.toPairs({ a: 1, b: 2, c: 3 }) // => [['a', 1], ['b', 2], ['c', 3]]
+ *    R.toPairs.strict({ a: 1 } as { a?: number }) // => [['a', 1]] typed Array<['a', number]>
+ * @strict
  * @category Object
  */
 export function toPairs<T>(object: { [s: string]: T }): Array<[string, T]> {
   return Object.entries(object);
+}
+
+// Inspired and largely copied from `sindresorhus/ts-extras`:
+// @see https://github.com/sindresorhus/ts-extras/blob/44f57392c5f027268330771996c4fdf9260b22d6/source/object-keys.ts
+// @see https://github.com/sindresorhus/ts-extras/blob/44f57392c5f027268330771996c4fdf9260b22d6/source/object-entries.ts
+type ObjectKeys<T extends object> = `${Exclude<keyof T, symbol>}`;
+type ObjectValues<T extends Record<PropertyKey, unknown>> =
+  Required<T>[ObjectKeys<T>];
+type ObjectEntry<T extends Record<PropertyKey, unknown>> = [
+  ObjectKeys<T>,
+  ObjectValues<T>
+];
+type ObjectEntries<T extends Record<PropertyKey, unknown>> = Array<
+  ObjectEntry<T>
+>;
+
+export namespace toPairs {
+  export function strict<T extends Record<PropertyKey, unknown>>(
+    object: T
+  ): ObjectEntries<T> {
+    // @ts-expect-error [ts2322] - This is deliberately stricter than what TS
+    // provides out of the box.
+    return Object.entries(object);
+  }
 }
