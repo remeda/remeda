7146a5aa13781427f5a1e04e56ac683205fcc04a
Eran Hirsch <eranhirsch@gmail.com>
2023-03-10 10:13:02 +0200

    remove defaults

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,5 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  semi: true,
-  useTabs: false,
-  printWidth: 80,
-  tabWidth: 2,
   singleQuote: true,
-  trailingComma: 'es5',
-  jsxBracketSameLine: false,
   arrowParens: 'avoid',
 };
