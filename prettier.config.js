79880ac9e20969d854e64d79316c604606b8d1c2
Eran Hirsch <eranhirsch@gmail.com>
2023-03-10 10:07:50 +0200

    remove comments

--- prettier.config.js
+++ prettier.config.js
@@ -1,24 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  // write: true,
   semi: true,
-  useTabs: false, // Indent lines with tabs instead of spaces.
-  printWidth: 80, // Specify the length of line that the printer will wrap on.
-  tabWidth: 2, // Specify the number of spaces per indentation-level.
-  singleQuote: true, // Use single quotes instead of double quotes.
-  /**
-   * Print trailing commas wherever possible.
-   * Valid options:
-   *   - "none" - no trailing commas
-   *   - "es5" - trailing commas where valid in ES5 (objects, arrays, etc)
-   *   - "all" - trailing commas wherever possible (function arguments)
-   */
+  useTabs: false,
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
   trailingComma: 'es5',
-  /**
-   * Do not print spaces between brackets.
-   * If true, puts the > of a multi-line jsx element at the end of the last line instead of being
-   * alone on the next line
-   */
   jsxBracketSameLine: false,
   arrowParens: 'avoid',
 };
