eda2f298133cae377841540940dd244df5b05341
Yanick Champoux <yanick@babyl.ca>
2023-03-23 12:01:33 -0400

    improve signature of prop as well

--- src/prop.test.ts
+++ src/prop.test.ts
@@ -1,7 +1,29 @@
 import { pipe } from './pipe';
 import { prop } from './prop';
+import { expectType } from './_expectType';
 
 test('prop', () => {
   const result = pipe({ foo: 'bar' }, prop('foo'));
   expect(result).toEqual('bar');
+});
+
+test('type for curried form', () => {
+  const propFoo = prop('foo');
+
+  expectType<(input: { foo: any }) => any>(true as any as typeof propFoo);
+
+  const result = propFoo({ foo: 1, bar: 'potato' });
+
+  expectType<number>(result);
+
+  expect(result).toBe(1);
+
+  // without the key
+  const r2 = propFoo({ bar: 'potato' });
+  expect(r2).toBe(undefined);
+
+  // can specify the target object type at creation
+  const propBar = prop('bar')<{ bar: number; baz: boolean }>;
+
+  expectType<number>(true as any as ReturnType<typeof propBar>);
 });
