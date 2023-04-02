940f8b4e3ca46c0b182a7dca629f23487ed78f80
Dominik Dorfmeister <office@dorfmeister.cc>
2023-04-02 11:45:30 +0200

    test: switch to expectTypeOf

--- src/zipObj.test.ts
+++ src/zipObj.test.ts
@@ -1,5 +1,5 @@
 import { zipObj } from './zipObj';
-import { AssertEqual } from './_types';
+import { expectTypeOf } from 'vitest';
 
 const first = ['a', 'b', 'c'];
 const second = [1, 2, 3];
@@ -31,24 +31,19 @@ describe('data first', () => {
 describe('data first typings', () => {
   test('arrays', () => {
     const actual = zipObj(first, second);
-    const result: AssertEqual<typeof actual, Record<string, number>> = true;
-    expect(result).toBe(true);
+    expectTypeOf(actual).toEqualTypeOf<Record<string, number>>();
   });
   test('tuples', () => {
     const actual = zipObj(first as ['a', 'b', 'c'], second as [1, 2, 3]);
-    const result: AssertEqual<typeof actual, Record<"a" | "b" | "c", 1 | 2 | 3>> =
-      true;
-    expect(result).toBe(true);
+    expectTypeOf(actual).toEqualTypeOf<Record<'a' | 'b' | 'c', 1 | 2 | 3>>();
   });
   test('variadic tuples', () => {
     const firstVariadic: [number, ...Array<string>] = [1, 'b', 'c'];
     const secondVariadic: [string, ...Array<number>] = ['a', 2, 3];
     const actual = zipObj(firstVariadic, secondVariadic);
-    const result: AssertEqual<
-      typeof actual,
+    expectTypeOf(actual).toEqualTypeOf<
       Record<string | number, number | string>
-    > = true;
-    expect(result).toBe(true);
+    >();
   });
 });
 
@@ -74,26 +69,21 @@ describe('data second', () => {
   });
 });
 
-describe('data second typings', () => {
+describe('data last typings', () => {
   test('arrays', () => {
     const actual = zipObj(second)(first);
-    const result: AssertEqual<typeof actual, Record<string, number>> = true;
-    expect(result).toBe(true);
+    expectTypeOf(actual).toEqualTypeOf<Record<string, number>>();
   });
   test('tuples', () => {
     const actual = zipObj(second as [1, 2, 3])(first as ['a', 'b', 'c']);
-    const result: AssertEqual<typeof actual, Record<"a" | "b" | "c", 1 | 2 | 3>> =
-      true;
-    expect(result).toBe(true);
+    expectTypeOf(actual).toEqualTypeOf<Record<'a' | 'b' | 'c', 1 | 2 | 3>>();
   });
   test('variadic tuples', () => {
     const firstVariadic: [number, ...Array<string>] = [1, 'b', 'c'];
     const secondVariadic: [string, ...Array<number>] = ['a', 2, 3];
     const actual = zipObj(secondVariadic)(firstVariadic);
-    const result: AssertEqual<
-      typeof actual,
+    expectTypeOf(actual).toEqualTypeOf<
       Record<string | number, string | number>
-    > = true;
-    expect(result).toBe(true);
+    >();
   });
 });
