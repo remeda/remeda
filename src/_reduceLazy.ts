9a4150c83cf8164fd9716bbc72ba46995733b628
Eran Hirsch <eranhirsch@gmail.com>
2023-04-03 12:43:48 +0300

    cleaner code

--- src/_reduceLazy.ts
+++ src/_reduceLazy.ts
@@ -33,12 +33,13 @@ export function _reduceLazy<T, K>(
   // We intentionally use a for loop here instead of reduce for performance reasons. See https://leanylabs.com/blog/js-forEach-map-reduce-vs-for-for_of/ for more info
   for (const item of array) {
     const result = indexed ? lazy(item, index, array) : lazy(item);
+    index += 1;
+
     if (result.hasMany === true) {
       newArray.push(...result.next);
     } else if (result.hasNext) {
       newArray.push(result.next);
     }
-    index += 1;
   }
   return newArray;
 }
