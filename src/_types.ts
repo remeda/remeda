f22903190f7e26fa16ac4d5c2478e230a5acf16b
Eran Hirsch <eranhirsch@gmail.com>
2023-04-04 17:16:25 +0300

    chore: remove generic types

--- src/_types.ts
+++ src/_types.ts
@@ -6,12 +6,6 @@ export type PredIndexedOptional<T, K> = (
   array?: Array<T>
 ) => K;
 
-/** types that may be returned by `keyof` */
-export type Key = string | number | symbol;
-
-/** Mapped type to remove optional, null, and undefined from all props */
-export type NonNull<T> = { [K in keyof T]-?: Exclude<T[K], null | undefined> };
-
 export type NonEmptyArray<T> = [T, ...Array<T>];
 
 /**
