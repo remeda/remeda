1a846295d0b59f26f2dea3083ca2fe7fa52acc7e
Eran Hirsch <eranhirsch@gmail.com>
2023-04-12 14:50:42 +0300

    docs

--- src/join.ts
+++ src/join.ts
@@ -31,11 +31,49 @@ type JoinedValue<V> =
     ? `${V}`
     : "ERROR: Trying to join a value type that isn't castable to string";
 
+/**
+ * Joins the elements of the array by: casting them to a string and
+ * concatenating them one to the other, with the provided glue string in between
+ * every two elements.
+ *
+ * When called on a tuple and with stricter item types (union of literal values,
+ * the result is strictly typed to the tuples shape and it's item types).
+ *
+ * @param data The array to join
+ * @param glue The string to put in between every two elements
+ * @signature
+ *    R.join(data, glue)
+ * @example
+ *    R.join([1,2,3], ",") // => "1,2,3" (typed `string`)
+ *    R.join(['a','b','c'], "") // => "abc" (typed `string`)
+ *    R.join(['hello', 'world'] as const, " ") // => "hello world" (typed `hello world`)
+ * @data_first
+ * @category Array
+ */
 export function join<
   T extends ReadonlyArray<Joinable> | [],
   Glue extends string
 >(data: T, glue: Glue): Joined<T, Glue>;
 
+/**
+ * Joins the elements of the array by: casting them to a string and
+ * concatenating them one to the other, with the provided glue string in between
+ * every two elements.
+ *
+ * When called on a tuple and with stricter item types (union of literal values,
+ * the result is strictly typed to the tuples shape and it's item types).
+ *
+ * @param data The array to join
+ * @param glue The string to put in between every two elements
+ * @signature
+ *    R.join(glue)(data)
+ * @example
+ *    R.pipe([1,2,3], R.join(",")) // => "1,2,3" (typed `string`)
+ *    R.pipe(['a','b','c'], R.join("")) // => "abc" (typed `string`)
+ *    R.pipe(['hello', 'world'] as const, R.join(" ")) // => "hello world" (typed `hello world`)
+ * @data_last
+ * @category Array
+ */
 export function join<
   T extends ReadonlyArray<Joinable> | [],
   Glue extends string
