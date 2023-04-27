a0396f72bed481b6127e79e848fe9015284e4346
Eran Hirsch <eranhirsch@gmail.com>
2023-04-27 14:55:57 +0300

    docs and index

--- src/isDefined.ts
+++ src/isDefined.ts
@@ -1,14 +1,19 @@
 /**
- * A function that checks if the passed parameter is defined and narrows its type accordingly
+ * A function that checks if the passed parameter is defined and narrows its type accordingly.
+ * To test specifically for `undefined` (and not `null`) use the strict variant of this function.
  * @param data the variable to check
  * @signature
  *    R.isDefined(data)
+ *    R.isDefined.strict(data)
  * @returns true if the passed input is defined, false otherwise
  * @example
  *    R.isDefined('string') //=> true
  *    R.isDefined(null) //=> false
  *    R.isDefined(undefined) //=> false
+ *    R.isDefined.strict(null) //=> true
+ *    R.isDefined.strict(undefined) //=> false
  * @category Guard
+ * @strict
  */
 export function isDefined<T>(data: T): data is NonNullable<T> {
   return typeof data !== 'undefined' && data !== null;
--- src/isNonNull.ts
+++ src/isNonNull.ts
@@ -1,3 +1,16 @@
+/**
+ * A function that checks if the passed parameter is not `null` and narrows its type accordingly.
+ * Notice that `undefined` is not null!
+ * @param data the variable to check
+ * @signature
+ *    R.isDefined(data)
+ * @returns true if the passed input is defined, false otherwise
+ * @example
+ *    R.isDefined('string') //=> true
+ *    R.isDefined(null) //=> false
+ *    R.isDefined(undefined) //=> true
+ * @category Guard
+ */
 export function isNonNull<T>(data: T | null): data is T {
   return data !== null;
 }
