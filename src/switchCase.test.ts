1bb9e7c598b0749b042a3b3a8ae345f7684107c0
aose-yuu <aose.developer@gmail.com>
2023-05-04 18:07:44 +0900

    refactor: update test cases for consistency and order

--- src/switchCase.test.ts
+++ src/switchCase.test.ts
@@ -2,107 +2,82 @@ import { switchCase } from './switchCase';
 import { pipe } from './pipe';
 
 describe('switchCase', () => {
-  test('data-first: with matching case', () => {
+  test('data-first: matching case', () => {
     const result = switchCase(
       5,
-      {
-        1: () => 'first',
-        5: () => 'last',
-      },
+      { 1: () => 'first', 5: () => 'last' },
       () => 'not exists'
     );
     expect(result).toEqual('last');
   });
 
-  test('data-first: without matching case', () => {
+  test('data-first: no matching case', () => {
     const result = switchCase(
       3,
-      {
-        1: () => 'first',
-        5: () => 'last',
-      },
+      { 1: () => 'first', 5: () => 'last' },
       () => 'not exists'
     );
     expect(result).toEqual('not exists');
   });
 
-  test('data-last: with matching case', () => {
+  test('data-first: non-string keys', () => {
+    const result = switchCase(
+      5,
+      { '2': () => 'even', '5': () => 'odd' },
+      () => 'not exists'
+    );
+    expect(result).toEqual('odd');
+  });
+
+  test('data-first: non-numeric keys', () => {
+    const result = switchCase(
+      'apple',
+      { banana: () => 'fruit', apple: () => 'fruit too' },
+      () => 'not exists'
+    );
+    expect(result).toEqual('fruit too');
+  });
+
+  test('data-last: matching case', () => {
     const result = pipe(
       5,
-      switchCase(
-        {
-          1: () => 'first',
-          5: () => 'last',
-        },
-        () => 'not exists'
-      )
+      switchCase({ 1: () => 'first', 5: () => 'last' }, () => 'not exists')
     );
     expect(result).toEqual('last');
   });
 
-  test('data-last: without matching case', () => {
+  test('data-last: no matching case', () => {
     const result = pipe(
       3,
-      switchCase(
-        {
-          1: () => 'first',
-          5: () => 'last',
-        },
-        () => 'not exists'
-      )
+      switchCase({ 1: () => 'first', 5: () => 'last' }, () => 'not exists')
     );
     expect(result).toEqual('not exists');
   });
 
-  test('data-first: with non-string keys', () => {
-    const result = switchCase(
+  test('data-last: non-string keys', () => {
+    const result = pipe(
       5,
-      {
-        '2': () => 'even',
-        '5': () => 'odd',
-      },
-      () => 'unknown'
+      switchCase({ '2': () => 'even', '5': () => 'odd' }, () => 'not exists')
     );
     expect(result).toEqual('odd');
   });
 
-  test('data-last: with non-string keys', () => {
+  test('data-last: non-numeric keys', () => {
     const result = pipe(
-      5,
+      'apple',
       switchCase(
-        {
-          '2': () => 'even',
-          '5': () => 'odd',
-        },
-        () => 'unknown'
+        { banana: () => 'fruit', apple: () => 'fruit too' },
+        () => 'not exists'
       )
     );
-    expect(result).toEqual('odd');
+    expect(result).toEqual('fruit too');
   });
 
-  test('data-first: with non-numeric keys', () => {
+  test('R.switchCase: data-last matching case', () => {
     const result = switchCase(
-      'apple',
-      {
-        banana: () => 'fruit',
-        apple: () => 'fruit too',
-      },
-      () => 'unknown'
-    );
-    expect(result).toEqual('fruit too');
-  });
-
-  test('data-last: with non-numeric keys', () => {
-    const result = pipe(
-      'apple',
-      switchCase(
-        {
-          banana: () => 'fruit',
-          apple: () => 'fruit too',
-        },
-        () => 'unknown'
-      )
-    );
-    expect(result).toEqual('fruit too');
+      { 1: () => 'first', 5: () => 'last' },
+      () => 'not exists'
+    )(5);
+    expect(result).toEqual('last');
   });
 });
