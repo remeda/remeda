86770ecaa10604ba9303c8204c68ea3825ba1ffc
Landon Schropp <schroppl@gmail.com>
2023-05-24 12:56:33 -0700

    Update the documentation
    Split the data-first and data-last signatures apart, and put the example
    in the correct places.

--- src/invert.ts
+++ src/invert.ts
@@ -11,16 +11,24 @@ type Inverted = { [index: string]: string };
  *    R.invert(object)
  * @example
  *    R.invert({ a: "d", b: "e", c: "f" }) // => { d: "a", e: "b", f: "c" }
- *    R.invert({}) // => {}
- *    R.pipe(
- *      { a: "d", b: "e", c: "f" }
- *      R.invert()
- *    ); // => { d: "a", e: "b", f: "c" }
- *
+ * @data_first
  * @category object
  * @pipeable
  */
 export function invert<T>(object: Invertable): Inverted;
+
+/**
+ * Returns an object whose keys are values are swapped. If the object contains duplicate values,
+ * subsequent values will overwrite previous values.
+ * @param object the object
+ * @signature
+ *    R.invert()(object)
+ * @example
+ *    R.pipe({ a: "d", b: "e", c: "f" }, R.invert()); // => { d: "a", e: "b", f: "c" }
+ * @data_last
+ * @category object
+ * @pipeable
+ */
 export function invert<T>(): (object: Invertable) => Inverted;
 
 export function invert() {
