f36eab282dbbc4a30467a04c5857c14d9342c75c
Eran Hirsch <eranhirsch@gmail.com>
2023-06-26 08:36:37 +0000

    fix

--- src/prop.ts
+++ src/prop.ts
@@ -1,17 +1,13 @@
 /**
  * Gets the value of the given property.
- * @param prop the property name
+ * @param propName the property name
  * @signature R.prop(prop)(object)
  * @example
  *    R.pipe({foo: 'bar'}, R.prop('foo')) // => 'bar'
  * @data_last
  * @category Object
  */
-export function prop<T, K extends keyof T>(prop: K): (data: T) => T[K];
-export function prop<K extends PropertyKey>(
-  prop: K
-): <T>(data: T) => K extends keyof T ? T[K] : undefined;
-
-export function prop<T, K extends keyof T>(prop: K): (data: T) => unknown {
-  return data => data[prop];
-}
+export const prop =
+  <T, K extends keyof T>(propName: K): ((data: T) => T[K]) =>
+  ({ [propName]: value }) =>
+    value;
