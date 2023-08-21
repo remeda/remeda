77377cf9c944e8cdf42878bb00135abc1564e783
u-yas <yuya3.21998@gmail.com>
2023-08-21 20:11:37 +0900

    remove debug print in App.tsx https://github.com/remeda/remeda/pull/359#discussion_r1299785603

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -2,14 +2,8 @@ import { Header } from './Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Docs } from './Docs';
 import { Home } from './Home';
-import { useEffect } from 'react';
 
 export function App() {
-  useEffect(()=>{
-    window.addEventListener('popstate', e => {
-      console.log(e);
-    });
-  },[])
   return (
     <Router>
       <div className="wrapper">
