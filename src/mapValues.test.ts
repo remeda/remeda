a8dfb56f295446b05b105e2430b5558746f88d67
Sergey Kozlov <dartess@mail.ru>
2023-08-21 12:54:14 +0400

    add mapValues key tests

--- src/mapValues.test.ts
+++ src/mapValues.test.ts
@@ -42,19 +42,43 @@ describe('mapValues key types', () => {
     );
   });
 
+  test('interface with symbols', () => {
+    const mySymbol = Symbol('mySymbol');
+    mapValues(
+      {} as { [mySymbol]: unknown; foo: unknown; bar: unknown },
+      (_, key) => expectTypeOf(key).toEqualTypeOf<'foo' | 'bar'>()
+    );
+  });
+
   test('mapped type', () => {
     mapValues({} as { [K in string]: unknown }, (_, key) => {
       expectTypeOf(key).toEqualTypeOf<string>();
     });
+    mapValues({} as { [K in number]: unknown }, (_, key) => {
+      expectTypeOf(key).toEqualTypeOf<`${number}`>();
+    });
+    mapValues({} as { [K in `prefix${string}`]: unknown }, (_, key) => {
+      expectTypeOf(key).toEqualTypeOf<`prefix${string}`>();
+    });
+    mapValues({} as { [K in symbol]: unknown }, (_, key) => {
+      expectTypeOf(key).toEqualTypeOf<never>();
+    });
   });
 
   test('indexed signature', () => {
-    mapValues(
-      // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style -- must be "indexed signature"
-      {} as { [key: string]: unknown },
-      (_, key) => {
-        expectTypeOf(key).toEqualTypeOf<string>();
-      }
-    );
+    /* eslint-disable @typescript-eslint/consistent-indexed-object-style -- must be "indexed signature" */
+    mapValues({} as { [key: string]: unknown }, (_, key) => {
+      expectTypeOf(key).toEqualTypeOf<string>();
+    });
+    mapValues({} as { [key: number]: unknown }, (_, key) => {
+      expectTypeOf(key).toEqualTypeOf<`${number}`>();
+    });
+    mapValues({} as { [key: `prefix${string}`]: unknown }, (_, key) => {
+      expectTypeOf(key).toEqualTypeOf<`prefix${string}`>();
+    });
+    mapValues({} as { [key: symbol]: unknown }, (_, key) => {
+      expectTypeOf(key).toEqualTypeOf<never>();
+    });
+    /* eslint-enable @typescript-eslint/consistent-indexed-object-style */
   });
 });
