3a799cbe6eb13561574882f15b4bcffaea047be1
Sergey Kozlov <dartess@mail.ru>
2023-08-21 11:48:57 +0400

    add type tests for mapValues keys

--- src/mapValues.test.ts
+++ src/mapValues.test.ts
@@ -34,3 +34,27 @@ describe('data last', () => {
     });
   });
 });
+
+describe('mapValues key types', () => {
+  test('interface', () => {
+    mapValues({} as { foo: unknown; bar: unknown }, (_, key) =>
+      expectTypeOf(key).toEqualTypeOf<'foo' | 'bar'>()
+    );
+  });
+
+  test('mapped type', () => {
+    mapValues({} as { [K in string]: unknown }, (_, key) => {
+      expectTypeOf(key).toEqualTypeOf<string>();
+    });
+  });
+
+  test('indexed signature', () => {
+    mapValues(
+      // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style -- must be "indexed signature"
+      {} as { [key: string]: unknown },
+      (_, key) => {
+        expectTypeOf(key).toEqualTypeOf<string>();
+      }
+    );
+  });
+});
