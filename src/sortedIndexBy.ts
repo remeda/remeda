bb25fd0eda68921a7196c28f0a4a52ac01d1cbbb
Eran Hirsch <eranhirsch@gmail.com>
2023-08-22 16:46:20 +0300

    feat (sortedIndex): added functions for binary searching arrays (#357)
    * sortedIndex
    
    * revert
    
    * more docs
    
    * docs
    
    * add mapping
    
    * correct
    
    * indexed
    
    * mark as indexed
    
    * fix everything
    
    * tests
    
    * indexBy tests
    
    * test
    
    * sort mapping
    
    * docs
    
    * split binary search from sortedIndexWith
    
    * nit
    
    * fix our docs site
    
    * comment
    
    ---------
    
    Co-authored-by: Dominik Dorfmeister <office@dorfmeister.cc>

---
+++ src/sortedIndexBy.ts
@@ -0,0 +1,124 @@
+import { purry } from './purry';
+import { _binarySearchCutoffIndex } from './_binarySearchCutoffIndex';
+
+/**
+ * Find the insertion position (index) of an item in an array with items sorted
+ * in ascending order using a value function; so that `splice(sortedIndex, 0, item)`
+ * would result in maintaining the arrays sort-ness. The array can contain
+ * duplicates.
+ * If the item already exists in the array the index would be of the *first*
+ * occurrence of the item.
+ *
+ * Runs in O(logN) time.
+ *
+ * @param data - The (ascending) sorted array.
+ * @param item - The item to insert.
+ * @param valueFunction - All comparisons would be performed on the result of
+ * calling this function on each compared item. Preferably this function should
+ * return a `number` or `string`. This function should be the same as the one
+ * provided to sortBy to sort the array.
+ * @return - Insertion index (In the range 0..data.length)
+ *
+ * @signature
+ *    R.sortedIndexBy(data, item, valueFunction)
+ * @example
+ *    R.sortedIndexBy([{age:20},{age:22}],{age:21},prop('age')) // => 1
+ * @data_first
+ * @indexed
+ * @category Array
+ *
+ * @see sortedIndex, sortedIndexWith, sortedLastIndex, sortedLastIndexBy
+ */
+export function sortedIndexBy<T>(
+  data: ReadonlyArray<T>,
+  item: T,
+  valueFunction: (item: T) => NonNullable<unknown>
+): number;
+
+/**
+ * Find the insertion position (index) of an item in an array with items sorted
+ * in ascending order using a value function; so that `splice(sortedIndex, 0, item)`
+ * would result in maintaining the arrays sort-ness. The array can contain
+ * duplicates.
+ * If the item already exists in the array the index would be of the *first*
+ * occurrence of the item.
+ *
+ * Runs in O(logN) time.
+ *
+ * @param data - The (ascending) sorted array.
+ * @param item - The item to insert.
+ * @param valueFunction - All comparisons would be performed on the result of
+ * calling this function on each compared item. Preferably this function should
+ * return a `number` or `string`. This function should be the same as the one
+ * provided to sortBy to sort the array.
+ * @return - Insertion index (In the range 0..data.length)
+ *
+ * @signature
+ *    R.sortedIndexBy(data, item, valueFunction)
+ * @example
+ *    R.sortedIndexBy([{age:20},{age:22}],{age:21},prop('age')) // => 1
+ * @data_last
+ * @indexed
+ * @category Array
+ *
+ * @see sortedIndex, sortedIndexWith, sortedLastIndex, sortedLastIndexBy
+ */
+export function sortedIndexBy<T>(
+  item: T,
+  valueFunction: (item: T) => NonNullable<unknown>
+): (data: ReadonlyArray<T>) => number;
+
+/**
+ * Find the insertion position (index) of an item in an array with items sorted
+ * in ascending order using a value function; so that `splice(sortedIndex, 0, item)`
+ * would result in maintaining the arrays sort-ness. The array can contain
+ * duplicates. If the item already exists in the array the index would be of the
+ * first occurrence of the item.
+ *
+ * @param data - The (ascending) sorted array.
+ * @param item - The item to insert.
+ * @param valueFunction - All comparisons would be performed on the result of
+ * calling this function on each compared item. Preferably this function should
+ * return a `number` or `string`. This function should be the same as the one
+ * provided to sortBy to sort the array.
+ * @return - Insertion index (In the range 0..array.length - 1)
+ *
+ * @signature
+ *    R.sortedIndexBy(item, valueFunction)(data)
+ * @example
+ *    R.pipe([{age:20},{age:22}], R.sortedIndexBy({age:21},prop('age'))) // => 1
+ * @data_last
+ * @category Array
+ *
+ * @see sortedIndex, sortedIndexWith, sortedLastIndex, sortedLastIndexBy
+ */
+export function sortedIndexBy(): unknown {
+  return purry(sortedIndexByImplementation, arguments);
+}
+
+export namespace sortedIndexBy {
+  export function indexed<T>(
+    data: ReadonlyArray<T>,
+    item: T,
+    valueFunction: (item: T, index?: number) => NonNullable<unknown>
+  ): number;
+  export function indexed<T>(
+    item: T,
+    valueFunction: (item: T, index?: number) => NonNullable<unknown>
+  ): (data: ReadonlyArray<T>) => number;
+  export function indexed(): unknown {
+    return purry(sortedIndexByImplementation, arguments);
+  }
+}
+
+function sortedIndexByImplementation<T>(
+  array: ReadonlyArray<T>,
+  item: T,
+  valueFunction: (item: T, index?: number) => NonNullable<unknown>
+): number {
+  const value = valueFunction(item);
+  return _binarySearchCutoffIndex(
+    array,
+    (pivot, index) => valueFunction(pivot, index) < value
+  );
+}
---
+++ src/sortedLastIndex.test.ts
@@ -0,0 +1,63 @@
+import { sortedLastIndex } from './sortedLastIndex';
+
+describe('runtime correctness', () => {
+  test('empty array', () => {
+    expect(sortedLastIndex([], 1)).toBe(0);
+  });
+
+  describe('array with single item', () => {
+    test('item smaller than item in array', () => {
+      expect(sortedLastIndex([2], 1)).toBe(0);
+    });
+
+    test('item larger than item in array', () => {
+      expect(sortedLastIndex([0], 1)).toBe(1);
+    });
+
+    test('item in array', () => {
+      expect(sortedLastIndex([1], 1)).toBe(1);
+    });
+  });
+
+  describe('array with multiple values', () => {
+    test('item smaller than all items in array', () => {
+      expect(sortedLastIndex([1, 2, 3, 4, 5], 0)).toBe(0);
+    });
+
+    test('item larger than all items in array', () => {
+      expect(sortedLastIndex([1, 2, 3, 4, 5], 6)).toBe(5);
+    });
+
+    test('item in array', () => {
+      expect(sortedLastIndex([1, 2, 3, 4, 5], 3)).toBe(3);
+    });
+  });
+
+  describe('array with duplicates', () => {
+    test('item smaller than all items in array', () => {
+      expect(sortedLastIndex([1, 1, 1, 1, 1], 0)).toBe(0);
+    });
+
+    test('item larger than all items in array', () => {
+      expect(sortedLastIndex([1, 1, 1, 1, 1], 2)).toBe(5);
+    });
+
+    test('item in array', () => {
+      expect(sortedLastIndex([1, 1, 1, 1, 1], 1)).toBe(5);
+    });
+  });
+
+  describe('string array', () => {
+    test('item smaller than all items in array', () => {
+      expect(sortedLastIndex(['a', 'b', 'c', 'd', 'e'], ' ')).toBe(0);
+    });
+
+    test('item larger than all items in array', () => {
+      expect(sortedLastIndex(['a', 'b', 'c', 'd', 'e'], 'z')).toBe(5);
+    });
+
+    test('item in array', () => {
+      expect(sortedLastIndex(['a', 'b', 'c', 'd', 'e'], 'c')).toBe(3);
+    });
+  });
+});
