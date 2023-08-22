d6d7006a21e3453b40d0003b8b23b187705f79a2
inuneko <yuya3.21998@gmail.com>
2023-08-23 00:17:56 +0900

    refactor: import named createRoot module

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 
 const rootEl = document.getElementById('root');
 if (!rootEl) {
   throw new Error('element with id="root" not found. Please check index.html');
 }
-ReactDOM.createRoot(rootEl).render(
+createRoot(rootEl).render(
   <StrictMode>
     <App />
   </StrictMode>
