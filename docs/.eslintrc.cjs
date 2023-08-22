9570462c2cc399ee13bbb64220aaa9d3d3765330
inuneko <52757302+u-yas@users.noreply.github.com>
2023-08-22 16:49:28 +0900

    Do not extend root eslintrc.js docs/.eslintrc.cjs
    Co-authored-by: Eran Hirsch <eranhirsch@gmail.com>

--- docs/.eslintrc.cjs
+++ docs/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['../.eslintrc.js', 'plugin:react-hooks/recommended'],
+  extends: ['plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
