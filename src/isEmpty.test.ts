9c79944e9cda461c153e19d956aae50b1122bb2f
Eran Hirsch <eranhirsch@gmail.com>
2023-09-01 14:55:17 +0300

    feat(types): isEmpty accept undefined for strings (#377)
    * allow undefined
    
    * more testing

--- src/isEmpty.test.ts
+++ src/isEmpty.test.ts
@@ -24,17 +24,112 @@ describe('isEmpty', () => {
     expect(isEmpty({ length: 0 })).toBe(false);
   });
 
+  test('returns true for undefined', () => {
+    expect(isEmpty(undefined)).toBe(true);
+  });
+
   test('does not accept invalid input types', () => {
-    // @ts-expect-error number is not a valid input type
+    // @ts-expect-error [ts2769] number is not a valid input type
     isEmpty(2);
 
-    // @ts-expect-error boolean is not a valid input type
+    // @ts-expect-error [ts2769] boolean is not a valid input type
     isEmpty(false);
 
-    // @ts-expect-error null is not a valid input type
+    // @ts-expect-error [ts2769] null is not a valid input type
     isEmpty(null);
 
-    // @ts-expect-error undefined is not a valid input type
-    isEmpty(undefined);
+    // @ts-expect-error [ts2769] undefined is only allowed with strings
+    isEmpty([] as ReadonlyArray<string> | undefined);
+
+    // @ts-expect-error [ts2769] undefined is only allowed with strings
+    isEmpty({} as Record<string, string> | undefined);
+  });
+});
+
+describe('strings are narrowed correctly', () => {
+  test('just undefined', () => {
+    const data = undefined;
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<undefined>();
+    }
+  });
+
+  test('just string', () => {
+    const data = '' as string;
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<''>();
+    }
+  });
+
+  test('just EMPTY string', () => {
+    const data = '' as const;
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<''>();
+    }
+  });
+
+  test('string or undefined', () => {
+    const data = undefined as string | undefined;
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<'' | undefined>();
+    }
+  });
+
+  test('string literals that CANT be empty or undefined', () => {
+    const data = 'cat' as 'cat' | 'dog';
+    if (isEmpty(data)) {
+      // unreachable
+      expectTypeOf(data).toEqualTypeOf<never>();
+    }
+  });
+
+  test('string literals that CAN be empty', () => {
+    const data = 'cat' as 'cat' | 'dog' | '';
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<''>();
+    }
+  });
+
+  test('string literals that CAN be undefined', () => {
+    const data = 'cat' as 'cat' | 'dog' | undefined;
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<undefined>();
+    }
+  });
+
+  test('string literals that CAN be undefined or empty', () => {
+    const data = 'cat' as 'cat' | 'dog' | '' | undefined;
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<'' | undefined>();
+    }
+  });
+
+  test('string templates that CANT be empty or undefined', () => {
+    const data = 'prefix_0' as `prefix_${number}`;
+    if (isEmpty(data)) {
+      // unreachable
+      expectTypeOf(data).toEqualTypeOf<never>();
+    }
+  });
+
+  test('string templates that CAN be empty', () => {
+    const data = '' as `prefix_${number}` | '';
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<''>();
+    }
+  });
+
+  test('string templates that CAN be undefined', () => {
+    const data = 'prefix_0' as `prefix_${number}` | undefined;
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<undefined>();
+    }
+  });
+
+  test('string templates that CAN be undefined or empty', () => {
+    const data = 'prefix_0' as `prefix_${number}` | '' | undefined;
+    if (isEmpty(data)) {
+      expectTypeOf(data).toEqualTypeOf<'' | undefined>();
+    }
   });
 });
