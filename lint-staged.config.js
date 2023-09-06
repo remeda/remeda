6422e02d1e6db1d8bfdf1323b17cd8ae772cb465
Eran Hirsch <eranhirsch@gmail.com>
2023-09-06 16:03:57 +0300

    revert

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  // Docs site
-  'docs/**/*.+(js|ts|jsx|tsx|mjs|cjs)': [
-    'cd docs && eslint --fix',
-    'cd docs && prettier --write',
-  ],
-  'docs/**/*.+(json|json5|yaml|yml|css|scss|less|htm|html|svg|md|graphql)':
-    'cd docs && prettier --write',
-
   // Code
   '*.+(js|ts|jsx|tsx|mjs|cjs)': ['eslint --fix', 'prettier --write'],
   // Configs
