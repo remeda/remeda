fe5b276d7997d9e1e2d515ecefa608028feda00d
Eran Hirsch <eranhirsch@gmail.com>
2023-09-06 13:57:50 +0300

    modernize

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
+import invariant from 'tiny-invariant';
 
-const rootEl = document.getElementById('root');
-if (!rootEl) {
-  throw new Error('element with id="root" not found. Please check index.html');
-}
-createRoot(rootEl).render(
+const ROOT_ELEMENT_ID = 'root';
+
+const rootElement = document.getElementById(ROOT_ELEMENT_ID);
+invariant(
+  rootElement !== null,
+  `element with id=${ROOT_ELEMENT_ID} not found. Please check index.html`,
+);
+
+const reactRoot = createRoot(rootElement);
+reactRoot.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
