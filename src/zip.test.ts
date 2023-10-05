66f83d6ac494014401b42561d0c770f0a716a1d9
Adam Di Mario <nev9adam@gmail.com>
2023-10-05 19:54:03 +0200

    feat: strict data first zip

--- src/zip.test.ts
+++ src/zip.test.ts
@@ -82,3 +82,68 @@ describe('data second typings', () => {
     assertType<Array<[string | number, string | number]>>(actual);
   });
 });
+
+describe('strict typings', () => {
+  it('on empty tuples', () => {
+    const array: [] = [];
+    const result = zip.strict(array, array);
+    expectTypeOf(result).toEqualTypeOf<[]>();
+  });
+
+  it('on empty readonly tuples', () => {
+    const array: readonly [] = [];
+    const result = zip.strict(array, array);
+    expectTypeOf(result).toEqualTypeOf<[]>();
+  });
+
+  it('on arrays', () => {
+    const array: Array<number> = [];
+    const result = zip.strict(array, array);
+    expectTypeOf(result).toEqualTypeOf<Array<[number, number]>>();
+  });
+
+  it('on mixed typeds array', () => {
+    const array1: Array<number> = [];
+    const array2: Array<string> = [];
+    const result = zip.strict(array1, array2);
+    expectTypeOf(result).toEqualTypeOf<Array<[number, string]>>();
+  });
+
+  it('on readonly arrays', () => {
+    const array: ReadonlyArray<number> = [];
+    const result = zip.strict(array, array);
+    expectTypeOf(result).toEqualTypeOf<Array<[number, number]>>();
+  });
+
+  it('on tuples', () => {
+    const array1: [1, 2, 3] = [1, 2, 3];
+    const array2: [4, 5, 6] = [4, 5, 6];
+    const result = zip.strict(array1, array2);
+    expectTypeOf(result).toEqualTypeOf<[[1, 4], [2, 5], [3, 6]]>();
+  });
+
+  it('on readonly tuples', () => {
+    const array1: readonly [1, 2, 3] = [1, 2, 3];
+    const array2: readonly [4, 5, 6] = [4, 5, 6];
+    const result = zip.strict(array1, array2);
+    expectTypeOf(result).toEqualTypeOf<[[1, 4], [2, 5], [3, 6]]>();
+  });
+
+  it('on tuples of different lengths', () => {
+    const array1: [1, 2, 3] = [1, 2, 3];
+    const array2: [4, 5] = [4, 5];
+    const result1 = zip.strict(array1, array2);
+    expectTypeOf(result1).toEqualTypeOf<[[1, 4], [2, 5]]>();
+    const result2 = zip.strict(array2, array1);
+    expectTypeOf(result2).toEqualTypeOf<[[4, 1], [5, 2]]>();
+  });
+
+  test('on variadic tuples', () => {
+    const firstVariadic: [number, ...Array<string>] = [1, 'b', 'c'];
+    const secondVariadic: [string, ...Array<number>] = ['a', 2, 3];
+    const result = zip.strict(firstVariadic, secondVariadic);
+    expectTypeOf(result).toEqualTypeOf<
+      Array<[string | number, string | number]>
+    >;
+  });
+});
--- src/zip.ts
+++ src/zip.ts
@@ -1,16 +1,22 @@
+import { IterableContainer } from './_types';
 import { purry } from './purry';
 
 /**
  * Creates a new list from two supplied lists by pairing up equally-positioned items.
  * The length of the returned list will match the shortest of the two inputs.
+ *
+ * If the input array are non-variadic tuple, you can use the strict option
+ * to get another tuple instead of an array type.
  * @param first the first input list
  * @param second the second input list
  * @signature
  *   R.zip(first, second)
  * @example
- *   R.zip([1, 2], ['a', 'b']) // => [1, 'a'], [2, 'b']
+ *   R.zip([1, 2], ['a', 'b']) // => [[1, 'a'], [2, 'b']] (type: [1 | 2, 'a' | 'b'][])
+ *   R.zip.strict([1, 2] as const, ['a', 'b'] as const) // => [[1, 'a'], [2, 'b']]  (type: [[1, 'a'], [2, 'b']])
  * @dataFirst
  * @category Array
+ * @strict
  */
 export function zip<F, S>(
   first: ReadonlyArray<F>,
@@ -24,7 +30,7 @@ export function zip<F, S>(
  * @signature
  *   R.zip(second)(first)
  * @example
- *   R.zip(['a', 'b'])([1, 2]) // => [[1, 'a'], [2, 'b']
+ *   R.zip(['a', 'b'])([1, 2]) // => [[1, 'a'], [2, 'b']]
  * @dataLast
  * @category Array
  */
@@ -45,4 +51,40 @@ function _zip(first: Array<unknown>, second: Array<unknown>) {
   }
 
   return result;
+}
+
+type Strict = <T extends IterableContainer, K extends IterableContainer>(
+  first: T,
+  second: K
+) => StrictOut<T, K>;
+
+type JoinTuples<
+  Shorter extends IterableContainer,
+  Longer extends IterableContainer,
+  ShorterFirst extends 0 | 1
+> = {
+  -readonly [P in keyof Shorter]: P extends keyof Longer
+    ? ShorterFirst extends 1
+      ? [Shorter[P], Longer[P]]
+      : [Longer[P], Shorter[P]]
+    : never;
+};
+
+// The first two checks are to ensure that both tuples are finite (checking all
+// the edge cases and possible combinations of variadic tuples is nearly impossible),
+// while the last check is used to determine which tuple is smaller so that it can be
+// used to index the other one
+type StrictOut<
+  K extends IterableContainer,
+  T extends IterableContainer
+> = number extends K['length']
+  ? number extends T['length']
+    ? Array<[K[number], T[number]]>
+    : Array<[K[number], T[number]]>
+  : keyof K extends keyof T
+  ? JoinTuples<K, T, 1>
+  : JoinTuples<T, K, 0>;
+
+export namespace zip {
+  export const strict: Strict = zip;
 }
