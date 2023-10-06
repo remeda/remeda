150c7357512bfdc7d25e9657375a916482af0dba
Ava Johnson <ava.johnson@zohomail.com>
2023-10-05 17:14:24 -0700

    Add more tests

--- src/values.test.ts
+++ src/values.test.ts
@@ -8,21 +8,42 @@ describe('Test for values as data first', () => {
   it('should return values of object', () => {
     expect(values({ a: 'x', b: 'y', c: 'z' })).toEqual(['x', 'y', 'z']);
   });
+});
 
-  it('should accept indexed types', () => {
+describe('typing', () => {
+  it('should correctly types indexed types', () => {
     // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
     expectTypeOf(values<{ [index: string]: string }>({ a: 'b' })).toEqualTypeOf<
       Array<string>
     >();
   });
 
-  it('should correctly type object keys', () => {
+  it('should correctly type functions', () => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    expectTypeOf(values(function namedFunction() {})).toEqualTypeOf<
+      Array<never>
+    >();
+  });
+
+  it('should correctly type arrays', () => {
+    expectTypeOf(values([1, 2, 3])).toEqualTypeOf<Array<number>>();
+  });
+
+  it('should correctly type const arrays', () => {
+    expectTypeOf(values([1, 2, 3] as const)).toEqualTypeOf<Array<1 | 2 | 3>>();
+  });
+
+  it('should correctly type objects', () => {
+    expectTypeOf(values({ a: true })).toEqualTypeOf<Array<boolean>>();
+  });
+
+  it('should correctly type Records', () => {
+    expectTypeOf(values({ a: true })).toEqualTypeOf<Array<boolean>>();
+  });
+
+  it('should correctly type typed objects', () => {
     expectTypeOf(
       values<{ type: 'cat' | 'dog'; age: number }>({ type: 'cat', age: 7 })
     ).toEqualTypeOf<Array<'cat' | 'dog' | number>>();
-  });
-
-  it('should correctly type array keys', () => {
-    expectTypeOf(values([1, 2, 3] as const)).toEqualTypeOf<Array<1 | 2 | 3>>();
   });
 });
