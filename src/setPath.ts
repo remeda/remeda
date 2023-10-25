5af92f81dbdd1a794e24b9e6d78654ddeb902a91
ふかねこ / SO-fuka <67921654+fneco@users.noreply.github.com>
2023-10-25 14:30:30 +0900

    refactor(setPath): change variable name, and change docs of `set`, `setPath` (#395)
    * docs: improve `set`, `setPath` docs
    
    * refactor: rename variable
    
    ---------
    
    Co-authored-by: fneco <fizumi.f.tech@gmail.com>

--- src/setPath.ts
+++ src/setPath.ts
@@ -3,9 +3,9 @@ import { Path, SupportsValueAtPath, ValueAtPath } from './_paths';
 import { Narrow } from './_narrow';
 
 /**
- * Sets the value at `path` of `object`. `path` can be an array or a path string.
+ * Sets the value at `path` of `object`.
  * @param object the target method
- * @param path the property name
+ * @param path the array of properties
  * @param value the value to set
  * @signature
  *    R.setPath(obj, path, value)
@@ -21,15 +21,15 @@ export function setPath<T, TPath extends Array<PropertyKey> & Path<T>>(
 ): T;
 
 /**
- * Sets the value at `path` of `object`. `path` can be an array or a path string.
+ * Sets the value at `path` of `object`.
  * @param obj the target method
- * @param path the property name
+ * @param path the array of properties
  * @param value the value to set
  * @signature
- *    R.setPath(obj, path, value)
+ *    R.setPath(path, value)(obj)
  * @example
  *    R.pipe({ a: { b: 1 } }, R.setPath(['a', 'b'], 2)) // { a: { b: 2 } }
- * @dataFirst
+ * @dataLast
  * @category Object
  */
 export function setPath<TPath extends Array<PropertyKey>, Value>(
@@ -41,17 +41,13 @@ export function setPath() {
   return purry(_setPath, arguments);
 }
 
-export function _setPath(
-  object: any,
-  path: Array<any>,
-  defaultValue: any
-): any {
-  if (path.length === 0) return defaultValue;
+export function _setPath(object: any, path: Array<any>, value: any): any {
+  if (path.length === 0) return value;
 
   if (Array.isArray(object)) {
     return object.map((item, index) => {
       if (index === path[0]) {
-        return _setPath(item, path.slice(1), defaultValue);
+        return _setPath(item, path.slice(1), value);
       }
       return item;
     });
@@ -59,6 +55,6 @@ export function _setPath(
 
   return {
     ...object,
-    [path[0]]: _setPath(object[path[0]], path.slice(1), defaultValue),
+    [path[0]]: _setPath(object[path[0]], path.slice(1), value),
   };
 }
