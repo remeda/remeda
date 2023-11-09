2ce0a9508f32100c6136944d4f437f78eac70da4
Neef Rehman <neef.rehman@gmail.com>
2023-11-09 17:04:22 +0000

    add indexBy.strict, change return type to be partial

--- src/indexBy.test.ts
+++ src/indexBy.test.ts
@@ -6,17 +6,30 @@ const array = [
   { dir: 'right', code: 100 },
 ] as const;
 const expected = {
-  left: { dir: 'left', code: 97 },
-  right: { dir: 'right', code: 100 },
+  '97': { dir: 'left', code: 97 },
+  '100': { dir: 'right', code: 100 },
 };
+const expectedStrict = {
+  97: { dir: 'left', code: 97 },
+  100: { dir: 'right', code: 100 },
+};
+type ExpectedTypeStrict = Partial<
+  Record<97 | 100, { dir: 'left' | 'right'; code: 97 | 100 }>
+>;
 
 describe('data first', () => {
   test('indexBy', () => {
-    expect(indexBy(array, x => x.dir)).toEqual(expected);
+    expect(indexBy(array, x => x.code)).toEqual(expected);
   });
 
   test('indexBy.indexed', () => {
-    expect(indexBy.indexed(array, x => x.dir)).toEqual(expected);
+    expect(indexBy.indexed(array, x => x.code)).toEqual(expected);
+  });
+
+  test('indexBy.strict', () => {
+    const result = indexBy.strict(array, x => x.code);
+    assertType<ExpectedTypeStrict>(result);
+    expect(result).toStrictEqual(expectedStrict);
   });
 });
 
@@ -25,7 +38,7 @@ describe('data last', () => {
     expect(
       pipe(
         array,
-        indexBy(x => x.dir)
+        indexBy(x => x.code)
       )
     ).toEqual(expected);
   });
@@ -34,8 +47,17 @@ describe('data last', () => {
     expect(
       pipe(
         array,
-        indexBy.indexed(x => x.dir)
+        indexBy.indexed(x => x.code)
       )
     ).toEqual(expected);
+  });
+
+  test('indexBy.strict', () => {
+    const result = pipe(
+      array,
+      indexBy.strict(x => x.code)
+    );
+    assertType<ExpectedTypeStrict>(result);
+    expect(result).toEqual(expectedStrict);
   });
 });
--- src/indexBy.ts
+++ src/indexBy.ts
@@ -2,40 +2,55 @@ import { purry } from './purry';
 import { PredIndexedOptional, PredIndexed } from './_types';
 
 /**
- * Converts a list of objects into an object indexing the objects by the given key.
+ * Converts a list of objects into an object indexing the objects by the given key
+ * (casted to a string). Use the strict version to maintain the given key's type, so
+ * long as it is a valid `PropertyKey`.
+ *
  * @param array the array
  * @param fn the indexing function
  * @signature
  *    R.indexBy(array, fn)
+ *    R.indexBy.strict(array, fn)
  * @example
- *    R.indexBy(['one', 'two', 'three'], x => x.length) // => {3: 'two', 5: 'three'}
+ *    R.indexBy(['one', 'two', 'three'], x => x.length) // => {"3": 'two', "5": 'three'}
+ *    R.indexBy.strict(['one', 'two', 'three'], x => x.length) // => {3: 'two', 5: 'three'}
  * @dataFirst
  * @indexed
  * @category Array
+ * @strict
  */
-export function indexBy<K extends PropertyKey, T>(
+export function indexBy<T>(
   array: ReadonlyArray<T>,
-  fn: (item: T) => K
-): Record<K, T>;
+  fn: (item: T) => any
+): Record<string, T>;
 
 /**
  * Converts a list of objects into an object indexing the objects by the given key.
+ * (casted to a string). Use the strict version to maintain the given key's type, so
+ * long as it is a valid `PropertyKey`.
+ *
  * @param array the array
  * @param fn the indexing function
  * @signature
  *    R.indexBy(fn)(array)
+ *    R.indexBy.strict(fn)(array)
  * @example
  *    R.pipe(
  *      ['one', 'two', 'three'],
  *      R.indexBy(x => x.length)
+ *    ) // => {"3": 'two', "5": 'three'}
+ *    R.pipe(
+ *      ['one', 'two', 'three'],
+ *      R.indexBy.strict(x => x.length)
  *    ) // => {3: 'two', 5: 'three'}
  * @dataLast
  * @indexed
  * @category Array
+ * @strict
  */
-export function indexBy<K extends PropertyKey, T>(
-  fn: (item: T) => K
-): (array: ReadonlyArray<T>) => Record<K, T>;
+export function indexBy<T>(
+  fn: (item: T) => any
+): (array: ReadonlyArray<T>) => Record<string, T>;
 
 export function indexBy() {
   return purry(_indexBy(false), arguments);
@@ -43,16 +58,39 @@ export function indexBy() {
 
 const _indexBy =
   (indexed: boolean) =>
+  <T>(array: Array<T>, fn: PredIndexedOptional<T, any>) => {
+    return array.reduce<Record<string, T>>((ret, item, index) => {
+      const value = indexed ? fn(item, index, array) : fn(item);
+      const key = String(value);
+      ret[key] = item;
+      return ret;
+    }, {});
+  };
+
+export function indexByStrict<K extends PropertyKey, T>(
+  array: ReadonlyArray<T>,
+  fn: (item: T) => K
+): Partial<Record<K, T>>;
+
+export function indexByStrict<K extends PropertyKey, T>(
+  fn: (item: T) => K
+): (array: ReadonlyArray<T>) => Partial<Record<K, T>>;
+
+export function indexByStrict() {
+  return purry(_indexByStrict(false), arguments);
+}
+
+const _indexByStrict =
+  (indexed: boolean) =>
   <K extends PropertyKey, T>(
     array: Array<T>,
     fn: PredIndexedOptional<T, K>
   ) => {
-    const object = array.reduce<Partial<Record<K, T>>>((ret, item, index) => {
+    return array.reduce<Partial<Record<K, T>>>((ret, item, index) => {
       const key = indexed ? fn(item, index, array) : fn(item);
       ret[key] = item;
       return ret;
     }, {});
-    return object as Record<K, T>;
   };
 
 export namespace indexBy {
@@ -66,4 +104,5 @@ export namespace indexBy {
   export function indexed() {
     return purry(_indexBy(true), arguments);
   }
+  export const strict = indexByStrict;
 }
