f23f486ea4181d967e665e40d9a1b3f10836aa98
Eran Hirsch <eranhirsch@gmail.com>
2023-12-06 11:39:33 +0200

    nit for marked 11

--- docs/scripts/transform.ts
+++ docs/scripts/transform.ts
@@ -116,7 +116,10 @@ async function transformFunction({
   const description =
     summary.length === 0
       ? undefined
-      : markedParse(summary.map(({ text }) => text).join(''), MARKED_OPTIONS);
+      : (markedParse(
+          summary.map(({ text }) => text).join(''),
+          MARKED_OPTIONS
+        ) as string);
 
   const methods = await Promise.all(
     signaturesWithComments.map(transformSignature)
@@ -175,12 +178,12 @@ function getReturnType(type: JSONOutput.SomeType | undefined) {
   return type === undefined
     ? 'Object'
     : type.type === 'intrinsic'
-    ? type.name
-    : type.type === 'array'
-    ? 'Array'
-    : type.type === 'predicate'
-    ? 'boolean'
-    : 'Object';
+      ? type.name
+      : type.type === 'array'
+        ? 'Array'
+        : type.type === 'predicate'
+          ? 'boolean'
+          : 'Object';
 }
 
 async function getExample(
