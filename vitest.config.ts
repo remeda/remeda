dfaefd49d09157ebaa77851ae7c0dcc20350b00e
Eran Hirsch <eranhirsch@gmail.com>
2023-12-06 23:11:02 +0200

    chore: define files for coverage repors (#435)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      include: ['src/*.ts'],
+      exclude: ['src/index.ts', 'src/_*.ts'],
+    },
   },
 });
