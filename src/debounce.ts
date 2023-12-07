6fe0ac92e7a993515d0371b7dcb3005726010cdb
Eran Hirsch <eranhirsch@gmail.com>
2023-12-07 11:17:40 +0200

    more

--- src/debounce.ts
+++ src/debounce.ts
@@ -45,8 +45,10 @@ type DebounceOptions = {
 };
 
 /**
- * Wraps `func` with a debouncer object that "debounces" (delays) invocations of the function during a defined cool-down period (`waitMs`). The debouncer can be configured to invoke the function either at the start of the cool-down period, the end of it, or at both ends. It can also be configured to allow invocations during the cool-down period (`maxWaitMs`). The debouncer stores the latest arguments provided to the call function so they could be used at the end of the cool-down period when invoking `func` (if configured to invoke the function at the end of the cool-down period).
- * The debouncer stores the value returned by `func` whenever it's invoked. This value is returned on every call, and is accessible via the `cachedValue` property of the debouncer. It's important to note that the value might be different from the value that would be returned from running `func` with the current arguments as it's a cached value from a previous invocation.
+ * Wraps `func` with a debouncer object that "debounces" (delays) invocations of the function during a defined cool-down period (`waitMs`). It can be configured to invoke the function either at the start of the cool-down period, the end of it, or at both ends (`timing`).
+ * It can also be configured to allow invocations during the cool-down period (`maxWaitMs`).
+ * It stores the latest call's arguments so they could be used at the end of the cool-down period when invoking `func` (if configured to invoke the function at the end of the cool-down period).
+ * It stores the value returned by `func` whenever it's invoked. This value is returned on every call, and is accessible via the `cachedValue` property of the debouncer. It's important to note that the value might be different from the value that would be returned from running `func` with the current arguments as it's a cached value from a previous invocation.
  * **Important**: The cool-down period defines the minimum between two invocations, and not the maximum. The period will be **extended** each time a call is made until a full cool-down period has elapsed without any additional calls.
  * @param func The function to debounce, the returned `call` function will have
  * the exact same signature.
