e701bf7b48672239330b498c6a0b393fee5b829e
Eran Hirsch <eranhirsch@gmail.com>
2023-12-11 13:53:47 +0200

    dataLast

--- src/firstBy.test.ts
+++ src/firstBy.test.ts
@@ -1,8 +1,9 @@
 import { NonEmptyArray } from './_types';
 import { firstBy } from './firstBy';
 import { identity } from './identity';
+import { pipe } from './pipe';
 
-describe('runtime', () => {
+describe('runtime (dataFirst)', () => {
   it('returns undefined on empty', () => {
     expect(firstBy([], identity)).toBeUndefined();
   });
@@ -57,6 +58,79 @@ describe('runtime', () => {
     expect(firstBy([new Date(), new Date(1), new Date(2)], identity)).toEqual(
       new Date(1)
     );
+  });
+});
+
+describe('runtime (dataLast)', () => {
+  it('returns undefined on empty', () => {
+    expect(pipe([], firstBy(identity))).toBeUndefined();
+  });
+
+  it('returns the item on a single item array', () => {
+    expect(pipe([1], firstBy(identity))).toBe(1);
+  });
+
+  it('finds the minimum', () => {
+    expect(pipe([2, 1, 4, 3, 5], firstBy(identity))).toBe(1);
+  });
+
+  it('finds the minimum with a non-trivial order rule', () => {
+    expect(
+      pipe(
+        ['aa', 'a', 'aaaa', 'aaa', 'aaaaa'],
+        firstBy(x => x.length)
+      )
+    ).toBe('a');
+  });
+
+  it("finds the max with 'desc' order rules", () => {
+    expect(pipe([2, 1, 4, 3, 5], firstBy([identity, 'desc']))).toBe(5);
+  });
+
+  it("finds the max with non-trivial 'desc' order rules", () => {
+    expect(
+      pipe(['aa', 'a', 'aaaa', 'aaa', 'aaaaa'], firstBy([identity, 'desc']))
+    ).toBe('aaaaa');
+  });
+
+  it('breaks ties with multiple order rules', () => {
+    const data = ['a', 'bb', 'b', 'aaaa', 'bbb', 'aa', 'aaa', 'bbbb'];
+    expect(
+      pipe(
+        data,
+        firstBy(x => x.length, identity)
+      )
+    ).toBe('a');
+    expect(pipe(data, firstBy([x => x.length, 'desc'], identity))).toBe('aaaa');
+    expect(
+      pipe(
+        data,
+        firstBy(x => x.length, [identity, 'desc'])
+      )
+    ).toBe('b');
+    expect(
+      pipe(data, firstBy([(x: string) => x.length, 'desc'], [identity, 'desc']))
+    ).toBe('bbbb');
+  });
+
+  it('can compare strings', () => {
+    expect(pipe(['b', 'a', 'c'], firstBy(identity))).toBe('a');
+  });
+
+  it('can compare numbers', () => {
+    expect(pipe([2, 1, 3], firstBy(identity))).toBe(1);
+  });
+
+  it('can compare booleans', () => {
+    expect(pipe([true, false, true, true, false], firstBy(identity))).toBe(
+      false
+    );
+  });
+
+  it('can compare valueOfs', () => {
+    expect(
+      pipe([new Date(), new Date(1), new Date(2)], firstBy(identity))
+    ).toEqual(new Date(1));
   });
 });
 
