1a8114a2d3682a51634ff199242ddeb4957cee04
Luca Barone <baro.luc@gmail.com>
2023-12-12 10:24:04 +0100

    type(toPairs): Improve toPairs.strict typings (#410)

--- src/toPairs.ts
+++ src/toPairs.ts
@@ -1,5 +1,3 @@
-import { ObjectKeys } from './_types';
-
 /**
  * Returns an array of key/values of the enumerable properties of an object.
  * @param object
@@ -16,22 +14,12 @@ export function toPairs<T>(object: Record<string, T>): Array<[string, T]> {
   return Object.entries(object);
 }
 
-// Inspired and largely copied from `sindresorhus/ts-extras`:
-// @see https://github.com/sindresorhus/ts-extras/blob/44f57392c5f027268330771996c4fdf9260b22d6/source/object-entries.ts
-type ObjectValues<T extends Record<PropertyKey, unknown>> =
-  Required<T>[ObjectKeys<T>];
-type ObjectEntry<T extends Record<PropertyKey, unknown>> = [
-  ObjectKeys<T>,
-  ObjectValues<T>,
-];
-type ObjectEntries<T extends Record<PropertyKey, unknown>> = Array<
-  ObjectEntry<T>
+type Pairs<T> = Array<
+  { [K in keyof T]-?: [key: K, value: Required<T>[K]] }[keyof T]
 >;
 
 export namespace toPairs {
-  export function strict<T extends Record<PropertyKey, unknown>>(
-    object: T
-  ): ObjectEntries<T> {
+  export function strict<T extends NonNullable<unknown>>(object: T): Pairs<T> {
     // @ts-expect-error [ts2322] - This is deliberately stricter than what TS
     // provides out of the box.
     return Object.entries(object);
