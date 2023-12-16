72bcb84bbd2379a4de00aeeffe8fd6c2e0c1de03
Eran Hirsch <eranhirsch@gmail.com>
2023-12-16 22:26:22 +0200

    nit

--- src/isObjectType.ts
+++ src/isObjectType.ts
@@ -1,9 +1,9 @@
 import type { NarrowedTo } from './_types';
 
 /**
- * Checks if the given parameter is of type `"object"` in JavaScript runtime (via `typeof`), excluding `null`.
+ * Checks if the given parameter is of type `"object"` via `typeof`, excluding `null`.
  *
- * It's important to note that in JavaScript, many entities are considered objects, like Arrays, Classes, RegExps, Maps, Sets, Dates, URLs, Promise, Errors, and more. Although technically an object too, `null` is not considered an object by this function, so that its easier to narrow nullable.
+ * It's important to note that in JavaScript, many entities are considered objects, like Arrays, Classes, RegExps, Maps, Sets, Dates, URLs, Promise, Errors, and more. Although technically an object too, `null` is not considered an object by this function, so that its easier to narrow nullables.
  *
  * For a more specific check that is limited to plain objects (simple struct/shape/record-like objects), consider using `isPlainObject` instead. For a simpler check that only removes `null` from the type prefer `isNonNull` or `isDefined`.
  *
