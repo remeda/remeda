450169eaad215afd4dfa02bd492fb90043cbb520
Eran Hirsch <eranhirsch@gmail.com>
2023-12-16 22:21:44 +0200

    fixed

--- src/isObject.ts
+++ src/isObject.ts
@@ -12,7 +12,7 @@ type DefinitelyObject<T> = Exclude<
 /**
  * A function that checks if the passed parameter is of type Object and narrows its type accordingly
  *
- * @deprecated this guard is confusing because it rejects arrays but doesn't reject classes or built-in objects. Instead, use `isObjectType` for a broader check for anything "object", or `isPlainObject` for simple struct/shape/record-like objects. You can also achieve the same logic with `(x) => isObjectType(x) && !isArray(x)`.
+ * ! **DEPRECATED: The semantics of this guard are confusing. It excludes Arrays but does not exclude other complex objects like Classes or built-in objects (such as Date, Promise, Error, etc.). Instead, consider using `isObjectType` for a more inclusive validation encompassing any JavaScript "object" type, or `isPlainObject` for a more specific validation targeting simple struct/shape/record-like objects. To replicate the existing logic, use: `const isObject = (x) => isObjectType(x) && !Array.isArray(x)`.** !
  *
  * @param data the variable to check
  * @signature
@@ -25,6 +25,7 @@ type DefinitelyObject<T> = Exclude<
  *    R.isObject(new Error("error")) //=> true
  *    R.isObject('somethingElse') //=> false
  * @category Guard
+ * @deprecated - The semantics of this guard are confusing. It excludes Arrays but does not exclude other complex objects like Classes or built-in objects (such as Date, Promise, Error, etc.). Instead, consider using `isObjectType` for a more inclusive validation encompassing any JavaScript "object" type, or `isPlainObject` for a more specific validation targeting simple struct/shape/record-like objects. To replicate the existing logic, use: `const isObject = (x) => isObjectType(x) && !Array.isArray(x)`.
  */
 export function isObject<T>(data: T | object): data is DefinitelyObject<T> {
   return !!data && !Array.isArray(data) && typeof data === 'object';
