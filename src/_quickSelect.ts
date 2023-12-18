212f28e53dad39d76756e03d48334312687ba777
Eran Hirsch <eranhirsch@gmail.com>
2023-12-18 09:50:30 +0200

    docs

--- src/_quickSelect.ts
+++ src/_quickSelect.ts
@@ -1,6 +1,22 @@
+/**
+ * A simple implementation of the *QuickSelect* algorithm.
+ * @see https://en.wikipedia.org/wiki/Quickselect
+ */
+
 import { swapInPlace } from './_swapInPlace';
 import type { CompareFunction } from './_types';
 
+/**
+ * Perform QuickSelect on the given data. Notice that the data would be cloned
+ * shallowly so that it could be mutated in-place, and then discarded once the
+ * algorithm is done. This means that running this function multiple times on
+ * the same array might be slower then sorting the array before.
+ * @param data - The data to perform the selection on.
+ * @param index - The index of the item we are looking for.
+ * @param compareFn - The compare function to use for sorting.
+ * @returns The item at the given index, or `undefined` if the index is out-of-
+ * bounds.
+ */
 export const quickSelect = <T>(
   data: ReadonlyArray<T>,
   index: number,
@@ -18,6 +34,9 @@ export const quickSelect = <T>(
         compareFn
       );
 
+/**
+ * The actual implementation, called recursively.
+ */
 function quickSelectImplementation<T>(
   data: Array<T>,
   left: number,
