6079d6f5d7868f6ffd299a191f7e34d868777035
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-12-20 14:54:07 +0200

    test(takeLastWhile): Use inline params, ensure array is cloned

--- src/takeLastWhile.test.ts
+++ src/takeLastWhile.test.ts
@@ -1,23 +1,24 @@
 import { pipe } from './pipe';
 import { takeLastWhile } from './takeLastWhile';
 
-const DATA = [1, 2, 3, 4] as const;
-
 describe('data first', () => {
   it('should return items after the last predicate failure', () => {
-    expect(takeLastWhile(DATA, n => n !== 2)).toStrictEqual([3, 4]);
+    expect(takeLastWhile([1, 2, 3, 4], n => n !== 2)).toStrictEqual([3, 4]);
   });
 
   it('should return an empty array when the last item fails the predicate', () => {
-    expect(takeLastWhile(DATA, n => n !== 4)).toStrictEqual([]);
+    expect(takeLastWhile([1, 2, 3, 4], n => n !== 4)).toStrictEqual([]);
   });
 
   it('should return an empty array when an empty array is passed', () => {
     expect(takeLastWhile([], n => n > 0)).toStrictEqual([]);
   });
 
-  it('should return the original array when all items pass the predicate', () => {
-    expect(takeLastWhile(DATA, n => n > 0)).toStrictEqual(DATA);
+  it('should return a copy of the original array when all items pass the predicate', () => {
+    const data = [1, 2, 3, 4];
+    const result = takeLastWhile(data, n => n > 0);
+    expect(result).toStrictEqual(data);
+    expect(result).not.toBe(data);
   });
 });
 
@@ -25,7 +26,7 @@ describe('data last', () => {
   it('should return items after the last predicate failure', () => {
     expect(
       pipe(
-        DATA,
+        [1, 2, 3, 4],
         takeLastWhile(n => n !== 2)
       )
     ).toStrictEqual([3, 4]);
@@ -34,7 +35,7 @@ describe('data last', () => {
   it('should return an empty array when the last item fails the predicate', () => {
     expect(
       pipe(
-        DATA,
+        [1, 2, 3, 4],
         takeLastWhile(n => n !== 4)
       )
     ).toStrictEqual([]);
@@ -49,12 +50,13 @@ describe('data last', () => {
     ).toStrictEqual([]);
   });
 
-  it('should return the original array when all items pass the predicate', () => {
-    expect(
-      pipe(
-        DATA,
-        takeLastWhile(n => n > 0)
-      )
-    ).toStrictEqual(DATA);
+  it('should return a copy of the original array when all items pass the predicate', () => {
+    const data = [1, 2, 3, 4];
+    const result = pipe(
+      data,
+      takeLastWhile(n => n > 0)
+    );
+    expect(result).toStrictEqual(data);
+    expect(result).not.toBe(data);
   });
 });
