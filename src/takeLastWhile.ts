7250d614c3b12124b3b9bf9c6778b634a21b870b
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-12-20 14:31:07 +0200

    refactor(takeLastWhile): Rename params to data and predicate

--- src/takeLastWhile.ts
+++ src/takeLastWhile.ts
@@ -3,44 +3,44 @@ import { purry } from './purry';
 /**
  * Returns elements from the end of the array until the predicate returns false.
  *
- * @param array the array
- * @param fn the predicate
+ * @param data the array
+ * @param predicate the predicate
  * @signature
- *    R.takeLastWhile(array, fn)
+ *    R.takeLastWhile(data, predicate)
  * @example
  *    R.takeLastWhile([1, 2, 3, 4, 3, 2, 1], x => x !== 4) // => [3, 2, 1]
  * @dataFirst
  * @category Array
  */
 export function takeLastWhile<T>(
-  array: ReadonlyArray<T>,
-  fn: (item: T) => boolean
+  data: ReadonlyArray<T>,
+  predicate: (item: T) => boolean
 ): Array<T>;
 
 /**
  * Returns elements from the end of the array until the predicate returns false.
  *
- * @param fn the predicate
+ * @param predicate the predicate
  * @signature
- *    R.takeLastWhile(fn)(array)
+ *    R.takeLastWhile(predicate)(data)
  * @example
  *    R.pipe([1, 2, 3, 4, 3, 2, 1], R.takeLastWhile(x => x !== 4))  // => [3, 2, 1]
  * @dataLast
  * @category Array
  */
 export function takeLastWhile<T>(
-  fn: (item: T) => boolean
-): (array: ReadonlyArray<T>) => Array<T>;
+  predicate: (item: T) => boolean
+): (data: ReadonlyArray<T>) => Array<T>;
 
 export function takeLastWhile() {
   return purry(_takeLastWhile, arguments);
 }
 
-function _takeLastWhile<T>(array: Array<T>, fn: (item: T) => boolean) {
-  for (let i = array.length - 1; i >= 0; i--) {
-    if (!fn(array[i])) {
-      return array.slice(i + 1);
+function _takeLastWhile<T>(data: Array<T>, predicate: (item: T) => boolean) {
+  for (let i = data.length - 1; i >= 0; i--) {
+    if (!predicate(data[i])) {
+      return data.slice(i + 1);
     }
   }
-  return array;
+  return data;
 }
