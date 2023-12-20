636b2e0178a015f084e2e681a2831fda828142b4
Pekka Maanpää <pekka.maanpaa@gmail.com>
2023-12-20 14:38:40 +0200

    docs(takeLastWhile): Mention the returned array order

--- src/takeLastWhile.ts
+++ src/takeLastWhile.ts
@@ -2,13 +2,14 @@ import { purry } from './purry';
 
 /**
  * Returns elements from the end of the array until the predicate returns false.
+ * The returned elements will be in the same order as in the original array.
  *
  * @param data the array
  * @param predicate the predicate
  * @signature
  *    R.takeLastWhile(data, predicate)
  * @example
- *    R.takeLastWhile([1, 2, 3, 4, 3, 2, 1], x => x !== 4) // => [3, 2, 1]
+ *    R.takeLastWhile([1, 2, 10, 3, 4, 5], x => x < 10) // => [3, 4, 5]
  * @dataFirst
  * @category Array
  */
@@ -19,12 +20,13 @@ export function takeLastWhile<T>(
 
 /**
  * Returns elements from the end of the array until the predicate returns false.
+ * The returned elements will be in the same order as in the original array.
  *
  * @param predicate the predicate
  * @signature
  *    R.takeLastWhile(predicate)(data)
  * @example
- *    R.pipe([1, 2, 3, 4, 3, 2, 1], R.takeLastWhile(x => x !== 4))  // => [3, 2, 1]
+ *    R.pipe([1, 2, 10, 3, 4, 5], R.takeLastWhile(x => x < 10))  // => [3, 4, 5]
  * @dataLast
  * @category Array
  */
