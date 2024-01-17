d371a721f74ed3a72ba2186823cb73675fa07d06
Paweł Dąbrowski <tuptus95@gmail.com>
2024-01-17 15:19:00 +0100

    feat(round, ceil, floor): Add round, ceil, floor functions (#463)
    Includes implementation of `round`, `ceil` and `floor` functions. I need
    these (well, mostly `round` but the other two are trivial to implement
    once you have one) to migrate off of `lodash`, hence this PR.
    
    The main difference between these and native functions from the `Math`
    object is the second argument `precision`. I implemented it in the
    simplest way possible which I believe is correct for most actual use
    cases. Still, there are certainly one-off rounding issues caused by
    floating-point nature of `number` type in the language.
    
    There's one pretty significant difference from `lodash` - `precision` is
    limited to values from -15 to 15 as higher precision doesn't really make
    any sense, for example, `Math.PI` has only 15 decimal places. Similar
    function implemented in C# native library has a similar validation rule
    (0 to 15, I believe negative precision is pretty useful in some
    scenarios), [link to their docs for
    reference](https://learn.microsoft.com/en-us/dotnet/api/system.math.round?view=net-8.0#system-math-round(system-double-system-int32)).
    Happy to discuss your concerns regarding this set of functions. 
    
    Also, closes #462
    
    ---
    
    Make sure that you:
    
    - [x] Typedoc added for new methods and updated for changed
    - [x] Tests added for new methods and updated for changed
    - [x] New methods added to `src/index.ts`
    - [x] New methods added to `mapping.md`
    
    ---
    
    <details><summary>We use semantic PR titles to automate the release
    process!</summary>
    
    https://conventionalcommits.org
    
    PRs should be titled following using the format: `< TYPE >(< scope >)?:
    description`
    
    ### Available Types:
    
    - `feat`: new functions, and changes to a function's type that would
    impact users.
    - `fix`: changes to the runtime behavior of an existing function, or
    refinements to it's type that shouldn't impact most users.
    - `perf`: changes to function implementations that improve a functions
    _runtime_ performance.
    - `refactor`: changes to function implementations that are neither `fix`
    nor `perf`
    - `test`: tests-only changes (transparent to users of the function).
    - `docs`: changes to the documentation of a function **or the
    documentation site**.
    - `build`, `ci`, `style`, `chore`, and `revert`: are only relevant for
    the internals of the library.
    
    For scope put the name of the function you are working on (either new or
    existing).
    
    </details>

---
+++ src/ceil.ts
@@ -0,0 +1,42 @@
+import { _withPrecision } from './_withPrecision';
+import { purry } from './purry';
+
+/**
+ * Rounds up a given number to a specific precision.
+ * If you'd like to round up to an integer (i.e. use this function with constant `precision === 0`),
+ * use `Math.ceil` instead, as it won't incur the additional library overhead.
+ * @param value The number to round up.
+ * @param precision The precision to round up to. Must be an integer between -15 and 15.
+ * @signature
+ *    R.ceil(value, precision);
+ * @example
+ *    R.ceil(123.9876, 3) // => 123.988
+ *    R.ceil(483.22243, 1) // => 483.3
+ *    R.ceil(8541, -1) // => 8550
+ *    R.ceil(456789, -3) // => 457000
+ * @dataFirst
+ * @category Number
+ */
+export function ceil(value: number, precision: number): number;
+
+/**
+ * Rounds up a given number to a specific precision.
+ * If you'd like to round up to an integer (i.e. use this function with constant `precision === 0`),
+ * use `Math.ceil` instead, as it won't incur the additional library overhead.
+ * @param value The number to round up.
+ * @param precision The precision to round up to. Must be an integer between -15 and 15.
+ * @signature
+ *    R.ceil(precision)(value);
+ * @example
+ *    R.ceil(3)(123.9876) // => 123.988
+ *    R.ceil(1)(483.22243) // => 483.3
+ *    R.ceil(-1)(8541) // => 8550
+ *    R.ceil(-3)(456789) // => 457000
+ * @dataLast
+ * @category Number
+ */
+export function ceil(precision: number): (value: number) => number;
+
+export function ceil() {
+  return purry(_withPrecision(Math.ceil), arguments);
+}
