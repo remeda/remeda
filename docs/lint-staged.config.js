05b72244c0e83eb9ac5d9f82ecabeb3316329d0c
Eran Hirsch <eranhirsch@gmail.com>
2024-02-07 11:40:24 +0200

    with lint-staged

--- docs/lint-staged.config.js
+++ docs/lint-staged.config.js
@@ -6,7 +6,7 @@ export default {
   "*.ts?(x)": () => "tsc -p tsconfig.json --noEmit",
 
   // Javascript and Typescript (including commonJs and esm variants)
-  "*.@(js|jsx|ts|tsx|cjs|mjs)": ["prettier --write"],
+  "*.@(js|jsx|ts|tsx|cjs|mjs)": ["eslint --fix", "prettier --write"],
 
   // _try_ prettier on anything, it supports a lot of things! If this is touching a file
   // you don't want add it to .prettierignore.
