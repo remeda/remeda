75bed89540001df8516f54d4a464c312d1ee98c8
Eran Hirsch <eranhirsch@gmail.com>
2024-02-07 12:11:30 +0200

    more rules

--- docs/.eslintrc.cjs
+++ docs/.eslintrc.cjs
@@ -2,10 +2,7 @@
 module.exports = {
   root: true,
 
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
     ecmaVersion: "latest",
     sourceType: "module",
   },
@@ -16,22 +13,16 @@ module.exports = {
     node: true,
   },
 
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ["@typescript-eslint", "react", "jsx-a11y", "astro"],
 
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/strict-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-strict",
     "prettier",
   ],
 
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  reportUnusedDisableDirectives: true,
 
   rules: {
     // We stay consistent with the main project on this...
@@ -40,12 +31,42 @@ module.exports = {
 
   overrides: [
     {
+      files: [".eslintrc.{js,cjs}"],
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
         sourceType: "script",
+      },
+    },
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+
+      extends: [
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+      ],
+
+      settings: {
+        react: {
+          version: "detect",
+        },
       },
     },
   ],
