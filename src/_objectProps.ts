eda7ee8af5cd41a4c171f26e07737fafc3080ff9
Aaron Pettengill <pettengill.aaron@gmail.com>
2024-02-17 23:17:18 -0500

    fix: update prop utility types to pass tests

--- src/_objectProps.ts
+++ src/_objectProps.ts
@@ -3,7 +3,11 @@ import { Simplify } from './type-fest/simplify';
 export type AllUnionKeys<Union> = Union extends Union ? keyof Union : never;
 
 type MakeKeyRequired<Obj, Prop extends keyof Obj> = Obj & {
-  [key in Prop]-?: Obj[key];
+  // We need to both remove the optional modifier and exclude undefined.
+  // This is because the `-?` modifier will only exclude undefined from the type if the property is optional,
+  // which makes the behavior of the type inconsistent.
+  // So excluding `undefined` explicitly also does this for required properties that include undefined.
+  [key in Prop]-?: Exclude<Obj[key], undefined>;
 };
 
 export type WithRequiredProp<Obj, Prop extends AllUnionKeys<Obj>> = Simplify<
