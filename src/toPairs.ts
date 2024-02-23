73ac34e0a7057e5d9f008a39e2b83ff96bc1047a
Eran Hirsch <eranhirsch@gmail.com>
2024-02-23 10:50:10 +0200

    add proper toPairs strict

--- src/toPairs.ts
+++ src/toPairs.ts
@@ -1,3 +1,5 @@
+import { purry } from './purry';
+
 /**
  * Returns an array of key/values of the enumerable properties of an object.
  * @param object
@@ -9,8 +11,38 @@
  *    R.toPairs.strict({ a: 1 } as const) // => [['a', 1]] typed Array<['a', 1]>
  * @strict
  * @category Object
+ * @dataFirst
  */
-export function toPairs<T>(object: Record<string, T>): Array<[string, T]> {
+export function toPairs<T>(object: Record<string, T>): Array<[string, T]>;
+
+/**
+ * Returns an array of key/values of the enumerable properties of an object.
+ * @param object
+ * @signature
+ *    R.toPairs()(object)
+ *    R.toPairs.strict()(object)
+ * @example
+ *    R.pipe(
+ *      { a: 1, b: 2, c: 3 },
+ *      toPairs(),
+ *    ); // => [['a', 1], ['b', 2], ['c', 3]]
+ *    R.pipe(
+ *      { a: 1 } as const,
+ *      toPairs.strict(),
+ *    ); // => [['a', 1]] typed Array<['a', 1]>
+ * @strict
+ * @category Object
+ * @dataLast
+ */
+export function toPairs(): <T>(object: Record<string, T>) => Array<[string, T]>;
+
+export function toPairs() {
+  return purry(toPairsImplementation, arguments);
+}
+
+function toPairsImplementation<T>(
+  object: Record<string, T>
+): Array<[string, T]> {
   return Object.entries(object);
 }
 
@@ -18,10 +50,11 @@ type Pairs<T> = Array<
   { [K in keyof T]-?: [key: K, value: Required<T>[K]] }[keyof T]
 >;
 
+type Strict = {
+  <T extends NonNullable<unknown>>(object: T): Pairs<T>;
+  (): <T extends NonNullable<unknown>>(object: T) => Pairs<T>;
+};
+
 export namespace toPairs {
-  export function strict<T extends NonNullable<unknown>>(object: T): Pairs<T> {
-    // @ts-expect-error [ts2322] - This is deliberately stricter than what TS
-    // provides out of the box.
-    return Object.entries(object);
-  }
+  export const strict = toPairsImplementation as Strict;
 }
