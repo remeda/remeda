d37e1ad75f7764c3a5c18601cb2e702cd67e9039
Eran Hirsch <eranhirsch@gmail.com>
2024-02-23 10:38:41 +0200

    fix purryOn

--- src/_purryOn.ts
+++ src/_purryOn.ts
@@ -6,15 +6,18 @@
 export function purryOn<T>(
   isArg: (firstArg: unknown) => firstArg is T,
   implementation: (
+    data: unknown,
     firstArg: T,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Function inference in typescript relies on `any` to work, it doesn't work with `unknown`
-    ...args: Array<any>
+    ...args: any
   ) => unknown,
   args: IArguments
 ): unknown {
-  const callArgs = Array.from(args);
-  const [dataOrArg, ...rest] = callArgs;
-  return isArg(dataOrArg)
-    ? (data: T) => implementation(data, ...callArgs)
-    : implementation(dataOrArg, ...rest);
+  // TODO: Once we bump our target beyond ES5 we can spread the args array directly and don't need this...
+  const callArgs = Array.from(args) as ReadonlyArray<unknown>;
+  return isArg(callArgs[0])
+    ? // @ts-expect-error [ts2556] - This is a low-level function that assumes the function declaration and setup is correct and won't result in typing issues when called dynamically.
+      (data: unknown) => implementation(data, ...callArgs)
+    : // @ts-expect-error [ts2556] - This is a low-level function that assumes the function declaration and setup is correct and won't result in typing issues when called dynamically.
+      implementation(...callArgs);
 }
