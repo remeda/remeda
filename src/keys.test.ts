6a6689c556d43a7d5f13f3eeb5a7daa2405f7ea7
Eran Hirsch <eranhirsch@gmail.com>
2024-02-23 14:51:36 +0200

    some more tests

--- src/keys.test.ts
+++ src/keys.test.ts
@@ -1,32 +1,56 @@
 import { keys } from './keys';
 import { pipe } from './pipe';
 
-describe('Test for keys', () => {
-  it('should return keys of array', () => {
-    expect(keys(['x', 'y', 'z'])).toEqual(['0', '1', '2']);
-  });
-
-  it('should return keys of object', () => {
-    expect(keys({ a: 'x', b: 'y', c: 'z' })).toEqual(['a', 'b', 'c']);
-  });
-
-  describe('strict', () => {
-    it('should return strict types', () => {
-      const actual = keys.strict({ 5: 'x', b: 'y', c: 'z' } as const);
-      expect(actual).toEqual(['5', 'b', 'c']);
-
-      assertType<Array<'5' | 'b' | 'c'>>(actual);
+describe('runtime', () => {
+  describe('dataFirst', () => {
+    it('work with arrays', () => {
+      expect(keys(['x', 'y', 'z'])).toEqual(['0', '1', '2']);
     });
 
-    it('should work with Partial in pipe', () => {
-      const data: Partial<{ foo: string; bar?: number }> = {
-        foo: '1',
-        bar: 7,
-      };
-      const actual = pipe(data, keys.strict());
-      expect(actual).toEqual(['foo', 'bar']);
+    it('work with objects', () => {
+      expect(keys({ a: 'x', b: 'y', c: 'z' })).toEqual(['a', 'b', 'c']);
+    });
 
-      assertType<Array<'foo' | 'bar'>>(actual);
+    it('should return strict types', () => {
+      expect(keys.strict({ 5: 'x', b: 'y', c: 'z' } as const)).toEqual([
+        '5',
+        'b',
+        'c',
+      ]);
+    });
+
+    describe('dataLast', () => {
+      it('work with arrays', () => {
+        expect(keys()(['x', 'y', 'z'])).toEqual(['0', '1', '2']);
+      });
+
+      it('work with objects', () => {
+        expect(keys()({ a: 'x', b: 'y', c: 'z' })).toEqual(['a', 'b', 'c']);
+      });
+
+      it('should return strict types', () => {
+        expect(keys.strict()({ 5: 'x', b: 'y', c: 'z' } as const)).toEqual([
+          '5',
+          'b',
+          'c',
+        ]);
+      });
+
+      it('should work in pipes', () => {
+        expect(pipe({ a: 'x', b: 'y', c: 'z' }, keys())).toEqual([
+          'a',
+          'b',
+          'c',
+        ]);
+      });
+
+      it('should work in pipes with strict', () => {
+        expect(pipe({ a: 'x', b: 'y', c: 'z' }, keys.strict())).toEqual([
+          'a',
+          'b',
+          'c',
+        ]);
+      });
     });
   });
 });
