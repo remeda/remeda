5137c91f2c61311daa7234702357adb06d24ea19
Eran Hirsch <eranhirsch@gmail.com>
2024-03-05 15:31:12 +0200

    docs

--- src/purry.ts
+++ src/purry.ts
@@ -10,10 +10,18 @@ type MaybeLazyFunction = {
 };
 
 /**
- * Creates a function with `data-first` and `data-last` signatures.
+ * Creates a function with `dataFirst` and `dataLast` signatures.
  *
- * `purry` is a dynamic function and it's not type safe. It should be wrapped by a function that have proper typings.
- * Refer to the example below for correct usage.
+ * `purry` is a dynamic function and it's not type safe. It should be wrapped by
+ * a function that have proper typings. Refer to the example below for correct
+ * usage.
+ *
+ * !IMPORTANT: functions that simply call `purry` and return the result (like
+ * almost all functions in this library) should return `unknown` themselves if
+ * an explicit return type is required. This is because we currently don't
+ * provide a generic return type that is built from the input function, and
+ * crafting one manually isn't worthwhile as we rely on function declaration
+ * overloading to combine the types for dataFirst and dataLast invocations!
  *
  * @param fn the function to purry.
  * @param args the arguments
