975dd72b13fdc488bec3928c76de0c87b9d9fb8b
Eran Hirsch <eranhirsch@gmail.com>
2024-03-11 20:15:48 +0200

    docs

--- src/isIncludedIn.ts
+++ src/isIncludedIn.ts
@@ -1,10 +1,61 @@
 import type { IterableContainer } from "./_types";
 
+/**
+ * Checks if the item is included in the container. This is a wrapper around
+ * `Array.prototype.includes` and `Set.prototype.has` and thus relies on the
+ * same equality checks that those functions do (which is reference equality,
+ * e.g. `===`). The input's type is narrowed to the container's type if
+ * possible.
+ *
+ * Notice that unlike most functions, this function takes a generic item as it's
+ * data and **an array** as it's parameter.
+ *
+ * @param data - The item that is checked.
+ * @param container - The items that are checked against.
+ * @returns A narrowed version of the input data on success, `false` otherwise.
+ * @signature
+ *   R.isIncludedIn(data, container);
+ * @example
+ *   R.isIncludedIn(2, [1, 2, 3]); // => true
+ *   R.isIncludedIn(4, [1, 2, 3]); // => false
+ *
+ *   const data = "cat" as "cat" | "dog" | "mouse";
+ *   R.isIncludedIn(data, ["cat", "dog"] as const); // true (typed "cat" | "dog");
+ * @dataFirst
+ * @category Guard
+ */
 export function isIncludedIn<T, S extends T>(
   data: T,
   container: IterableContainer<S>,
 ): data is S;
 
+/**
+ * Checks if the item is included in the container. This is a wrapper around
+ * `Array.prototype.includes` and `Set.prototype.has` and thus relies on the
+ * same equality checks that those functions do (which is reference equality,
+ * e.g. `===`). The input's type is narrowed to the container's type if
+ * possible.
+ *
+ * Notice that unlike most functions, this function takes a generic item as it's
+ * data and **an array** as it's parameter.
+ *
+ * @param data - The item that is checked.
+ * @param container - The items that are checked against.
+ * @returns A narrowed version of the input data on success, `false` otherwise.
+ * @signature
+ *   R.isIncludedIn(container)(data);
+ * @example
+ *   R.pipe(2, R.isIncludedIn([1, 2, 3])); // => true
+ *   R.pipe(4, R.isIncludedIn([1, 2, 3])); // => false
+ *
+ *   const data = "cat" as "cat" | "dog" | "mouse";
+ *   R.pipe(
+ *     data,
+ *     R.isIncludedIn(["cat", "dog"] as const),
+ *   ); // => true (typed "cat" | "dog");
+ * @dataLast
+ * @category Guard
+ */
 export function isIncludedIn<T, S extends T>(
   container: IterableContainer<S>,
 ): (data: T) => data is S;
