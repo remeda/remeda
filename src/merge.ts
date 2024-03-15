44f77e976c3abbd179b04b6d829ea390586d4d07
Eran Hirsch <eranhirsch@gmail.com>
2024-03-15 09:02:22 +0200

    naming

--- src/merge.ts
+++ src/merge.ts
@@ -3,40 +3,43 @@ import type { Merge } from "./type-fest/merge";
 
 /**
  * Spreads the properties of the second object into the first object, overriding
- * any existing properties; equivalent to `{ ...data, ...other }`.
+ * any existing properties; equivalent to `{ ...data, ...source }`.
  *
- * @param data - The first object, shared props will be overridden.
- * @param other - The second object, this object would be fully contained within
- * the result object.
+ * @param data - The destination object, shared props will be overridden.
+ * @param source - The source object, this object would be fully contained
+ * within the result object.
  * @signature
- *    R.merge(data, other)
+ *    R.merge(data, source)
  * @example
  *    R.merge({ x: 1, y: 2 }, { y: 10, z: 2 }) // => { x: 1, y: 10, z: 2 }
  * @dataFirst
  * @category Object
  */
-export function merge<T, S>(data: T, other: S): Merge<T, S>;
+export function merge<T, Source>(data: T, source: Source): Merge<T, Source>;
 
 /**
  * Spreads the properties of the second object into the first object, overriding
- * any existing properties; equivalent to `{ ...data, ...other }`.
+ * any existing properties; equivalent to `{ ...data, ...source }`.
  *
- * @param data - The first object, shared props will be overridden.
- * @param other - The second object, this object would be fully contained within
- * the result object.
+ * @param data - The destination object, shared props will be overridden.
+ * @param source - The source object, this object would be fully contained
+ * within the result object.
  * @signature
- *    R.merge(other)(data)
+ *    R.merge(source)(data)
  * @example
- *    R.pipe({ x: 1, y: 2 }, R.merge({ y: 10, z: 2 })) // => { x: 1, y: 10, z: 2 }
+ *    R.pipe(
+ *      { x: 1, y: 2 },
+ *      R.merge({ y: 10, z: 2 }),
+ *    ); // => { x: 1, y: 10, z: 2 }
  * @dataLast
  * @category Object
  */
-export function merge<S>(other: S): <T>(data: T) => Merge<T, S>;
+export function merge<Source>(source: Source): <T>(data: T) => Merge<T, Source>;
 
 export function merge(): unknown {
   return purry(_merge, arguments);
 }
 
-function _merge<T, S>(data: T, other: S): Merge<T, S> {
-  return { ...data, ...other };
+function _merge<T, Source>(data: T, source: Source): Merge<T, Source> {
+  return { ...data, ...source };
 }
