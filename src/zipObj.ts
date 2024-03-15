9771a8e7018ea50700e6f7927aff1f87f3101d24
Eran Hirsch <eranhirsch@gmail.com>
2024-03-15 17:41:27 +0200

    more

--- src/zipObj.ts
+++ src/zipObj.ts
@@ -13,7 +13,7 @@ import { purry } from "./purry";
  * @example
  *   R.zipObj(['a', 'b'], [1, 2]) // => {a: 1, b: 2}
  * @dataFirst
- * @category Array
+ * @category Deprecated
  * @deprecated Use `R.fromPairs.strict(R.zip(first, second))`. Will be removed in V2!
  */
 export function zipObj<F extends PropertyKey, S>(
@@ -25,7 +25,7 @@ export function zipObj<F extends PropertyKey, S>(
  * Creates a new object from two supplied lists by pairing up equally-positioned items.
  * Key/value pairing is truncated to the length of the shorter of the two lists.
  *
- * ! **DEPRECATED**: Use `<F extends PropertyKey>(first: ReadonlyArray<F>) => R.fromPairs.strict(R.zip(first, second))`. Will be removed in V2!
+ * ! **DEPRECATED**: Use `<F extends PropertyKey>(first: ReadonlyArray<F>) => R.fromPairs.strict(R.zip(first, second))` or if as part of a pipe: `R.pipe(..., R.zip(second), R.fromPairs.strict(), ...)`. Will be removed in V2!
  *
  * @param second - The second input list.
  * @signature
@@ -33,8 +33,8 @@ export function zipObj<F extends PropertyKey, S>(
  * @example
  *   R.zipObj([1, 2])(['a', 'b']) // => {a: 1, b: 2}
  * @dataLast
- * @category Array
- * @deprecated Use `<F extends PropertyKey>(first: ReadonlyArray<F>) => R.fromPairs.strict(R.zip(first, second))`. Will be removed in V2!
+ * @category Deprecated
+ * @deprecated Use `<F extends PropertyKey>(first: ReadonlyArray<F>) => R.fromPairs.strict(R.zip(first, second))` or if as part of a pipe: `R.pipe(..., R.zip(second), R.fromPairs.strict(), ...)`. Will be removed in V2!
  */
 export function zipObj<S>(
   second: ReadonlyArray<S>,
