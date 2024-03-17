072fde277df35e3357bd97c1ceff601ab4d893be
CJ Quines <cj@cjquines.com>
2024-03-17 13:00:37 -0400

    fix sortedIndexBy

--- src/sortedIndexBy.ts
+++ src/sortedIndexBy.ts
@@ -63,22 +63,6 @@ export function sortedIndexBy<T>(
   valueFunction: (item: T) => NonNullable<unknown>,
 ): (data: ReadonlyArray<T>) => number;
 
-/**
- * Find the insertion position (index) of an item in an array with items sorted
- * in ascending order using a value function; so that `splice(sortedIndex, 0, item)`
- * would result in maintaining the arrays sort-ness. The array can contain
- * duplicates. If the item already exists in the array the index would be of the
- * first occurrence of the item.
- *
- * @returns Insertion index (In the range 0..array.length - 1).
- * @signature
- *    R.sortedIndexBy(item, valueFunction)(data)
- * @example
- *    R.pipe([{age:20},{age:22}], R.sortedIndexBy({age:21},prop('age'))) // => 1
- * @dataLast
- * @category Array
- * @see sortedIndex, sortedIndexWith, sortedLastIndex, sortedLastIndexBy
- */
 export function sortedIndexBy(): unknown {
   return purry(sortedIndexByImplementation, arguments);
 }
