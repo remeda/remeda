6acbc58c601491617d6867836a73196b39ded14d
CJ Quines <cj@cjquines.com>
2024-03-18 02:11:20 -0400

    feat(hasSubObject): Add `hasSubObject` (#518)
    closes #386 (at least the function request part)
    
    ---
    
    Make sure that you:
    
    - [x] Typedoc added for new methods and updated for changed
    - [x] Tests added for new methods and updated for changed
    - [x] New methods added to `src/index.ts`
    - [x] New methods added to `mapping.md`
    
    ---
    
    <details><summary>We use semantic PR titles to automate the release
    process!</summary>
    
    https://conventionalcommits.org
    
    PRs should be titled following using the format: `< TYPE >(< scope >)?:
    description`
    
    ### Available Types:
    
    - `feat`: new functions, and changes to a function's type that would
    impact users.
    - `fix`: changes to the runtime behavior of an existing function, or
    refinements to it's type that shouldn't impact most users.
    - `perf`: changes to function implementations that improve a functions
    _runtime_ performance.
    - `refactor`: changes to function implementations that are neither `fix`
    nor `perf`
    - `test`: tests-only changes (transparent to users of the function).
    - `docs`: changes to the documentation of a function **or the
    documentation site**.
    - `build`, `ci`, `style`, `chore`, and `revert`: are only relevant for
    the internals of the library.
    
    For scope put the name of the function you are working on (either new or
    existing).
    
    </details>
    
    ---------
    
    Co-authored-by: Eran Hirsch <eranhirsch@gmail.com>

---
+++ src/hasSubObject.test.ts
@@ -0,0 +1,148 @@
+import { hasSubObject } from "./hasSubObject";
+import { pipe } from "./pipe";
+
+describe("data first", () => {
+  test("works with empty sub-object", () => {
+    expect(hasSubObject({ a: 1, b: "b", c: 3 }, {})).toBe(true);
+    expect(hasSubObject({}, {})).toBe(true);
+  });
+
+  test("works with primitives", () => {
+    expect(hasSubObject({ a: 1, b: "b", c: 3 }, { a: 1, b: "b" })).toBe(true);
+    expect(hasSubObject({ a: 1, b: "c", c: 3 }, { a: 1, b: "b" })).toBe(false);
+    expect(hasSubObject({ a: 2, b: "b", c: 3 }, { a: 1, b: "b" })).toBe(false);
+  });
+
+  test("works with deep objects", () => {
+    expect(hasSubObject({ a: { b: 1, c: 2 } }, { a: { b: 1, c: 2 } })).toBe(
+      true,
+    );
+    expect(hasSubObject({ a: { b: 1, c: 2 } }, { a: { b: 1, c: 0 } })).toBe(
+      false,
+    );
+  });
+
+  test("checks for matching key", () => {
+    const data = {} as { a?: undefined };
+    expect(hasSubObject(data, { a: undefined })).toBe(false);
+  });
+});
+
+describe("data last", () => {
+  test("empty sub-object", () => {
+    expect(pipe({ a: 1, b: 2, c: 3 }, hasSubObject({}))).toBe(true);
+    expect(pipe({}, hasSubObject({}))).toBe(true);
+  });
+
+  test("works with primitives", () => {
+    expect(pipe({ a: 1, b: "b", c: 3 }, hasSubObject({ a: 1, b: "b" }))).toBe(
+      true,
+    );
+    expect(pipe({ a: 1, b: "c", c: 3 }, hasSubObject({ a: 1, b: "b" }))).toBe(
+      false,
+    );
+    expect(pipe({ a: 2, b: "b", c: 3 }, hasSubObject({ a: 1, b: "b" }))).toBe(
+      false,
+    );
+  });
+
+  test("works with deep objects", () => {
+    expect(
+      pipe({ a: { b: 1, c: 2 } }, hasSubObject({ a: { b: 1, c: 2 } })),
+    ).toBe(true);
+
+    expect(
+      pipe({ a: { b: 1, c: 2 } }, hasSubObject({ a: { b: 1, c: 3 } })),
+    ).toBe(false);
+  });
+});
+
+describe("typing", () => {
+  describe("data-first", () => {
+    test("must have matching keys and values", () => {
+      expectTypeOf(hasSubObject({ a: 2 }, { a: 1 })).toEqualTypeOf<boolean>();
+
+      // @ts-expect-error [ts2353] - missing a key
+      hasSubObject({ b: 2 }, { a: 1 });
+
+      // @ts-expect-error [ts2322] - different value type
+      hasSubObject({ a: "a" }, { a: 1 });
+
+      // ok - wider value type
+      hasSubObject({ a: "a" } as { a: number | string }, { a: 1 });
+
+      // @ts-expect-error [ts2345] - narrower value type
+      hasSubObject({ a: "a" }, { a: 1 } as { a: number | string });
+
+      // ok - unknown data type
+      hasSubObject({ a: 2 } as unknown, { a: 1 });
+
+      // ok - const value
+      hasSubObject({ a: 2 }, { a: 1 } as const);
+    });
+
+    test("allows nested objects", () => {
+      // ok - nested object has extra keys
+      hasSubObject({ a: { b: 4 } }, { a: { b: 1, c: 2 } });
+
+      // @ts-expect-error [ts2741] - nested object has missing keys
+      hasSubObject({ a: { b: 1, c: 2 } }, { a: { b: 1 } });
+
+      // @ts-expect-error [ts2322] - nested object has wrong value types
+      hasSubObject({ a: { b: 4, c: "c" } }, { a: { b: 1, c: 2 } });
+    });
+
+    test("narrows type", () => {
+      const obj = {} as { a?: string; b?: number };
+
+      if (hasSubObject(obj, {})) {
+        expectTypeOf(obj).toEqualTypeOf<{ a?: string; b?: number }>();
+      }
+
+      if (hasSubObject(obj, { a: "a" })) {
+        expectTypeOf(obj).toEqualTypeOf<{ a: string; b?: number }>();
+      }
+
+      if (hasSubObject(obj, { a: "a" } as const)) {
+        expectTypeOf(obj).toEqualTypeOf<{ a: "a"; b?: number }>();
+      }
+    });
+  });
+
+  describe("data-last", () => {
+    test("must have matching keys and values", () => {
+      expectTypeOf(
+        pipe({ a: 2 }, hasSubObject({ a: 1 })),
+      ).toEqualTypeOf<boolean>();
+
+      // @ts-expect-error [ts2353] - missing a key
+      pipe({ b: 2 }, hasSubObject({ a: 1 }));
+
+      // @ts-expect-error [ts2345] - different value type
+      pipe({ a: "a" }, hasSubObject({ a: 1 }));
+
+      // ok - wider value type
+      pipe({ a: "a" } as { a: number | string }, hasSubObject({ a: 1 }));
+
+      // @ts-expect-error [ts2345] - narrower value type
+      pipe({ a: "a" }, hasSubObject({ a: 1 } as { a: number | string }));
+
+      // ok - unknown data type
+      pipe({ a: 2 } as unknown, hasSubObject({ a: 1 }));
+
+      // ok - const value
+      pipe({ a: 2 }, hasSubObject({ a: 1 } as const));
+    });
+
+    test("allows nested objects", () => {
+      // ok - nested object has extra keys
+      pipe({ a: { b: 4 } }, hasSubObject({ a: { b: 1, c: 2 } }));
+
+      // @ts-expect-error [ts2345] - nested object has missing keys
+      pipe({ a: { b: 1, c: 2 } }, hasSubObject({ a: { b: 1 } }));
+
+      // @ts-expect-error [ts2345] - nested object has wrong value types
+      pipe({ a: { b: 4, c: "c" } }, hasSubObject({ a: { b: 1, c: 2 } }));
+    });
+  });
+});
