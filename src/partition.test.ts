15407c246b01505563e00e4dfd183e7080492028
Eran Hirsch <eranhirsch@gmail.com>
2024-03-22 21:03:47 +0200

    partition

--- src/partition.test.ts
+++ src/partition.test.ts
@@ -2,66 +2,126 @@ import { isNumber } from "./isNumber";
 import { partition } from "./partition";
 import { pipe } from "./pipe";
 
-const array = [
-  { a: 1, b: 1 },
-  { a: 1, b: 2 },
-  { a: 2, b: 1 },
-  { a: 1, b: 3 },
-] as const;
-const expected = [
-  [
-    { a: 1, b: 1 },
-    { a: 1, b: 2 },
-    { a: 1, b: 3 },
-  ],
-  [{ a: 2, b: 1 }],
-];
+describe("runtime", () => {
+  describe("data first", () => {
+    test("partition", () => {
+      expect(
+        partition(
+          [
+            { a: 1, b: 1 },
+            { a: 1, b: 2 },
+            { a: 2, b: 1 },
+            { a: 1, b: 3 },
+          ],
+          ({ a }) => a === 1,
+        ),
+      ).toEqual([
+        [
+          { a: 1, b: 1 },
+          { a: 1, b: 2 },
+          { a: 1, b: 3 },
+        ],
+        [{ a: 2, b: 1 }],
+      ]);
+    });
 
-describe("data first", () => {
-  test("partition", () => {
-    expect(partition(array, (x) => x.a === 1)).toEqual(expected);
+    test("partition with type guard", () => {
+      expect(partition([1, "a", 2, "b"], isNumber)).toEqual([
+        [1, 2],
+        ["a", "b"],
+      ]);
+    });
+
+    test("partition with type guard in pipe", () => {
+      expect(
+        pipe(
+          [1, "a", 2, "b"],
+          partition((value): value is number => typeof value === "number"),
+        ),
+      ).toEqual([
+        [1, 2],
+        ["a", "b"],
+      ]);
+    });
+
+    test("indexed", () => {
+      expect(
+        partition(
+          [
+            { a: 1, b: 1 },
+            { a: 1, b: 2 },
+            { a: 2, b: 1 },
+            { a: 1, b: 3 },
+          ],
+          (_, index) => index !== 2,
+        ),
+      ).toEqual([
+        [
+          { a: 1, b: 1 },
+          { a: 1, b: 2 },
+          { a: 1, b: 3 },
+        ],
+        [{ a: 2, b: 1 }],
+      ]);
+    });
   });
+
+  describe("data last", () => {
+    test("partition", () => {
+      expect(
+        pipe(
+          [
+            { a: 1, b: 1 },
+            { a: 1, b: 2 },
+            { a: 2, b: 1 },
+            { a: 1, b: 3 },
+          ],
+          partition(({ a }) => a === 1),
+        ),
+      ).toEqual([
+        [
+          { a: 1, b: 1 },
+          { a: 1, b: 2 },
+          { a: 1, b: 3 },
+        ],
+        [{ a: 2, b: 1 }],
+      ]);
+    });
+
+    test("indexed", () => {
+      expect(
+        pipe(
+          [
+            { a: 1, b: 1 },
+            { a: 1, b: 2 },
+            { a: 2, b: 1 },
+            { a: 1, b: 3 },
+          ],
+          partition((_, index) => index !== 2),
+        ),
+      ).toEqual([
+        [
+          { a: 1, b: 1 },
+          { a: 1, b: 2 },
+          { a: 1, b: 3 },
+        ],
+        [{ a: 2, b: 1 }],
+      ]);
+    });
+  });
+});
+
+describe("typing", () => {
   test("partition with type guard", () => {
     const actual = partition([1, "a", 2, "b"], isNumber);
-    expect(actual).toEqual([
-      [1, 2],
-      ["a", "b"],
-    ]);
-    assertType<[Array<number>, Array<string>]>(actual);
+    expectTypeOf(actual).toEqualTypeOf<[Array<number>, Array<string>]>();
   });
+
   test("partition with type guard in pipe", () => {
     const actual = pipe(
       [1, "a", 2, "b"],
       partition((value): value is number => typeof value === "number"),
     );
-    expect(actual).toEqual([
-      [1, 2],
-      ["a", "b"],
-    ]);
-    assertType<[Array<number>, Array<string>]>(actual);
-  });
-  test("partition.indexed", () => {
-    expect(partition.indexed(array, (_, index) => index !== 2)).toEqual(
-      expected,
-    );
-  });
-});
-
-describe("data last", () => {
-  test("partition", () => {
-    expect(
-      pipe(
-        array,
-        partition((x) => x.a === 1),
-      ),
-    ).toEqual(expected);
-  });
-  test("partition.indexed", () => {
-    expect(
-      pipe(
-        array,
-        partition.indexed((_, index) => index !== 2),
-      ),
-    ).toEqual(expected);
+    expectTypeOf(actual).toEqualTypeOf<[Array<number>, Array<string>]>();
   });
 });
