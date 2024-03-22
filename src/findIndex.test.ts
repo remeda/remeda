f8d98ae84509877921cd660add30c6e8519adfa4
Eran Hirsch <eranhirsch@gmail.com>
2024-03-22 12:49:03 +0200

    findIndex

--- src/findIndex.test.ts
+++ src/findIndex.test.ts
@@ -1,41 +1,27 @@
-import { createLazyInvocationCounter } from "../test/lazy_invocation_counter";
 import { findIndex } from "./findIndex";
+import { identity } from "./identity";
+import { map } from "./map";
 import { pipe } from "./pipe";
 
 describe("data first", () => {
   test("findIndex", () => {
-    expect(findIndex([10, 20, 30] as const, (x) => x === 20)).toBe(1);
-  });
-
-  test("findIndex.indexed", () => {
-    expect(findIndex([10, 20, 30] as const, (x) => x === 20)).toBe(1);
+    expect(findIndex([10, 20, 30], (x) => x === 20)).toBe(1);
   });
 
   test("findIndex -1", () => {
-    expect(findIndex([2, 3, 4] as const, (x) => x === (20 as number))).toBe(-1);
+    expect(findIndex([2, 3, 4], (x) => x === 20)).toBe(-1);
   });
 });
 
 describe("data last", () => {
   test("findIndex", () => {
-    const counter = createLazyInvocationCounter();
+    const counter = vi.fn(identity);
     const actual = pipe(
       [10, 20, 30] as const,
-      counter.fn(),
+      map(counter),
       findIndex((x) => x === 20),
     );
-    expect(counter.count).toHaveBeenCalledTimes(2);
-    expect(actual).toEqual(1);
-  });
-
-  test("findIndex.indexed", () => {
-    const counter = createLazyInvocationCounter();
-    const actual = pipe(
-      [10, 20, 30] as const,
-      counter.fn(),
-      findIndex.indexed((x) => x === 20),
-    );
-    expect(counter.count).toHaveBeenCalledTimes(2);
+    expect(counter).toHaveBeenCalledTimes(2);
     expect(actual).toEqual(1);
   });
 });
