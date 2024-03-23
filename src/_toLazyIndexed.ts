ee11e35d391b17bb1309af6a4732d91ca10b5927
Eran Hirsch <eranhirsch@gmail.com>
2024-03-23 09:43:13 +0200

    remove unused stuff

--- src/_toLazyIndexed.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Typescript requires using `any` to infer any kind of function type, `unknown` is not enough.
-export const _toLazyIndexed = <Func extends (...args: any) => unknown>(
-  fn: Func,
-): Func & { readonly indexed: true } =>
-  Object.assign(fn, { indexed: true as const });
