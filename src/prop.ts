7d869cccb7eb348b2bde29cf1272114eeeb7de2b
Eran Hirsch <eranhirsch@gmail.com>
2024-03-23 15:43:14 +0200

    dataFirst prop

--- src/prop.ts
+++ src/prop.ts
@@ -1,14 +1,40 @@
+import { purry } from "./purry";
+
 /**
  * Gets the value of the given property.
  *
- * @param propName - The property name.
- * @signature R.prop(prop)(object)
+ * @param data - The object to extract the prop from.
+ * @param key - The key of the property to extract.
+ * @signature
+ *   R.prop(data, key);
+ * @example
+ *   R.prop({ foo: 'bar' }, 'foo'); // => 'bar'
+ * @dataFirst
+ * @category Object
+ */
+export function prop<T, K extends keyof T>(data: T, key: K): T[K];
+
+/**
+ * Gets the value of the given property.
+ *
+ * @param key - The key of the property to extract.
+ * @signature
+ *   R.prop(key)(data);
  * @example
  *    R.pipe({foo: 'bar'}, R.prop('foo')) // => 'bar'
  * @dataLast
  * @category Object
  */
-export const prop =
-  <T, K extends keyof T = keyof T>(propName: K) =>
-  ({ [propName]: value }: T): T[K] =>
-    value;
+export function prop<T, K extends keyof T>(key: K): (data: T) => T[K];
+
+export function prop(): unknown {
+  return purry(propImplementation, arguments);
+}
+
+export function propImplementation<T, K extends keyof T>(
+  data: T,
+  key: K,
+): T[K] {
+  const { [key]: value } = data;
+  return value;
+}
