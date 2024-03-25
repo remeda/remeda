44c951e3b8aae630c828cac50494fd4b23c35014
Eran Hirsch <eranhirsch@gmail.com>
2024-03-25 19:41:31 +0200

    revert

--- src/swapProps.ts
+++ src/swapProps.ts
@@ -54,9 +54,9 @@ function _swapProps<T extends object, K1 extends keyof T, K2 extends keyof T>(
 ): {
   [K in PropertyKey]: unknown;
 } {
-  const { [key1]: value1, [key2]: value2, ...remaining } = obj;
+  const { [key1]: value1, [key2]: value2 } = obj;
   return {
-    ...remaining,
+    ...obj,
     [key1]: value2,
     [key2]: value1,
   };
