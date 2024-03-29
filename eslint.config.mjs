a2324b5b22d72c1b37d8204fe55ce85df4f1585b
Eran Hirsch <eranhirsch@gmail.com>
2024-03-29 18:34:23 +0300

    skip coverage dir

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import jsdoc from "eslint-plugin-jsdoc";
 
 export default config(
   {
-    ignores: ["dist", "docs", "examples"],
+    ignores: ["coverage", "dist", "docs", "examples"],
   },
   core.configs.recommended,
   // @ts-expect-error [ts7016] -- I don't know why typing is broken here...
