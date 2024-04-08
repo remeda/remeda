44078a12e27e9e28a59ed3cf887151e7d684ab17
Eran Hirsch <eranhirsch@gmail.com>
2024-04-08 18:23:10 +0300

    swap indices

--- src/swapIndices.test.ts
+++ src/swapIndices.test.ts
@@ -4,8 +4,21 @@ describe("data_first", () => {
   it("swap array", () => {
     expect(swapIndices([1, 2, 3, 4, 5], 0, -1)).toEqual([5, 2, 3, 4, 1]);
   });
+
   it("swap string", () => {
     expect(swapIndices("apple", 0, 1)).toEqual("paple");
+  });
+
+  it("fails gracefully on NaN indices", () => {
+    expect(swapIndices("apple", Number.NaN, 1)).toEqual("apple");
+    expect(swapIndices("apple", 1, Number.NaN)).toEqual("apple");
+    expect(swapIndices("apple", Number.NaN, Number.NaN)).toEqual("apple");
+  });
+
+  it("fails gracefully on overflow indices", () => {
+    expect(swapIndices("apple", 100, 1)).toEqual("apple");
+    expect(swapIndices("apple", 1, 100)).toEqual("apple");
+    expect(swapIndices("apple", 200, 300)).toEqual("apple");
   });
 });
 
