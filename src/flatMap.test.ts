c52e8749bebe1b460c07078a749d776d2bbc9063
Eran Hirsch <eranhirsch@gmail.com>
2024-04-08 19:20:43 +0300

    revert

--- src/flatMap.test.ts
+++ src/flatMap.test.ts
@@ -4,51 +4,49 @@ import { find } from "./find";
 import { flatMap } from "./flatMap";
 import { pipe } from "./pipe";
 
-describe("runtime", () => {
-  describe("dataFirst", () => {
-    it("flatMap", () => {
-      const result = flatMap([1, 2] as const, (x) => [x * 2, x * 3]);
-      expect(result).toEqual([2, 3, 4, 6]);
-    });
-
-    it("should accept fn returning a readonly array", () => {
-      const result = flatMap([1, 2] as const, (x) => [x * 2, x * 3] as const);
-      expect(result).toEqual([2, 3, 4, 6]);
-    });
+describe("dataFirst", () => {
+  it("flatMap", () => {
+    const result = flatMap([1, 2] as const, (x) => [x * 2, x * 3]);
+    expect(result).toEqual([2, 3, 4, 6]);
   });
 
-  describe("dataLast", () => {
-    it("flatMap", () => {
-      const result = flatMap((x: number) => [x * 2, x * 3])([1, 2]);
-      expect(result).toEqual([2, 3, 4, 6]);
-    });
+  it("should accept fn returning a readonly array", () => {
+    const result = flatMap([1, 2] as const, (x) => [x * 2, x * 3] as const);
+    expect(result).toEqual([2, 3, 4, 6]);
+  });
+});
 
-    it("should accept fn returning a readonly array", () => {
-      const result = flatMap((x: number) => [x * 2, x * 3] as const)([
-        1, 2,
-      ] as const);
-      expect(result).toEqual([2, 3, 4, 6]);
-    });
+describe("dataLast", () => {
+  it("flatMap", () => {
+    const result = flatMap((x: number) => [x * 2, x * 3])([1, 2]);
+    expect(result).toEqual([2, 3, 4, 6]);
+  });
 
-    it("works with non array outputs", () => {
-      expect(pipe([1, 2, 3], flatMap(add(1)))).toStrictEqual([2, 3, 4]);
-    });
+  it("should accept fn returning a readonly array", () => {
+    const result = flatMap((x: number) => [x * 2, x * 3] as const)([
+      1, 2,
+    ] as const);
+    expect(result).toEqual([2, 3, 4, 6]);
+  });
 
-    describe("pipe", () => {
-      test("with find", () => {
-        const counter1 = createLazyInvocationCounter();
-        const counter2 = createLazyInvocationCounter();
-        const result = pipe(
-          [10, 20, 30, 40] as const,
-          counter1.fn(),
-          flatMap((x) => [x, x + 1, x + 2, x + 3]),
-          counter2.fn(),
-          find((x) => x === 22),
-        );
-        expect(counter1.count).toHaveBeenCalledTimes(2);
-        expect(counter2.count).toHaveBeenCalledTimes(7);
-        expect(result).toEqual(22);
-      });
+  it("works with non array outputs", () => {
+    expect(pipe([1, 2, 3], flatMap(add(1)))).toStrictEqual([2, 3, 4]);
+  });
+
+  describe("pipe", () => {
+    test("with find", () => {
+      const counter1 = createLazyInvocationCounter();
+      const counter2 = createLazyInvocationCounter();
+      const result = pipe(
+        [10, 20, 30, 40] as const,
+        counter1.fn(),
+        flatMap((x) => [x, x + 1, x + 2, x + 3]),
+        counter2.fn(),
+        find((x) => x === 22),
+      );
+      expect(counter1.count).toHaveBeenCalledTimes(2);
+      expect(counter2.count).toHaveBeenCalledTimes(7);
+      expect(result).toEqual(22);
     });
   });
 });
