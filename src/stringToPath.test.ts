5fb77d582124824903d9360aa2de5fd64257654f
Martin Brodziansky <martin.brodziansky@rossum.ai>
2024-04-10 21:22:55 +0200

    Rework tests to cover more interesting cases

--- src/stringToPath.test.ts
+++ src/stringToPath.test.ts
@@ -1,17 +1,57 @@
 import { stringToPath } from "./stringToPath";
 
 test("should convert a string to a deeply nested path", () => {
-  const res = stringToPath("a.b[0].c");
-  expect<["a", "b", "0", "c"]>(res).toEqual(["a", "b", "0", "c"]);
+  const res = stringToPath("foo.bar[3].baz");
+  expect<["foo", "bar", "3", "baz"]>(res).toEqual(["foo", "bar", "3", "baz"]);
 });
 
 test("should handle nested dot paths", () => {
-  const res = stringToPath("a.b[a.b].c");
-  expect<["a", "b", "a.b", "c"]>(res).toEqual(["a", "b", "a.b", "c"]);
+  const res = stringToPath("foo[bar.baz].qui.che");
+  expect<["foo", "bar.baz", "qui", "che"]>(res).toEqual([
+    "foo",
+    "bar.baz",
+    "qui",
+    "che",
+  ]);
 });
 
-test("test with more than one-letter keys", () => {
-  const res = stringToPath("foo.bar[3].baz");
-  assertType<["foo", "bar", "3", "baz"]>(res);
-  expect(res).toEqual(["foo", "bar", "3", "baz"]);
+test("should handle short path with only bracket access", () => {
+  const res = stringToPath("foo[bar]");
+  expect<["foo", "bar"]>(res).toEqual(["foo", "bar"]);
+});
+
+test("should handle bracket access at the end", () => {
+  const res = stringToPath("foo.bar[3]");
+  expect<["foo", "bar", "3"]>(res).toEqual(["foo", "bar", "3"]);
+});
+
+test("should handle relatively long paths without performance issues", () => {
+  const res = stringToPath(
+    "lorem.ipsum[dolor.sit].amet.con.sec.tetur[adi.pisc.ing].elit.42",
+  );
+  expect<
+    [
+      "lorem",
+      "ipsum",
+      "dolor.sit",
+      "amet",
+      "con",
+      "sec",
+      "tetur",
+      "adi.pisc.ing",
+      "elit",
+      "42",
+    ]
+  >(res).toEqual([
+    "lorem",
+    "ipsum",
+    "dolor.sit",
+    "amet",
+    "con",
+    "sec",
+    "tetur",
+    "adi.pisc.ing",
+    "elit",
+    "42",
+  ]);
 });
