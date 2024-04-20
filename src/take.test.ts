b3616ca54423b07dc29794e655acf7dcfd81b128
Martin Brodziansky <martin.brodziansky@rossum.ai>
2024-04-20 02:08:06 +0200

    Add preliminary test

--- src/take.test.ts
+++ src/take.test.ts
@@ -11,3 +11,11 @@ describe("data_last", () => {
     expect(take(3)([1, 2, 3, 4, 3, 2, 1] as const)).toEqual([1, 2, 3]);
   });
 });
+
+describe("typings", () => {
+  it("infers tuple types properly", () => {
+    const result = take([1, 2, "foo", "bar"] as const, 3);
+
+    expectTypeOf(result).toEqualTypeOf<[1, 2, "foo"]>();
+  });
+});
