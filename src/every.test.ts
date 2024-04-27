0dc126531cc5de08a58f1b3e9e925aa36fdf9bfc
Martin Brodziansky <martin.brodziansky@rossum.ai>
2024-04-28 00:39:23 +0200

    Address PR comments

--- src/every.test.ts
+++ src/every.test.ts
@@ -2,73 +2,99 @@ import { every } from "./every";
 import { isNumber } from "./isNumber";
 import { pipe } from "./pipe";
 
-describe("data-first", () => {
-  test("returns correct values on simple arrays", () => {
-    const trueInput = [1, 2, 3];
-    const trueData = every(trueInput, (val) => val < 5);
-    expect(trueData).toBe(true);
+describe("runtime", () => {
+  describe("data-first", () => {
+    test("returns correct values on simple arrays", () => {
+      const trueInput = [1, 2, 3];
+      const trueData = every(trueInput, (val) => val < 5);
+      expect(trueData).toBe(true);
 
-    const falseInput = ["foo", "bar", "buzz"];
-    const falseData = every(falseInput, (val) => val.length > 3);
-    expect(falseData).toBe(false);
+      const falseInput = ["foo", "bar", "buzz"];
+      const falseData = every(falseInput, (val) => val.length > 3);
+      expect(falseData).toBe(false);
+    });
+
+    // empty set check
+    test("returns true on empty arrays regardless of predicate", () => {
+      const input = [] as const;
+      const data = every(input, () => false);
+      expect(data).toBe(true);
+    });
+
+    test("works with indexed predicate", () => {
+      const input = [1, 2, 3];
+      const data = every(input, (_, index) => index < 5);
+      expect(data).toBe(true);
+    });
   });
 
-  // empty set check
-  test("returns true on empty arrays regardless of predicate", () => {
-    const input = [] as const;
-    const data = every(input, () => false);
-    expect(data).toBe(true);
-  });
+  describe("data-last", () => {
+    test("returns correct values on simple arrays", () => {
+      const trueData = pipe(
+        [1, 2, 3],
+        every((val: number) => val < 5),
+      );
+      expect(trueData).toBe(true);
 
+      const falseData = pipe(
+        ["foo", "bar", "buzz"],
+        every((val: string) => val.length > 3),
+      );
+      expect(falseData).toBe(false);
+    });
+
+    // empty set check
+    test("returns true on empty arrays regardless of predicate", () => {
+      const data = pipe(
+        [] as const,
+        every(() => false),
+      );
+      expect(data).toBe(true);
+    });
+
+    test("works with indexed predicate", () => {
+      const input = [1, 2, 3];
+      const data = pipe(
+        input,
+        every((_, index) => index < 5),
+      );
+      expect(data).toBe(true);
+    });
+  });
+});
+
+describe("typing", () => {
   test("narrows types when a type predicate is passed", () => {
     const input: Array<number | string> = [1, 2];
-    const data = every(input, isNumber);
-    expect(data).toBe(true);
     if (every(input, isNumber)) {
       expectTypeOf(input).toEqualTypeOf<Array<number>>();
     }
   });
-});
 
-describe("data-last", () => {
-  test("returns correct values on simple arrays", () => {
-    const trueInput = [1, 2, 3];
-    const trueData = every((val: number) => val < 5)(trueInput);
-    expect(trueData).toBe(true);
-
-    const falseInput = ["foo", "bar", "buzz"];
-    const falseData = every((val: string) => val.length > 3)(falseInput);
-    expect(falseData).toBe(false);
-  });
-
-  // empty set check
-  test("returns true on empty arrays regardless of predicate", () => {
-    const input = [] as const;
-    const data = every(() => false)(input);
-    expect(data).toBe(true);
-  });
-
-  test("narrows types when a type predicate is passed", () => {
-    const input: Array<number | string> = [1, 2];
-    const data = every(isNumber)(input);
-    expect(data).toBe(true);
-    if (every(isNumber)(input)) {
-      expectTypeOf(input).toEqualTypeOf<Array<number>>();
-    }
-  });
-});
-
-describe("indexed", () => {
-  test("works correctly", () => {
+  test("works with indexed predicate", () => {
     const input = [1, 2, 3];
-    const data = every.indexed(input, (_, index) => index > 5);
-    expect(data).toBe(false);
-  });
-});
+    const data = every(input, (val, index, arr) => {
+      expectTypeOf(val).toEqualTypeOf<number>();
+      expectTypeOf(index).toEqualTypeOf<number>();
+      expectTypeOf(arr).toEqualTypeOf<ReadonlyArray<number>>();
+      return index < 5;
+    });
 
-describe("in pipe", () => {
-  test("infers types", () => {
-    const input: Array<number | string> = [1, 2, 3];
+    const dataPipe = pipe(
+      input,
+      every((val, index, arr) => {
+        expectTypeOf(val).toEqualTypeOf<number>();
+        expectTypeOf(index).toEqualTypeOf<number>();
+        expectTypeOf(arr).toEqualTypeOf<ReadonlyArray<number>>();
+        return index < 5;
+      }),
+    );
+    expect(data).toBe(true);
+    expect(dataPipe).toBe(true);
+  });
+
+  test("infers types when used in pipe", () => {
+    const input = [1, 2, 3] as Array<number | string>;
     const data = pipe(
       input,
       every((val) => {
--- src/every.ts
+++ src/every.ts
@@ -1,10 +1,4 @@
-import type {
-  Pred,
-  PredIndexed,
-  PredIndexedOptional,
-  TypePred,
-  TypePredIndexed,
-} from "./_types";
+import type { PredIndexed, TypePredIndexed } from "./_types";
 import { purry } from "./purry";
 
 /**
@@ -24,11 +18,11 @@ import { purry } from "./purry";
  */
 export function every<T, S extends T>(
   data: ReadonlyArray<T>,
-  predicate: TypePred<T, S>,
+  predicate: TypePredIndexed<T, S>,
 ): data is Array<S>;
 export function every<T>(
   data: ReadonlyArray<T>,
-  predicate: Pred<T, boolean>,
+  predicate: PredIndexed<T, boolean>,
 ): boolean;
 
 /**
@@ -41,54 +35,23 @@ export function every<T>(
  * @example
  *    R.every(isNumber)([1, 2, 3]) // => true
  *    R.every(isString)(['a', 0, 'b']) // => false
- * @example
- *    // using `R.every` as a type predicate within `conditional`
- *    const allNumbers = R.every(R.isNumber);
- *    const result = R.pipe(
- *      R.conditional(
- *        [allNumbers, val => {...}]
- *                  // ^? number[]
- *        ...
- *      )
- *    )
  * @dataLast
  * @indexed
  * @category Array
  */
 export function every<T, S extends T>(
-  predicate: TypePred<T, S>,
+  predicate: TypePredIndexed<T, S>,
 ): (data: ReadonlyArray<T>) => data is Array<S>;
 export function every<T>(
-  predicate: (input: T) => boolean,
+  predicate: PredIndexed<T, boolean>,
 ): (data: ReadonlyArray<T>) => boolean;
 
 export function every(): unknown {
-  return purry(_every(false), arguments);
+  return purry(everyImplementation, arguments);
 }
 
-const _every =
-  (indexed: boolean) =>
-  <T>(data: ReadonlyArray<T>, predicate: PredIndexedOptional<T, boolean>) =>
-    indexed
-      ? data.every((element, index, array) => predicate(element, index, array))
-      : data.every((element) => predicate(element));
-
-export namespace every {
-  export function indexed<T, S extends T>(
-    data: ReadonlyArray<T>,
-    predicate: TypePredIndexed<T, S>,
-  ): data is Array<S>;
-  export function indexed<T>(
-    data: ReadonlyArray<T>,
-    predicate: PredIndexed<T, boolean>,
-  ): boolean;
-  export function indexed<T, S extends T>(
-    predicate: TypePredIndexed<T, S>,
-  ): (data: ReadonlyArray<T>) => data is Array<S>;
-  export function indexed<T>(
-    predicate: PredIndexed<T, boolean>,
-  ): (data: ReadonlyArray<T>) => boolean;
-  export function indexed(): unknown {
-    return purry(_every(true), arguments);
-  }
-}
+const everyImplementation = <T>(
+  data: ReadonlyArray<T>,
+  predicate: PredIndexed<T, boolean>,
+): boolean =>
+  data.every((element, index, array) => predicate(element, index, array));
