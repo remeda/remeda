31b1c18ba61a2e1bdaa8f2862af5508916850ea6
Eran Hirsch <eranhirsch@gmail.com>
2024-05-05 18:03:06 +0300

    more testing

--- src/drop.test.ts
+++ src/drop.test.ts
@@ -1,37 +1,57 @@
-import { createLazyInvocationCounter } from "../test/lazyInvocationCounter";
 import { drop } from "./drop";
+import { identity } from "./identity";
+import { map } from "./map";
 import { pipe } from "./pipe";
 import { take } from "./take";
 
-const array = [1, 2, 3, 4, 5] as const;
-const expected = [3, 4, 5];
+describe("runtime", () => {
+  describe("data first", () => {
+    it("works on regular inputs", () => {
+      expect(drop([1, 2, 3, 4, 5], 2)).toStrictEqual([3, 4, 5]);
+    });
 
-describe("data first", () => {
-  test("should drop", () => {
-    expect(drop(array, 2)).toEqual(expected);
+    it("works trivially on empty arrays", () => {
+      expect(drop([], 2)).toStrictEqual([]);
+    });
+
+    it("works trivially with negative numbers", () => {
+      expect(drop([1, 2, 3, 4, 5], -1)).toStrictEqual([1, 2, 3, 4, 5]);
+    });
+
+    it("works when dropping more than the length of the array", () => {
+      expect(drop([1, 2, 3, 4, 5], 10)).toStrictEqual([]);
+    });
+
+    test("returns a shallow clone when no items are dropped", () => {
+      const data = [1, 2, 3, 4, 5];
+      const result = drop(data, 0);
+      expect(result).toStrictEqual([1, 2, 3, 4, 5]);
+      expect(result).not.toBe(data);
+    });
   });
 
-  test("should not drop", () => {
-    expect(drop(array, 0)).toEqual(array);
-    expect(drop(array, -0)).toEqual(array);
-    expect(drop(array, -1)).toEqual(array);
-    expect(drop(array, Number.NaN)).toEqual(array);
-  });
+  describe("data last", () => {
+    it("works on regular inputs", () => {
+      expect(pipe([1, 2, 3, 4, 5], drop(2))).toStrictEqual([3, 4, 5]);
+    });
 
-  test("should return a new array even if there was no drop", () => {
-    expect(drop(array, 0)).not.toBe(array);
-  });
-});
+    it("works trivially on empty arrays", () => {
+      expect(pipe([], drop(2))).toStrictEqual([]);
+    });
 
-describe("data last", () => {
-  test("drop", () => {
-    const result = pipe(array, drop(2));
-    expect(result).toEqual(expected);
-  });
-  test("drop with take", () => {
-    const counter = createLazyInvocationCounter();
-    const result = pipe(array, counter.fn(), drop(2), take(2));
-    expect(counter.count).toHaveBeenCalledTimes(4);
-    expect(result).toEqual([3, 4]);
+    it("works trivially with negative numbers", () => {
+      expect(pipe([1, 2, 3, 4, 5], drop(-1))).toStrictEqual([1, 2, 3, 4, 5]);
+    });
+
+    it("works when dropping more than the length of the array", () => {
+      expect(pipe([1, 2, 3, 4, 5], drop(10))).toStrictEqual([]);
+    });
+
+    test("drop with take", () => {
+      const mockFunc = vi.fn(identity());
+      const result = pipe([1, 2, 3, 4, 5], map(mockFunc), drop(2), take(2));
+      expect(mockFunc).toHaveBeenCalledTimes(4);
+      expect(result).toStrictEqual([3, 4]);
+    });
   });
 });
