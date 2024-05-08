5542b17a2895128cd45dd54cd62a8a28806e1b47
CJ Quines <cj@cjquines.com>
2024-05-07 21:11:45 -0400

    remove empty migration

--- docs/src/components/v1/migration-box.tsx
+++ docs/src/components/v1/migration-box.tsx
@@ -9,21 +9,9 @@ import { CaretSortIcon } from "@radix-ui/react-icons";
 
 export const MigrationBox = ({
   children,
-  hasMigration,
 }: {
   readonly children: ReactNode;
-  readonly hasMigration: boolean;
 }): ReactNode => {
-  if (!hasMigration) {
-    return (
-      <div className="flex flex-col gap-8 rounded-md border border-emerald-100 bg-gradient-to-tl from-emerald-50 to-emerald-100 p-3 shadow-sm dark:border-emerald-900 dark:from-emerald-900 dark:to-emerald-950">
-        <h6 className="text-sm text-neutral-800 dark:text-neutral-200">
-          No breaking changes.
-        </h6>
-      </div>
-    );
-  }
-
   return (
     <div className="flex flex-col gap-8 rounded-md border border-sky-100 bg-gradient-to-tl from-sky-50 to-sky-100 p-3 shadow-sm dark:border-sky-900 dark:from-sky-900 dark:to-sky-950">
       <Collapsible>
