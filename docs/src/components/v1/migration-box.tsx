0a03d1f429d8c647e9665a64cae9ee71127236aa
CJ Quines <cj@cjquines.com>
2024-05-07 21:06:31 -0400

    address review

--- docs/src/components/v1/migration-box.tsx
+++ docs/src/components/v1/migration-box.tsx
@@ -2,17 +2,18 @@ import type { ReactNode } from "react";
 import { Button } from "@/components/ui/button";
 import {
   CollapsibleContent,
-  Collapsible as CollapsibleRoot,
+  Collapsible,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 import { CaretSortIcon } from "@radix-ui/react-icons";
 
-interface MigrationBoxProps {
-  children: ReactNode;
-  hasMigration: boolean;
-}
-
-export const MigrationBox = ({ children, hasMigration }: MigrationBoxProps) => {
+export const MigrationBox = ({
+  children,
+  hasMigration,
+}: {
+  readonly children: ReactNode;
+  readonly hasMigration: boolean;
+}): ReactNode => {
   if (!hasMigration) {
     return (
       <div className="flex flex-col gap-8 rounded-md border border-emerald-100 bg-gradient-to-tl from-emerald-50 to-emerald-100 p-3 shadow-sm dark:border-emerald-900 dark:from-emerald-900 dark:to-emerald-950">
@@ -25,7 +26,7 @@ export const MigrationBox = ({ children, hasMigration }: MigrationBoxProps) => {
 
   return (
     <div className="flex flex-col gap-8 rounded-md border border-sky-100 bg-gradient-to-tl from-sky-50 to-sky-100 p-3 shadow-sm dark:border-sky-900 dark:from-sky-900 dark:to-sky-950">
-      <CollapsibleRoot>
+      <Collapsible>
         <div className="relative flex items-center">
           <h6 className="text-sm font-semibold uppercase tracking-wider text-neutral-800 dark:text-neutral-200">
             Breaking changes in v2
@@ -40,7 +41,7 @@ export const MigrationBox = ({ children, hasMigration }: MigrationBoxProps) => {
         </div>
 
         <CollapsibleContent className="pt-4">{children}</CollapsibleContent>
-      </CollapsibleRoot>
+      </Collapsible>
     </div>
   );
 };
