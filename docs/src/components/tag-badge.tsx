a1bba6144c9fa5689a581068cbe6d08e788bf94a
Eran Hirsch <eranhirsch@gmail.com>
2024-05-13 14:16:05 +0300

    nit

--- docs/src/components/tag-badge.tsx
+++ docs/src/components/tag-badge.tsx
@@ -1,5 +1,5 @@
 import type { Tag } from "@/lib/get-tags";
-import { type ReactNode } from "react";
+import type { ReactNode } from "react";
 import { Badge } from "./ui/badge";
 
 const TAG_COLOR = {
