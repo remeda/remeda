e87c563bf767866775248189c29325b82a8551ca
Eran Hirsch <eranhirsch@gmail.com>
2024-05-29 20:36:20 +0300

    docs(v2): New front page (#681)
    * Migrate the docs to use MDX instead of hardcoded html.
    * Add the mapping to the navbar.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://astro.build/config
@@ -10,6 +11,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    mdx(),
   ],
   vite: {
     plugins: [
