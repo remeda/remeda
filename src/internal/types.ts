43f3fc837d8f86f1a2762bbf88003ae9a7a7f827
Eran Hirsch <eranhirsch@gmail.com>
2024-06-04 22:06:37 +0300

    fix(set): prevent setting `undefined` on optional props (#715)
    The current type would allow setting an optional prop with `undefined`
    even when it isn't a valid value for the prop.
    
    I also expanded the typing to make the output more useful by making
    optional props required and have a fixed value (when possible).

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -1,4 +1,4 @@
-import type { IsAny, IsNever } from "type-fest";
+import type { IsAny, IsLiteral, IsNever, Simplify } from "type-fest";
 
 export type NonEmptyArray<T> = [T, ...Array<T>];
 
@@ -107,6 +107,33 @@ export type ExactRecord<Key extends PropertyKey, Value> = IfSimpleRecord<
 export type ReorderedArray<T extends IterableContainer> = {
   -readonly [P in keyof T]: T[number];
 };
+
+export type UpsertProp<T, K extends PropertyKey, V> = Simplify<
+  // Copy any uninvolved props from the object, they are unaffected by the type.
+  Omit<T, K> &
+    // Rebuild the object for the rest:
+    (IsSingleLiteral<K> extends true
+      ? // If it's a single literal we need to remove the optionality and set
+        // the value as we know this prop would be exactly this value in the
+        // output.
+        { -readonly [P in K]-?: V }
+      : // The key is either a broad type (`string`) or union of literals
+        // ('cat' | 'dog') so we can't say anything for sure, we need to narrow
+        // the types of all relevant props, this has 2 parts, for props already
+        // in the object this means the value _might_ change, or it might not.
+        {
+          -readonly [P in keyof T as P extends K ? P : never]: T[P] | V;
+        } & {
+          // And for new props they might have been added to the object, or they
+          // might not have been, so we need to set them as optional.
+          -readonly [P in K as P extends keyof T ? never : P]?: V;
+        })
+>;
+
+// This type attempts to detect when a type is a single literal value (e.g.
+// "cat"), and not anything else (e.g. "cat" | "dog", string, etc...)
+type IsSingleLiteral<K> =
+  IsLiteral<K> extends true ? (IsUnion<K> extends true ? false : true) : false;
 
 // TODO [2024-10-01]: This type is copied from type-fest because it isn't
 // exported. It's part of the "internal" types. We should check back in a while
