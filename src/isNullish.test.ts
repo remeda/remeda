427a5d8146c171cd1572d16c6eda388832f6247a
Eran Hirsch <eranhirsch@gmail.com>
2024-06-10 20:46:28 +0300

    fix(isNullish): better type narrowing (#725)
    This addresses
    https://github.com/remeda/remeda/issues/675#issuecomment-2155230051, but
    it doesn't yet fix conditional or the issue raised in that issue.

--- src/isNullish.test.ts
+++ src/isNullish.test.ts
@@ -1,23 +1,92 @@
-import {
-  ALL_TYPES_DATA_PROVIDER,
-  TYPES_DATA_PROVIDER,
-  type AllTypesDataProviderTypes,
-} from "../test/typesDataProvider";
+import { ALL_TYPES_DATA_PROVIDER } from "../test/typesDataProvider";
 import { isNullish } from "./isNullish";
 
-describe("isNullish", () => {
-  it("should work as type guard", () => {
-    const data = TYPES_DATA_PROVIDER.null as AllTypesDataProviderTypes;
+describe("runtime", () => {
+  it("accepts nulls", () => {
+    expect(isNullish(null)).toBe(true);
+  });
+
+  it("accepts undefined", () => {
+    expect(isNullish(undefined)).toBe(true);
+  });
+
+  it("rejects anything else", () => {
+    for (const data of ALL_TYPES_DATA_PROVIDER) {
+      if (data === null || data === undefined) {
+        continue;
+      }
+      expect(isNullish(data)).toBe(false);
+    }
+  });
+});
+
+describe("typing", () => {
+  it("narrows nulls", () => {
+    const data = 123 as number | null;
     if (isNullish(data)) {
-      expect(data).toEqual(null);
+      expectTypeOf(data).toBeNull();
+    } else {
+      expectTypeOf(data).toBeNumber();
+    }
+  });
+
+  it("narrows undefined", () => {
+    const data = 123 as number | undefined;
+    if (isNullish(data)) {
+      expectTypeOf(data).toBeUndefined();
+    } else {
+      expectTypeOf(data).toBeNumber();
+    }
+  });
+
+  it("narrows on both", () => {
+    const data = 123 as number | null | undefined;
+    if (isNullish(data)) {
       expectTypeOf(data).toEqualTypeOf<null | undefined>();
+    } else {
+      expectTypeOf(data).toBeNumber();
+    }
+  });
+
+  it("doesn't narrow non-nullables", () => {
+    const data = 123;
+    if (isNullish(data)) {
+      expectTypeOf(data).toBeNever();
+    } else {
+      expectTypeOf(data).toBeNumber();
+    }
+  });
+
+  it("narrows unknowns", () => {
+    const data = 123 as unknown;
+    if (isNullish(data)) {
+      expectTypeOf(data).toEqualTypeOf<null | undefined>();
+    } else {
+      expectTypeOf(data).toBeUnknown();
+    }
+  });
+
+  it("narrows any", () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+    const data = 123 as any;
+    if (isNullish(data)) {
+      expectTypeOf(data).toEqualTypeOf<null | undefined>();
+    } else {
+      expectTypeOf(data).toBeAny();
+    }
+  });
+
+  it("doesn't expand narrow types", () => {
+    const data = "cat" as "cat" | "dog" | null | undefined;
+    if (isNullish(data)) {
+      expectTypeOf(data).toEqualTypeOf<null | undefined>();
+    } else {
+      expectTypeOf(data).toEqualTypeOf<"cat" | "dog">();
     }
   });
 
   it("should work as type guard in filter", () => {
-    const data = ALL_TYPES_DATA_PROVIDER.filter(isNullish);
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- Intentional for the test.
-    expect(data.every((c) => c === null || c === undefined)).toEqual(true);
-    expectTypeOf(data).toEqualTypeOf<Array<null | undefined>>();
+    const result = ALL_TYPES_DATA_PROVIDER.filter(isNullish);
+    expectTypeOf(result).toEqualTypeOf<Array<null | undefined>>();
   });
 });
