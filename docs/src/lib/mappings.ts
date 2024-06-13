999e9893c8f15ad27afbc5ce5fe01e6e9cb29579
Eran Hirsch <eranhirsch@gmail.com>
2024-06-13 22:11:30 +0300

    even less coupled

--- docs/src/lib/mappings.ts
+++ docs/src/lib/mappings.ts
@@ -1,25 +1,51 @@
 import { getCollection } from "astro:content";
 import {
-  sortBy,
-  pipe,
+  first,
   groupBy,
-  mapValues,
   map,
+  mapValues,
   pick,
+  pipe,
   piped,
   prop,
+  sortBy,
+  unique,
 } from "remeda";
+import type { Split } from "type-fest";
 
 const COLLECTION = "mapping";
 
-export const LIBRARIES = ["lodash", "ramda"] as const;
-type Library = (typeof LIBRARIES)[number];
+export type Library = Awaited<ReturnType<typeof getLibraries>>[number];
+
+/**
+ * We use the collection itself as the source-of-truth for what libraries we
+ * offer migration guides for. As long as there's a directory for a mapping, we
+ * will consider that mapping to be available.
+ *
+ * @returns the list of all libraries with mappings in the collection.
+ */
+export const getLibraries = async () =>
+  pipe(
+    await getCollection(COLLECTION),
+    map(
+      piped(
+        prop("id"),
+        // TODO: Add split to remeda!
+        ($) => $.split("/") as Split<typeof $, "/">,
+        first(),
+      ),
+    ),
+    unique(),
+  );
 
 export async function getMappingEntries(library: string) {
   const fileNameRegExp = new RegExp(`^${library}/(?<name>.*).mdx?$`, "ui");
 
   return pipe(
     await getCollection(COLLECTION, ({ id }) => id.startsWith(library)),
+    // The files will be sorted by whatever linux considers for ordering, but
+    // that makes uppercase and lowercase letters be separated. We want to use
+    // a regular "dictionary" style order.
     sortBy(({ id }) => id.toLocaleLowerCase()),
     groupBy(({ data: { category } }) => category),
     mapValues(
