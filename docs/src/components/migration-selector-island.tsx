a0c8c578dca50ccd9a096951093a82bc122f2c2b
Eran Hirsch <eranhirsch@gmail.com>
2024-06-13 22:47:59 +0300

    dynamic library selector

---
+++ docs/src/components/migration-selector-island.tsx
@@ -0,0 +1,31 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import type { Library } from "@/lib/mappings";
+import type { ReactNode } from "react";
+
+export function MigrationSelectorIsland({
+  libraries,
+  children,
+}: {
+  readonly libraries: ReadonlyArray<Library>;
+  readonly children: ReactNode;
+}) {
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <button type="button">{children}</button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {libraries.map((library) => (
+          <DropdownMenuItem key={library} className="capitalize">
+            <a href={`/migrate/${library}`}>{library}</a>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
