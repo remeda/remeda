739bd7f00e506705a745c460a2ace98368750020
Grigoris Christainas <grigorisxristainas@gmail.com>
2024-06-15 19:51:12 +0300

    refactor(types): remove export for ValuesOf, move closer to reference

--- src/internal/types.ts
+++ src/internal/types.ts
@@ -103,9 +103,17 @@ export type EnumerableStringKeyOf<T> =
     : never;
 
 /**
+ * A union of all values of properties in T which are not keyed by a symbol,
+ * following the definition of `Object.values` and `Object.entries`.
+ */
+export type EnumerableStringKeyedValueOf<T> = ValuesOf<{
+  [K in keyof T]-?: K extends symbol ? never : T[K];
+}>;
+
+/**
  * Extracts the value type from an object type T.
  */
-export type ValuesOf<T> =
+type ValuesOf<T> =
   // EmptyObject is used to infer value type as never instead of unknown
   // for an empty object
   T extends EmptyObject
@@ -113,14 +121,6 @@ export type ValuesOf<T> =
     : T extends Record<PropertyKey, infer V>
       ? V
       : never;
-
-/**
- * A union of all values of properties in T which are not keyed by a symbol,
- * following the definition of `Object.values` and `Object.entries`.
- */
-export type EnumerableStringKeyedValueOf<T> = ValuesOf<{
-  [K in keyof T]-?: K extends symbol ? never : T[K];
-}>;
 
 /**
  * This is the type you'd get from doing:
