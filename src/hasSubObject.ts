70fef4726309037b1c4fc84c5b75228c7848661b
Alexey Ryabov <lesha_12_01@mail.ru>
2024-06-18 22:09:47 +0300

    feat(hasSubObject): improve typing (#686)
    > This PR depends on https://github.com/remeda/remeda/pull/683. See
    35afa2fee17aa0fe16af3d467c18b22fabe23a44 for changes.
    
    **Depends on:** https://github.com/remeda/remeda/pull/683
    
    - make type narrowing smarter
    - handle cases with overlapping union types (e.g. `{ a: string |
    number}, { a: number | boolean }`)
    - improve typing for nested objects
    
    ---
    
    Make sure that you:
    
    - [x] Typedoc added for new methods and updated for changed
    - [x] Tests added for new methods and updated for changed
    - [x] New methods added to `src/index.ts`
    - [x] New methods added to `mapping.md`
    
    ---
    
    <details><summary>We use semantic PR titles to automate the release
    process!</summary>
    
    https://conventionalcommits.org
    
    PRs should be titled following using the format: `< TYPE >(< scope >)?:
    description`
    
    ### Available Types:
    
    - `feat`: new functions, and changes to a function's type that would
    impact users.
    - `fix`: changes to the runtime behavior of an existing function, or
    refinements to it's type that shouldn't impact most users.
    - `perf`: changes to function implementations that improve a functions
    _runtime_ performance.
    - `refactor`: changes to function implementations that are neither `fix`
    nor `perf`
    - `test`: tests-only changes (transparent to users of the function).
    - `docs`: changes to the documentation of a function **or the
    documentation site**.
    - `build`, `ci`, `style`, `chore`, and `revert`: are only relevant for
    the internals of the library.
    
    For scope put the name of the function you are working on (either new or
    existing).
    
    </details>

--- src/hasSubObject.ts
+++ src/hasSubObject.ts
@@ -9,6 +9,56 @@ type HasSubObjectGuard<T, S> = Simplify<
   Branded<S & T, typeof HAS_SUB_OBJECT_BRAND>
 >;
 
+type HasSubObjectObjectValue<A, B> = Partial<{
+  [Key in keyof A & keyof B]: A[Key] & B[Key] extends never
+    ? B[Key]
+    : A[Key] | B[Key] extends object
+      ? HasSubObjectObjectValue<A[Key], B[Key]>
+      : A[Key] & B[Key] extends object
+        ? B[Key]
+        : A[Key];
+}> & {
+  [Key in
+    | Exclude<keyof A, keyof B>
+    | Exclude<keyof B, keyof A>]: Key extends keyof B ? B[Key] : never;
+};
+
+type HasSubObjectData<
+  Data,
+  SubObject,
+  RData = Required<Data>,
+  RSubObject = Required<SubObject>,
+> = Partial<{
+  [Key in keyof RData & keyof RSubObject]: RData[Key] &
+    RSubObject[Key] extends never
+    ? RSubObject[Key]
+    : RData[Key] | RSubObject[Key] extends object
+      ? HasSubObjectObjectValue<RData[Key], RSubObject[Key]>
+      : RData[Key] & RSubObject[Key] extends object
+        ? RSubObject[Key]
+        : RData[Key];
+}> & {
+  [Key in Exclude<keyof SubObject, keyof Data>]: SubObject[Key];
+};
+
+type HasSubObjectSubObject<
+  SubObject,
+  Data,
+  RSubObject = Required<SubObject>,
+  RData = Required<Data>,
+> = Partial<{
+  [Key in keyof RData & keyof RSubObject]: RData[Key] &
+    RSubObject[Key] extends never
+    ? RData[Key]
+    : RData[Key] | RSubObject[Key] extends object
+      ? HasSubObjectObjectValue<RSubObject[Key], RData[Key]>
+      : RData[Key] & RSubObject[Key] extends object
+        ? RData[Key]
+        : RSubObject[Key];
+}> & {
+  [Key in Exclude<keyof SubObject, keyof Data>]: never;
+};
+
 /**
  * Checks if `subObject` is a sub-object of `object`, which means for every
  * property and value in `subObject`, there's the same property in `object`
@@ -25,10 +75,10 @@ type HasSubObjectGuard<T, S> = Simplify<
  * @dataFirst
  * @category Guard
  */
-export function hasSubObject<T, S extends Partial<T>>(
-  data: T,
-  subObject: S,
-): data is HasSubObjectGuard<T, S>;
+export function hasSubObject<
+  T extends Record<PropertyKey, unknown>,
+  S extends HasSubObjectSubObject<S, T>,
+>(data: T, subObject: S): data is HasSubObjectGuard<T, S>;
 
 /**
  * Checks if `subObject` is a sub-object of `object`, which means for every
@@ -45,32 +95,29 @@ export function hasSubObject<T, S extends Partial<T>>(
  * @dataLast
  * @category Guard
  */
-export function hasSubObject<T, S extends Partial<T>>(
+export function hasSubObject<S extends Record<PropertyKey, unknown>>(
   subObject: S,
-): (data: T) => data is HasSubObjectGuard<T, S>;
+): <T extends HasSubObjectData<T, S>>(
+  data: T,
+) => data is HasSubObjectGuard<T, S>;
 
 export function hasSubObject(...args: ReadonlyArray<unknown>): unknown {
   return purry(hasSubObjectImplementation, args);
 }
 
-function hasSubObjectImplementation<T extends object, S extends Partial<T>>(
-  data: T,
-  subObject: S,
-): data is HasSubObjectGuard<T, S> {
+function hasSubObjectImplementation<
+  T extends Record<PropertyKey, unknown>,
+  S extends Record<PropertyKey, unknown>,
+>(data: T, subObject: S): data is HasSubObjectGuard<T, S> {
   for (const [key, value] of Object.entries(subObject)) {
     if (!Object.hasOwn(data, key)) {
       return false;
     }
 
-    if (
-      !isDeepEqual(
-        value,
-        // @ts-expect-error [ts7053] - We already checked that `data` has `key
-        data[key],
-      )
-    ) {
+    if (!isDeepEqual(value, data[key])) {
       return false;
     }
   }
+
   return true;
 }
