bfb2ecbb2c555e82804f2ce3df9d70331d126efe
Eran Hirsch <eranhirsch@gmail.com>
2024-06-20 22:18:51 +0300

    oops

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,7 +3,6 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   site: "https://remedajs.com",
