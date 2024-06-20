67c13a91ec2254e858bd6299b11bb079c4a6866a
Eran Hirsch <eranhirsch@gmail.com>
2024-06-20 22:15:24 +0300

    sort

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,9 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import { visualizer } from "rollup-plugin-visualizer";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://remedajs.com",
