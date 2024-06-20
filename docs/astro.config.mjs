cb9fd7225f8a08b3429d0ba9f4dac3dff008e341
Eran Hirsch <eranhirsch@gmail.com>
2024-06-20 23:41:16 +0300

    docs: Add sitemap and robots.txt (#745)
    Making it easier for crawlers to find our pages. This was surfaced by
    the Algolia crawler, although it did manage to find all our pages.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,21 +1,18 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import { visualizer } from "rollup-plugin-visualizer";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://remedajs.com",
+
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
     mdx(),
+    sitemap(),
   ],
-  vite: {
-    plugins: [
-      visualizer({ filename: "node_modules/.cache/bundleVisualizer.html" }),
-    ],
-  },
 });
