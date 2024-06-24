ec27ddc6653c1f75fcbe64c01b1e57bf90c32d54
Eran Hirsch <eranhirsch@gmail.com>
2024-06-24 19:15:05 +0300

    docs(docsearch): dark mode (#749)

--- docs/src/lib/theme.ts
+++ docs/src/lib/theme.ts
@@ -4,44 +4,31 @@ const LOCAL_STORAGE_KEY = "theme";
 
 const DARK_THEME_CLASS = "dark";
 
-export function initTheme(): void {
-  const {
-    documentElement: { classList },
-  } = document;
+const ALGOLIA_DOCSEARCH_DARK_THEME_DATA_ATTRIBUTE = "theme";
+const ALGOLIA_DOCSEARCH_DARK_THEME_VALUE = "dark";
 
+export function initTheme(): void {
   const storedTheme = getStoredTheme();
 
   const prefersDarkMediaQuery = window.matchMedia(
     "(prefers-color-scheme: dark)",
   );
 
-  if (
+  setDarkMode(
     storedTheme === "dark" ||
-    (storedTheme === null && prefersDarkMediaQuery.matches)
-  ) {
-    classList.add(DARK_THEME_CLASS);
-  } else {
-    classList.remove(DARK_THEME_CLASS);
-  }
+      (storedTheme === null && prefersDarkMediaQuery.matches),
+  );
 
   prefersDarkMediaQuery.addEventListener(
     "change",
     ({ matches: prefersDark }) => {
-      if (prefersDark && getStoredTheme() !== null) {
-        classList.add(DARK_THEME_CLASS);
-      } else {
-        classList.remove(DARK_THEME_CLASS);
-      }
+      setDarkMode(prefersDark && getStoredTheme() !== null);
     },
   );
 }
 
 export function switchTheme(theme: Theme): void {
-  const {
-    documentElement: { classList },
-  } = document;
-
-  classList.remove("dark");
+  setDarkMode(false);
 
   if (theme === "system") {
     const { matches: prefersDark } = window.matchMedia(
@@ -49,16 +36,31 @@ export function switchTheme(theme: Theme): void {
     );
 
     if (prefersDark) {
-      classList.add(DARK_THEME_CLASS);
+      setDarkMode();
     }
 
     localStorage.removeItem(LOCAL_STORAGE_KEY);
   } else {
     if (theme === "dark") {
-      classList.add(DARK_THEME_CLASS);
+      setDarkMode();
     }
 
     localStorage.setItem(LOCAL_STORAGE_KEY, theme);
+  }
+}
+
+function setDarkMode(isEnabled = true): void {
+  const { documentElement } = document;
+
+  if (isEnabled) {
+    documentElement.classList.add(DARK_THEME_CLASS);
+
+    documentElement.dataset[ALGOLIA_DOCSEARCH_DARK_THEME_DATA_ATTRIBUTE] =
+      ALGOLIA_DOCSEARCH_DARK_THEME_VALUE;
+  } else {
+    documentElement.classList.remove(DARK_THEME_CLASS);
+
+    delete documentElement.dataset[ALGOLIA_DOCSEARCH_DARK_THEME_DATA_ATTRIBUTE];
   }
 }
 
