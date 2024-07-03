cc3de2725a940f1386fca20fe02255f6b626bb21
Sergey Kozlov <dartess@mail.ru>
2024-07-03 18:24:55 +0400

    add separated sandbox script with fetchpriority=low

---
+++ docs/public/sandbox.js
@@ -0,0 +1,8 @@
+import * as R from "https://esm.sh/remeda";
+
+window.R = R;
+
+console.log(
+  "%cTry out Remeda right here in your browser console; use the global R to access all the functions.",
+  "font-family: sans-serif; font-size: 16px",
+);
