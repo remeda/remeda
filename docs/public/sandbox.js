ccf42dcaf5719993e8be247ae438596fcf606f1a
Sergey Kozlov <dartess@mail.ru>
2024-07-03 21:47:48 +0400

    improve log about global R

--- docs/public/sandbox.js
+++ docs/public/sandbox.js
@@ -2,7 +2,18 @@ import * as R from "https://esm.sh/remeda";
 
 window.R = R;
 
+const sansSerif = "font-family: sans-serif; font-size: 16px";
+const monospace = "font-family: monospace; font-size: 16px";
+
 console.log(
-  "%cTry out Remeda right here in your browser console; use the global R to access all the functions.",
-  "font-family: sans-serif; font-size: 16px",
+  "%cYou can use Remeda with the %cR%c global.",
+  sansSerif,
+  monospace,
+  sansSerif,
+);
+console.log(
+  "%cTry typing %cR.add(5, 10)%c here:",
+  sansSerif,
+  monospace,
+  sansSerif,
 );
