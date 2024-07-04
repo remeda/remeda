fae177f14221b2569d76464e91f7cd115e628f7e
Sergey Kozlov <dartess@mail.ru>
2024-07-04 17:29:18 +0400

    add font-weight to code text & rename consts

--- docs/public/sandbox.js
+++ docs/public/sandbox.js
@@ -1,14 +1,14 @@
 import * as R from "https://esm.sh/remeda";
 
-const SANS_SERIF = "font-family: sans-serif; font-size: 16px";
-const MONOSPACE = "font-family: monospace; font-size: 16px";
+const NOTICE = "font-family: sans-serif; font-size: 16px";
+const CODE = "font-family: monospace; font-size: 16px; font-weight: 500";
 
 window.R = R;
 
 console.log(
   "%cYou can try out Remeda right here with the %cR%c global.",
-  SANS_SERIF,
-  MONOSPACE,
-  SANS_SERIF,
+  NOTICE,
+  CODE,
+  NOTICE,
 );
-console.log("%ce.g. %cR.add(5, 10)%c", SANS_SERIF, MONOSPACE, SANS_SERIF);
+console.log("%ce.g. %cR.add(5, 10)%c", NOTICE, CODE, NOTICE);
