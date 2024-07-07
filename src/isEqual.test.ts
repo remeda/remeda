48a3024642e38d1060bb2e80771ec925d1b48d7e
Eran Hirsch <eranhirsch@gmail.com>
2024-07-07 15:57:01 +0300

    impl

---
+++ src/isEqual.test.ts
@@ -0,0 +1,178 @@
+import { differenceWith } from "./differenceWith";
+import { isEqual } from "./isEqual";
+
+describe("runtime", () => {
+  describe("trivial cases", () => {
+    test("undefined", () => {
+      const data = undefined;
+      expect(isEqual(data, undefined)).toBe(true);
+    });
+
+    test("null", () => {
+      const data = null;
+      expect(isEqual(data, null)).toBe(true);
+    });
+
+    test("string", () => {
+      const data = "a";
+      expect(isEqual(data, "a")).toBe(true);
+    });
+
+    test("number", () => {
+      const data = 1;
+      expect(isEqual(data, 1)).toBe(true);
+    });
+
+    test("boolean", () => {
+      const data = true;
+      expect(isEqual(data, true)).toBe(true);
+    });
+
+    test("bigint", () => {
+      const data = 1n;
+      expect(isEqual(data, 1n)).toBe(true);
+    });
+
+    test("arrays", () => {
+      const data = [1, 2, 3];
+      expect(isEqual(data, [1, 2, 3])).toBe(false);
+      expect(isEqual(data, data)).toBe(true);
+    });
+
+    test("objects", () => {
+      const data = { a: 1, b: 2 };
+      expect(isEqual(data, { a: 1, b: 2 })).toBe(false);
+      expect(isEqual(data, data)).toBe(true);
+    });
+
+    test("regex", () => {
+      const data = /a/u;
+      expect(isEqual(data, /a/u)).toBe(false);
+      expect(isEqual(data, data)).toBe(true);
+    });
+
+    test("dates", () => {
+      const data = new Date();
+      expect(isEqual(data, new Date())).toBe(false);
+      expect(isEqual(data, data)).toBe(true);
+    });
+
+    test("promises", () => {
+      const data = Promise.resolve(1);
+      expect(isEqual(data, Promise.resolve(1))).toBe(false);
+      expect(isEqual(data, data)).toBe(true);
+    });
+
+    test("uint arrays", () => {
+      const data = new Uint8Array([1, 2, 3]);
+      expect(isEqual(data, new Uint8Array([1, 2, 3]))).toBe(false);
+      expect(isEqual(data, data)).toBe(true);
+    });
+
+    test("maps", () => {
+      const data = new Map([["a", 1]]);
+      expect(isEqual(data, new Map([["a", 1]]))).toBe(false);
+      expect(isEqual(data, data)).toBe(true);
+    });
+
+    test("sets", () => {
+      const data = new Set([1, 2, 3]);
+      expect(isEqual(data, new Set([1, 2, 3]))).toBe(false);
+      expect(isEqual(data, data)).toBe(true);
+    });
+  });
+
+  describe("special cases", () => {
+    test("NaN", () => {
+      // eslint-disable-next-line unicorn/prefer-number-properties
+      expect(isEqual(NaN, NaN)).toBe(true);
+      expect(isEqual(Number.NaN, Number.NaN)).toBe(true);
+    });
+
+    test("-0", () => {
+      expect(isEqual(-0, 0)).toBe(true);
+      expect(isEqual(-0, -0)).toBe(true);
+      expect(isEqual(0, 0)).toBe(true);
+    });
+
+    it("doesn't coalesce falsy values", () => {
+      expect(isEqual("" as unknown, 0)).toBe(false);
+      expect(isEqual("" as unknown, false)).toBe(false);
+      expect(isEqual(0 as unknown, false)).toBe(false);
+      expect(isEqual("" as unknown, null)).toBe(false);
+      expect(isEqual("" as unknown, undefined)).toBe(false);
+      expect(isEqual(0 as unknown, null)).toBe(false);
+      expect(isEqual(0 as unknown, undefined)).toBe(false);
+      expect(isEqual(false as unknown, null)).toBe(false);
+      expect(isEqual(false as unknown, undefined)).toBe(false);
+    });
+  });
+});
+
+describe("typing", () => {
+  it("narrows unions", () => {
+    const data = 1 as number | string;
+
+    if (isEqual(data, 1)) {
+      expectTypeOf(data).toEqualTypeOf<number>();
+    } else {
+      expectTypeOf(data).toEqualTypeOf<number | string>();
+    }
+
+    if (isEqual(data, "hello")) {
+      expectTypeOf(data).toEqualTypeOf<string>();
+    } else {
+      expectTypeOf(data).toEqualTypeOf<number | string>();
+    }
+  });
+
+  it("narrows to literal", () => {
+    const data = 1 as number;
+    if (isEqual(data, 1 as const)) {
+      expectTypeOf(data).toEqualTypeOf<1>();
+    } else {
+      expectTypeOf(data).toEqualTypeOf<number>();
+    }
+  });
+
+  it("doesn't accept non-overlapping types", () => {
+    // @ts-expect-error [ts2345] - Checking against the wrong type should fail
+    isEqual(1 as number, true);
+  });
+
+  it("works deeply", () => {
+    const data = [] as Array<
+      { a: Array<number> | Array<string> } | { b: Array<boolean> }
+    >;
+    if (isEqual(data, [{ a: [1] }])) {
+      expectTypeOf(data).toEqualTypeOf<Array<{ a: Array<number> }>>();
+    } else {
+      expectTypeOf(data).toEqualTypeOf<
+        Array<
+          | {
+              a: Array<number> | Array<string>;
+            }
+          | {
+              b: Array<boolean>;
+            }
+        >
+      >();
+    }
+  });
+
+  it("doesn't narrow when comparing objects of the same type", () => {
+    const data1 = { a: 1 } as { a: number };
+    const data2 = { a: 2 } as { a: number };
+    if (isEqual(data1, data2)) {
+      expectTypeOf(data1).toEqualTypeOf<{ a: number }>();
+    } else {
+      expectTypeOf(data1).toEqualTypeOf<{ a: number }>();
+    }
+  });
+
+  it("headless usage can infer types", () => {
+    // Tests the issue reported in: https://github.com/remeda/remeda/issues/641
+    const result = differenceWith(["a", "b", "c"], ["a", "c", "d"], isEqual);
+    expectTypeOf(result).toEqualTypeOf<Array<string>>();
+  });
+});
