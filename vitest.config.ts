6d99d3b1c83ec8cc16488a1ee78d1f21241a3a20
Eran Hirsch <eranhirsch@gmail.com>
2024-07-08 20:29:46 +0300

    typecheck

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+    typecheck: {
+      enabled: true,
+    },
     coverage: {
       include: ["src/**"],
-      exclude: ["src/index.ts"],
+      exclude: ["src/index.ts", "src/**.test-d.ts"],
     },
   },
 });
