d842c6f1a204b33c8424e8d42f13c28ec6084666
Eran Hirsch <eranhirsch@gmail.com>
2024-07-09 11:31:10 +0300

    split

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,12 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    typecheck: {
-      enabled: true,
-    },
     coverage: {
       include: ["src/**"],
-      exclude: ["src/index.ts", "src/**.test-d.ts"],
+      exclude: ["src/index.ts", "src/**/*.test-d.ts"],
     },
   },
 });
