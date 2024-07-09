fa9c4a9db1bbcc173e6f9b6db16f3fed84d2b3a8
Eran Hirsch <eranhirsch@gmail.com>
2024-07-09 12:29:41 +0300

    simplify

--- src/drop.test.ts
+++ src/drop.test.ts
@@ -4,53 +4,51 @@ import { map } from "./map";
 import { pipe } from "./pipe";
 import { take } from "./take";
 
-describe("runtime", () => {
-  describe("data first", () => {
-    it("works on regular inputs", () => {
-      expect(drop([1, 2, 3, 4, 5], 2)).toStrictEqual([3, 4, 5]);
-    });
-
-    it("works trivially on empty arrays", () => {
-      expect(drop([], 2)).toStrictEqual([]);
-    });
-
-    it("works trivially with negative numbers", () => {
-      expect(drop([1, 2, 3, 4, 5], -1)).toStrictEqual([1, 2, 3, 4, 5]);
-    });
-
-    it("works when dropping more than the length of the array", () => {
-      expect(drop([1, 2, 3, 4, 5], 10)).toStrictEqual([]);
-    });
-
-    test("returns a shallow clone when no items are dropped", () => {
-      const data = [1, 2, 3, 4, 5];
-      const result = drop(data, 0);
-      expect(result).toStrictEqual([1, 2, 3, 4, 5]);
-      expect(result).not.toBe(data);
-    });
+describe("data first", () => {
+  it("works on regular inputs", () => {
+    expect(drop([1, 2, 3, 4, 5], 2)).toStrictEqual([3, 4, 5]);
   });
 
-  describe("data last", () => {
-    it("works on regular inputs", () => {
-      expect(pipe([1, 2, 3, 4, 5], drop(2))).toStrictEqual([3, 4, 5]);
-    });
+  it("works trivially on empty arrays", () => {
+    expect(drop([], 2)).toStrictEqual([]);
+  });
 
-    it("works trivially on empty arrays", () => {
-      expect(pipe([], drop(2))).toStrictEqual([]);
-    });
+  it("works trivially with negative numbers", () => {
+    expect(drop([1, 2, 3, 4, 5], -1)).toStrictEqual([1, 2, 3, 4, 5]);
+  });
 
-    it("works trivially with negative numbers", () => {
-      expect(pipe([1, 2, 3, 4, 5], drop(-1))).toStrictEqual([1, 2, 3, 4, 5]);
-    });
+  it("works when dropping more than the length of the array", () => {
+    expect(drop([1, 2, 3, 4, 5], 10)).toStrictEqual([]);
+  });
 
-    it("works when dropping more than the length of the array", () => {
-      expect(pipe([1, 2, 3, 4, 5], drop(10))).toStrictEqual([]);
-    });
+  test("returns a shallow clone when no items are dropped", () => {
+    const data = [1, 2, 3, 4, 5];
+    const result = drop(data, 0);
+    expect(result).toStrictEqual([1, 2, 3, 4, 5]);
+    expect(result).not.toBe(data);
+  });
+});
 
-    test("lazy impl", () => {
-      const mockFunc = vi.fn(identity());
-      pipe([1, 2, 3, 4, 5], map(mockFunc), drop(2), take(2));
-      expect(mockFunc).toHaveBeenCalledTimes(4);
-    });
+describe("data last", () => {
+  it("works on regular inputs", () => {
+    expect(pipe([1, 2, 3, 4, 5], drop(2))).toStrictEqual([3, 4, 5]);
+  });
+
+  it("works trivially on empty arrays", () => {
+    expect(pipe([], drop(2))).toStrictEqual([]);
+  });
+
+  it("works trivially with negative numbers", () => {
+    expect(pipe([1, 2, 3, 4, 5], drop(-1))).toStrictEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("works when dropping more than the length of the array", () => {
+    expect(pipe([1, 2, 3, 4, 5], drop(10))).toStrictEqual([]);
+  });
+
+  test("lazy impl", () => {
+    const mockFunc = vi.fn(identity());
+    pipe([1, 2, 3, 4, 5], map(mockFunc), drop(2), take(2));
+    expect(mockFunc).toHaveBeenCalledTimes(4);
   });
 });
--- src/take.test.ts
+++ src/take.test.ts
@@ -3,53 +3,51 @@ import { map } from "./map";
 import { pipe } from "./pipe";
 import { take } from "./take";
 
-describe("runtime", () => {
-  describe("data first", () => {
-    it("works on regular inputs", () => {
-      expect(take([1, 2, 3, 4, 5], 2)).toStrictEqual([1, 2]);
-    });
-
-    it("works trivially on empty arrays", () => {
-      expect(take([], 2)).toStrictEqual([]);
-    });
-
-    it("works trivially with negative numbers", () => {
-      expect(take([1, 2, 3, 4, 5], -1)).toStrictEqual([]);
-    });
-
-    it("works when taking more than the length of the array", () => {
-      expect(take([1, 2, 3, 4, 5], 10)).toStrictEqual([1, 2, 3, 4, 5]);
-    });
-
-    test("returns a shallow clone when all items are taken", () => {
-      const data = [1, 2, 3, 4, 5];
-      const result = take(data, 5);
-      expect(result).toStrictEqual([1, 2, 3, 4, 5]);
-      expect(result).not.toBe(data);
-    });
+describe("data first", () => {
+  it("works on regular inputs", () => {
+    expect(take([1, 2, 3, 4, 5], 2)).toStrictEqual([1, 2]);
   });
 
-  describe("data last", () => {
-    it("works on regular inputs", () => {
-      expect(pipe([1, 2, 3, 4, 5], take(2))).toStrictEqual([1, 2]);
-    });
+  it("works trivially on empty arrays", () => {
+    expect(take([], 2)).toStrictEqual([]);
+  });
 
-    it("works trivially on empty arrays", () => {
-      expect(pipe([], take(2))).toStrictEqual([]);
-    });
+  it("works trivially with negative numbers", () => {
+    expect(take([1, 2, 3, 4, 5], -1)).toStrictEqual([]);
+  });
 
-    it("works trivially with negative numbers", () => {
-      expect(pipe([1, 2, 3, 4, 5], take(-1))).toStrictEqual([]);
-    });
+  it("works when taking more than the length of the array", () => {
+    expect(take([1, 2, 3, 4, 5], 10)).toStrictEqual([1, 2, 3, 4, 5]);
+  });
 
-    it("works when taking more than the length of the array", () => {
-      expect(pipe([1, 2, 3, 4, 5], take(10))).toStrictEqual([1, 2, 3, 4, 5]);
-    });
+  test("returns a shallow clone when all items are taken", () => {
+    const data = [1, 2, 3, 4, 5];
+    const result = take(data, 5);
+    expect(result).toStrictEqual([1, 2, 3, 4, 5]);
+    expect(result).not.toBe(data);
+  });
+});
 
-    test("lazy implementation", () => {
-      const mockFunc = vi.fn(identity());
-      pipe([1, 2, 3, 4, 5], map(mockFunc), take(2));
-      expect(mockFunc).toHaveBeenCalledTimes(2);
-    });
+describe("data last", () => {
+  it("works on regular inputs", () => {
+    expect(pipe([1, 2, 3, 4, 5], take(2))).toStrictEqual([1, 2]);
+  });
+
+  it("works trivially on empty arrays", () => {
+    expect(pipe([], take(2))).toStrictEqual([]);
+  });
+
+  it("works trivially with negative numbers", () => {
+    expect(pipe([1, 2, 3, 4, 5], take(-1))).toStrictEqual([]);
+  });
+
+  it("works when taking more than the length of the array", () => {
+    expect(pipe([1, 2, 3, 4, 5], take(10))).toStrictEqual([1, 2, 3, 4, 5]);
+  });
+
+  test("lazy implementation", () => {
+    const mockFunc = vi.fn(identity());
+    pipe([1, 2, 3, 4, 5], map(mockFunc), take(2));
+    expect(mockFunc).toHaveBeenCalledTimes(2);
   });
 });
