f80c64616b2eaa21dfb0c61be2f32d42e54af26e
Eran Hirsch <eranhirsch@gmail.com>
2024-07-09 10:55:42 +0300

    internal

--- src/internal/purryFromLazy.test.ts
+++ src/internal/purryFromLazy.test.ts
@@ -6,24 +6,22 @@
 import { type LazyEvaluator } from "../pipe";
 import { purryFromLazy } from "./purryFromLazy";
 
-describe("runtime", () => {
-  test("throws on wrong number of arguments", () => {
-    const zeroArgsLazyImpl = (): LazyEvaluator => () =>
-      ({ done: true, hasNext: false }) as const;
+test("throws on wrong number of arguments", () => {
+  const zeroArgsLazyImpl = (): LazyEvaluator => () =>
+    ({ done: true, hasNext: false }) as const;
 
-    const zeroArgsPurried = (...args: ReadonlyArray<unknown>) =>
-      purryFromLazy(zeroArgsLazyImpl, args);
+  const zeroArgsPurried = (...args: ReadonlyArray<unknown>) =>
+    purryFromLazy(zeroArgsLazyImpl, args);
 
-    expect(() =>
-      zeroArgsPurried(
-        // The first argument to the lazy purried function will always be an
-        // array.
-        ["hello"],
-        // But from the second param and onward the params belong to the lazy
-        // impl. Because our lazy impl takes 0 args, this extra param should
-        // throw.
-        "world",
-      ),
-    ).toThrowError("Wrong number of arguments");
-  });
+  expect(() =>
+    zeroArgsPurried(
+      // The first argument to the lazy purried function will always be an
+      // array.
+      ["hello"],
+      // But from the second param and onward the params belong to the lazy
+      // impl. Because our lazy impl takes 0 args, this extra param should
+      // throw.
+      "world",
+    ),
+  ).toThrowError("Wrong number of arguments");
 });
