6ef7279458a69e34a0c0832e8493be61d4d32ac9
Eran Hirsch <eranhirsch@gmail.com>
2024-07-12 10:43:28 +0300

    runtime tests

--- src/branch.test.ts
+++ src/branch.test.ts
@@ -1,8 +1,11 @@
 import { branch } from "./branch";
 import { constant } from "./constant";
+import { identity } from "./identity";
 import { isDefined } from "./isDefined";
 import { isNot } from "./isNot";
+import { isNullish } from "./isNullish";
 import { isStrictEqual } from "./isStrictEqual";
+import { isString } from "./isString";
 import { map } from "./map";
 import { pipe } from "./pipe";
 
@@ -18,6 +21,22 @@ describe("dataFirst", () => {
       expect(
         branch("hello", isStrictEqual("olleh"), constant("was true")),
       ).toBe("hello");
+    });
+
+    it("passes extra args to the functions (workaround)", () => {
+      expect(
+        branch(
+          "123",
+          isString,
+          (x, radix) => Number.parseInt(x, radix),
+          // The no-else signature doesn't allow for extra params to be passed
+          // so we need to use the with-else signature and pass identity which
+          // is what the no-else signature does with the else branch.
+          identity(),
+          // Extra args:
+          10 /* radix */,
+        ),
+      ).toBe(123);
     });
   });
 
@@ -38,6 +57,19 @@ describe("dataFirst", () => {
         ),
       ).toBe("was false");
     });
+
+    it("passes extra args to the functions", () => {
+      expect(
+        branch(
+          123,
+          (x) => x % 2 === 0,
+          (x, offset) => x + offset,
+          (x, offset) => x + offset + 1,
+          // Extra args:
+          100,
+        ),
+      ).toBe(224);
+    });
   });
 });
 
@@ -53,6 +85,18 @@ describe("dataLast", () => {
       expect(
         pipe("hello", branch(isStrictEqual("olleh"), constant("was true"))),
       ).toBe("hello");
+    });
+
+    it("passes extra args to the functions", () => {
+      expect(
+        map(
+          [1, 2, 3, 4],
+          branch(
+            (x) => x % 2 === 0,
+            (x, index) => x + index,
+          ),
+        ),
+      ).toStrictEqual([1, 3, 3, 7]);
     });
   });
 
@@ -76,6 +120,19 @@ describe("dataLast", () => {
       ).toBe("was false");
     });
   });
+
+  it("passes extra args to the functions", () => {
+    expect(
+      map(
+        [1, 2, 3, 4],
+        branch(
+          (x) => x % 2 === 0,
+          (x, index) => x + index,
+          (x, index) => `${x}${index}`,
+        ),
+      ),
+    ).toStrictEqual(["10", 3, "32", 7]);
+  });
 });
 
 it("can return other types", () => {
@@ -95,16 +152,41 @@ it("can return other types", () => {
       constant(42),
       constant({ a: "hello" }),
     ),
-  ).toEqual({ a: "hello" });
+  ).toStrictEqual({ a: "hello" });
 });
 
-it("acts as a coalesce tool", () => {
-  expect(
-    map(
-      [0, 1, 2, undefined, 4, undefined, 6],
-      branch(isNot(isDefined), constant("missing")),
-    ),
-  ).toEqual([0, 1, 2, "missing", 4, "missing", 6]);
+describe("recipes", () => {
+  it("acts as a coalesce tool", () => {
+    expect(
+      map(
+        [0, 1, 2, undefined, 4, undefined, 6],
+        branch(isNot(isDefined), constant("missing")),
+      ),
+    ).toStrictEqual([0, 1, 2, "missing", 4, "missing", 6]);
+  });
+
+  it("can replace defaultTo", () => {
+    // Nullish
+    expect(
+      map([undefined, null, 1], branch(isNullish, constant(42))),
+    ).toStrictEqual([42, 42, 1]);
+
+    // NaN
+    expect(
+      map([Number.NaN, 1], branch(Number.isNaN, constant(42))),
+    ).toStrictEqual([42, 1]);
+
+    // All
+    expect(
+      map(
+        [Number.NaN, undefined, null, 1],
+        branch(
+          (x) => Number.isNaN(x) || x === null || x === undefined,
+          constant(42),
+        ),
+      ),
+    ).toStrictEqual([42, 42, 42, 1]);
+  });
 });
 
 describe("known issues", () => {
