8f8aed2b6075f2e0f8b30b4aa3cacbd0c95a2e56
Eran Hirsch <eranhirsch@gmail.com>
2024-07-24 18:48:43 +0300

    add capitalize

---
+++ src/capitalize.ts
@@ -0,0 +1,39 @@
+import { purry } from "./purry";
+
+/**
+ * Makes first character of a string upper-case. Uses the built-in
+ * [`String.prototype.toUpperCase`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
+ * for the runtime, and the built-in [`Capitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#capitalizestringtype)
+ * utility type for typing.
+ *
+ * @param data - A string.
+ * @signature
+ *   R.capitalize(data);
+ * @example
+ *   R.capitalize("hello world"); // "Hello world"
+ * @dataFirst
+ * @category String
+ */
+export function capitalize<T extends string>(data: T): Capitalize<T>;
+
+/**
+ * Makes first character of a string upper-case. Uses the built-in
+ * [`String.prototype.toUpperCase`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
+ * for the runtime, and the built-in [`Capitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#capitalizestringtype)
+ * utility type for typing.
+ *
+ * @signature
+ *   R.capitalize()(data);
+ * @example
+ *   R.pipe("hello world", R.capitalize()); // "Hello world"
+ * @dataLast
+ * @category String
+ */
+export function capitalize(): <T extends string>(data: T) => Capitalize<T>;
+
+export function capitalize(...args: ReadonlyArray<unknown>): unknown {
+  return purry(capitalizeImplementation, args);
+}
+
+const capitalizeImplementation = <T extends string>(data: T): Capitalize<T> =>
+  `${data[0]?.toUpperCase() ?? ""}${data.slice(1)}` as Capitalize<T>;
