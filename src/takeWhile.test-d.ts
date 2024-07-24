d991e0ffe226ed159c7d863b282f2d34f393b05e
Eran Hirsch <eranhirsch@gmail.com>
2024-07-24 18:11:00 +0300

    more tests

--- src/takeWhile.test-d.ts
+++ src/takeWhile.test-d.ts
@@ -111,4 +111,104 @@ describe("data-last", () => {
     );
     expectTypeOf(result).toEqualTypeOf<Array<boolean | string>>();
   });
+
+  describe("predicate is typed correctly", () => {
+    test("empty array", () => {
+      pipe(
+        [] as [],
+        takeWhile((item, index, array) => {
+          expectTypeOf(item).toEqualTypeOf<never>();
+          expectTypeOf(index).toEqualTypeOf<number>();
+          expectTypeOf(array).toEqualTypeOf<[]>();
+          return true;
+        }),
+      );
+    });
+
+    test("regular array", () => {
+      pipe(
+        [] as Array<number>,
+        takeWhile((item, index, array) => {
+          expectTypeOf(item).toEqualTypeOf<number>();
+          expectTypeOf(index).toEqualTypeOf<number>();
+          expectTypeOf(array).toEqualTypeOf<Array<number>>();
+          return true;
+        }),
+      );
+    });
+
+    test("regular array with union type", () => {
+      pipe(
+        [] as Array<number | string>,
+        takeWhile((item, index, array) => {
+          expectTypeOf(item).toEqualTypeOf<number | string>();
+          expectTypeOf(index).toEqualTypeOf<number>();
+          expectTypeOf(array).toEqualTypeOf<Array<number | string>>();
+          return true;
+        }),
+      );
+    });
+
+    test("prefix array", () => {
+      pipe(
+        [1] as [number, ...Array<boolean>],
+        takeWhile((item, index, array) => {
+          expectTypeOf(item).toEqualTypeOf<boolean | number>();
+          expectTypeOf(index).toEqualTypeOf<number>();
+          expectTypeOf(array).toEqualTypeOf<[number, ...Array<boolean>]>();
+          return true;
+        }),
+      );
+    });
+
+    test("suffix array", () => {
+      pipe(
+        [1] as [...Array<boolean>, number],
+        takeWhile((item, index, array) => {
+          expectTypeOf(item).toEqualTypeOf<boolean | number>();
+          expectTypeOf(index).toEqualTypeOf<number>();
+          expectTypeOf(array).toEqualTypeOf<[...Array<boolean>, number]>();
+          return true;
+        }),
+      );
+    });
+
+    test("array with suffix and prefix", () => {
+      pipe(
+        [1, "a"] as [number, ...Array<boolean>, string],
+        takeWhile((item, index, array) => {
+          expectTypeOf(item).toEqualTypeOf<boolean | number | string>();
+          expectTypeOf(index).toEqualTypeOf<number>();
+          expectTypeOf(array).toEqualTypeOf<
+            [number, ...Array<boolean>, string]
+          >();
+          return true;
+        }),
+      );
+    });
+
+    test("tuple", () => {
+      pipe(
+        [1, "a", true] as const,
+        takeWhile((item, index, array) => {
+          expectTypeOf(item).toEqualTypeOf<"a" | 1 | true>();
+          expectTypeOf(index).toEqualTypeOf<number>();
+          expectTypeOf(array).toEqualTypeOf<readonly [1, "a", true]>();
+          return true;
+        }),
+      );
+    });
+
+    test("union of arrays", () => {
+      pipe(
+        [] as Array<boolean> | Array<string>,
+        takeWhile((item, index, array) => {
+          expectTypeOf(item).toEqualTypeOf<boolean | string>();
+          expectTypeOf(index).toEqualTypeOf<number>();
+          expectTypeOf(array).toEqualTypeOf<Array<boolean> | Array<string>>();
+          return true;
+        }),
+      );
+    });
+  });
 });
