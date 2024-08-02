785b885c72f4cb0d2a2d325ba63d69668ff5c443
Eran Hirsch <eranhirsch@gmail.com>
2024-08-02 09:36:45 +0300

    feat(chunk): literal chunk sizes (#785)
    1. Fixes #784
    2. Adds typing for literal chunk sizes.
    3. Slight optimizations to the runtime to handle cases where the chunk
    size is larger than the array, when it is 1, and to use sparse arrays to
    handle very large arrays more efficiently.

--- src/chunk.test.ts
+++ src/chunk.test.ts
@@ -2,33 +2,58 @@ import { chunk } from "./chunk";
 
 describe("data first", () => {
   test("equal size", () => {
-    expect(chunk(["a", "b", "c", "d"] as const, 2)).toEqual([
+    expect(chunk(["a", "b", "c", "d"], 2)).toEqual([
       ["a", "b"],
       ["c", "d"],
     ]);
   });
 
   test("not equal size", () => {
-    expect(chunk(["a", "b", "c", "d"] as const, 3)).toEqual([
-      ["a", "b", "c"],
-      ["d"],
-    ]);
+    expect(chunk(["a", "b", "c", "d"], 3)).toEqual([["a", "b", "c"], ["d"]]);
   });
 
   test("1 element", () => {
-    expect(chunk(["x"] as const, 3)).toEqual([["x"]]);
+    expect(chunk(["x"], 3)).toEqual([["x"]]);
   });
 
   test("empty array", () => {
-    expect(chunk([] as const, 3)).toEqual([]);
+    expect(chunk([], 3)).toEqual([]);
   });
 });
 
 describe("data last", () => {
   test("equal size", () => {
-    expect(chunk(2)(["a", "b", "c", "d"] as const)).toEqual([
+    expect(chunk(2)(["a", "b", "c", "d"])).toEqual([
       ["a", "b"],
       ["c", "d"],
     ]);
+  });
+});
+
+describe("edge-cases", () => {
+  it("throws on 0 size", () => {
+    expect(() => chunk(["a", "b", "c", "d"], 0)).toThrow(
+      "chunk: A chunk size of '0' would result in an infinite array",
+    );
+  });
+
+  it("throws on negative size", () => {
+    expect(() => chunk(["a", "b", "c", "d"], -10)).toThrow(
+      "chunk: A chunk size of '-10' would result in an infinite array",
+    );
+  });
+
+  test("size > data.length", () => {
+    const data = [1, 2, 3] as const;
+    const result = chunk(data, 10);
+    expect(result).toStrictEqual([data]);
+    // We expect the result to be a shallow clone
+    expect(result[0]).not.toBe(data);
+  });
+
+  test("chunk of size 1", () => {
+    const data = [1, 2, 3, 4, 5] as const;
+    const result = chunk(data, 1);
+    expect(result).toStrictEqual([[1], [2], [3], [4], [5]]);
   });
 });
