b2d2ea2d0767fb9d74df2f433681ab10eafc3d8d
Eran Hirsch <eranhirsch@gmail.com>
2024-08-05 17:07:53 +0300

    oops

--- src/dropLast.test.ts
+++ src/dropLast.test.ts
@@ -24,10 +24,6 @@ test("N > length", () => {
   expect(dropLast([1, 2, 3, 4, 5], 10)).toStrictEqual([]);
 });
 
-test("should drop last", () => {
-  expect(dropLast([1, 2, 3, 4, 5], 2)).toStrictEqual([1, 2, 3]);
-});
-
 test("should return a new array even if there was no drop", () => {
   const data = [1, 2, 3, 4, 5];
   const result = dropLast(data, 0);
