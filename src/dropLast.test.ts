3edb9fdcb923c6b46cbafb755caff92616dc9bdc
Eran Hirsch <eranhirsch@gmail.com>
2024-08-05 15:38:05 +0300

    fix

--- src/dropLast.test.ts
+++ src/dropLast.test.ts
@@ -1,14 +1,31 @@
 import { dropLast } from "./dropLast";
 
-test("should drop last", () => {
+test("empty array", () => {
+  expect(dropLast([], 2)).toStrictEqual([]);
+});
+
+test("N < 0", () => {
+  expect(dropLast([1, 2, 3, 4, 5], -1)).toStrictEqual([1, 2, 3, 4, 5]);
+});
+
+test("N === 0", () => {
+  expect(dropLast([1, 2, 3, 4, 5], 0)).toStrictEqual([1, 2, 3, 4, 5]);
+});
+
+test("N < length", () => {
   expect(dropLast([1, 2, 3, 4, 5], 2)).toStrictEqual([1, 2, 3]);
 });
 
-test("should not drop last", () => {
-  expect(dropLast([1, 2, 3, 4, 5], 0)).toStrictEqual([1, 2, 3, 4, 5]);
-  expect(dropLast([1, 2, 3, 4, 5], -0)).toStrictEqual([1, 2, 3, 4, 5]);
-  expect(dropLast([1, 2, 3, 4, 5], -1)).toStrictEqual([1, 2, 3, 4, 5]);
-  expect(dropLast([1, 2, 3, 4, 5], Number.NaN)).toStrictEqual([1, 2, 3, 4, 5]);
+test("N === length", () => {
+  expect(dropLast([1, 2, 3, 4, 5], 5)).toStrictEqual([]);
+});
+
+test("N > length", () => {
+  expect(dropLast([1, 2, 3, 4, 5], 10)).toStrictEqual([]);
+});
+
+test("should drop last", () => {
+  expect(dropLast([1, 2, 3, 4, 5], 2)).toStrictEqual([1, 2, 3]);
 });
 
 test("should return a new array even if there was no drop", () => {
--- src/takeLast.test.ts
+++ src/takeLast.test.ts
@@ -1,14 +1,27 @@
 import { takeLast } from "./takeLast";
 
-test("should take last", () => {
+test("empty array", () => {
+  expect(takeLast([], 2)).toStrictEqual([]);
+});
+
+test("N < 0", () => {
+  expect(takeLast([1, 2, 3, 4, 5], -1)).toStrictEqual([]);
+});
+
+test("N === 0", () => {
+  expect(takeLast([1, 2, 3, 4, 5], 0)).toStrictEqual([]);
+});
+
+test("N < length", () => {
   expect(takeLast([1, 2, 3, 4, 5], 2)).toStrictEqual([4, 5]);
 });
 
-test("should not take last", () => {
-  expect(takeLast([1, 2, 3, 4, 5], 0)).toStrictEqual([]);
-  expect(takeLast([1, 2, 3, 4, 5], -0)).toStrictEqual([]);
-  expect(takeLast([1, 2, 3, 4, 5], -1)).toStrictEqual([]);
-  expect(takeLast([1, 2, 3, 4, 5], Number.NaN)).toStrictEqual([]);
+test("N === length", () => {
+  expect(takeLast([1, 2, 3, 4, 5], 5)).toStrictEqual([1, 2, 3, 4, 5]);
+});
+
+test("N > length", () => {
+  expect(takeLast([1, 2, 3, 4, 5], 10)).toStrictEqual([1, 2, 3, 4, 5]);
 });
 
 test("should return a new array even if everything was taken", () => {
