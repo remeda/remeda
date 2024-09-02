f35d23f20769efb6d6b093aa589d62bdaa691985
Eran Hirsch <eranhirsch@gmail.com>
2024-09-02 15:50:34 +0300

    test polyfill

--- src/randomBigInt.test.ts
+++ src/randomBigInt.test.ts
@@ -48,6 +48,54 @@ test("tiny ranges with huge numbers", () => {
   }
 });
 
+describe("crypto module polyfill", () => {
+  beforeAll(() => {
+    vi.stubGlobal("crypto", undefined);
+  });
+
+  afterAll(() => {
+    vi.unstubAllGlobals();
+  });
+
+  test("non-negative bigints", () => {
+    for (const v of randomBigInts(0n, 10n)) {
+      expect(v).toBeGreaterThanOrEqual(0n);
+      expect(v).toBeLessThanOrEqual(10n);
+    }
+  });
+
+  test("negative bigints", () => {
+    for (const v of randomBigInts(-10n, -5n)) {
+      expect(v).toBeGreaterThanOrEqual(-10n);
+      expect(v).toBeLessThanOrEqual(-5n);
+    }
+  });
+
+  test("bigints with same value", () => {
+    for (const v of randomBigInts(10n, 10n)) {
+      expect(v).toBe(10n);
+    }
+  });
+
+  test("huge bigints", () => {
+    for (const v of randomBigInts(
+      // This is Number.MAX_SAFE_INTEGER + 1
+      9_007_199_254_740_992n,
+      HUGE_NUMBER,
+    )) {
+      expect(v).toBeGreaterThanOrEqual(9_007_199_254_740_992n);
+      expect(v).toBeLessThanOrEqual(HUGE_NUMBER);
+    }
+  });
+
+  test("tiny ranges with huge numbers", () => {
+    for (const v of randomBigInts(HUGE_NUMBER, HUGE_NUMBER + 1n)) {
+      expect(v).toBeGreaterThanOrEqual(HUGE_NUMBER);
+      expect(v).toBeLessThanOrEqual(HUGE_NUMBER + 1n);
+    }
+  });
+});
+
 function* randomBigInts(from: bigint, to: bigint): Generator<bigint> {
   for (let i = 0; i < ITERATIONS; i += 1) {
     yield randomBigInt(from, to);
--- src/randomBigInt.ts
+++ src/randomBigInt.ts
@@ -6,7 +6,12 @@
 /**
  * Generate a random `bigint` between `from` and `to` (inclusive).
  *
- * This function uses [`crypto.getRandomValues()`](https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues) under-the-hood; it **is** cryptographically strong.
+ * ! Important: In most environments this function uses
+ * [`crypto.getRandomValues()`](https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues)
+ * under-the-hood which **is** cryptographically strong. When the WebCrypto API
+ * isn't available (Node 18) we fallback to an implementation that uses
+ * [`Math.random()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+ * which is **NOT** cryptographically secure.
  *
  * @param from - The minimum value.
  * @param to - The maximum value.
