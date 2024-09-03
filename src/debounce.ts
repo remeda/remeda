b00d70a3cb5f664bfa276fc0bd2b3682ffcb2c05
Eran Hirsch <eranhirsch@gmail.com>
2024-09-03 10:15:02 +0300

    nit

--- src/debounce.ts
+++ src/debounce.ts
@@ -63,14 +63,27 @@ type Debouncer<
 };
 
 /**
- * Wraps `func` with a debouncer object that "debounces" (delays) invocations of the function during a defined cool-down period (`waitMs`). It can be configured to invoke the function either at the start of the cool-down period, the end of it, or at both ends (`timing`).
- * It can also be configured to allow invocations during the cool-down period (`maxWaitMs`).
- * It stores the latest call's arguments so they could be used at the end of the cool-down period when invoking `func` (if configured to invoke the function at the end of the cool-down period).
- * It stores the value returned by `func` whenever its invoked. This value is returned on every call, and is accessible via the `cachedValue` property of the debouncer. Its important to note that the value might be different from the value that would be returned from running `func` with the current arguments as it is a cached value from a previous invocation.
- * **Important**: The cool-down period defines the minimum between two invocations, and not the maximum. The period will be **extended** each time a call is made until a full cool-down period has elapsed without any additional calls.
+ * Wraps `func` with a debouncer object that "debounces" (delays) invocations of
+ * the function during a defined cool-down period (`waitMs`). It can be
+ * configured to invoke the function either at the start of the cool-down
+ * period, the end of it, or at both ends (`timing`).
+ * It can also be configured to allow invocations during the cool-down period
+ * (`maxWaitMs`).
+ * It stores the latest call's arguments so they could be used at the end of the
+ * cool-down period when invoking `func` (if configured to invoke the function
+ * at the end of the cool-down period).
+ * It stores the value returned by `func` whenever its invoked. This value is
+ * returned on every call, and is accessible via the `cachedValue` property of
+ * the debouncer. Its important to note that the value might be different from
+ * the value that would be returned from running `func` with the current
+ * arguments as it is a cached value from a previous invocation.
+ * **Important**: The cool-down period defines the minimum between two
+ * invocations, and not the maximum. The period will be **extended** each time a
+ * call is made until a full cool-down period has elapsed without any additional
+ * calls.
  *
- * @param func - The function to debounce, the returned `call` function will have
- * the exact same signature.
+ * @param func - The function to debounce, the returned `call` function will
+ * have the exact same signature.
  * @param options - An object allowing further customization of the debouncer.
  * @param options.timing -
  * - `leading` - The function is invoked at the start of the cool-down period.
@@ -99,7 +112,10 @@ type Debouncer<
  * @signature
  *   R.debounce(func, options);
  * @example
- *   const debouncer = debounce(identity(), { timing: 'trailing', waitMs: 1000 });
+ *   const debouncer = debounce(
+ *     identity(),
+ *     { timing: 'trailing', waitMs: 1000 },
+ *   );
  *   const result1 = debouncer.call(1); // => undefined
  *   const result2 = debouncer.call(2); // => undefined
  *   // after 1 second
