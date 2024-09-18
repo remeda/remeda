8d02c1fa515635c96d4d8e38ed3e4155a898f99d
crypto <homura.dev@gmail.com>
2024-09-18 10:19:01 +0900

    test: inline mapper function to make it simpler

--- src/sumBy.test-d.ts
+++ src/sumBy.test-d.ts
@@ -1,21 +1,17 @@
 import { sumBy } from "./sumBy";
-import { expectTypeOf } from "vitest";
 import { pipe } from "./pipe";
 import { constant } from "./constant";
 
-const toNumber = constant(1);
-const toBigInt = constant(1n);
-
 test("empty array", () => {
-  const result1 = sumBy([], BigInt);
+  const result1 = sumBy([], constant(1n));
   expectTypeOf(result1).toEqualTypeOf<0>();
 
-  const result2 = sumBy([], toNumber);
+  const result2 = sumBy([], constant(1));
   expectTypeOf(result2).toEqualTypeOf<0>();
 });
 
 test("disallow mixed predicate", () => {
-  const toNumberOrBigint = constant<bigint | number>(1);
+  const toNumberOrBigint = constant(1 as bigint | number);
   // @ts-expect-error [ts2769]: Type `number | bigint` is not assignable to type number
   // Type `number | bigint` is not assignable to type bigint
   sumBy([], toNumberOrBigint);
@@ -26,76 +22,79 @@ test("disallow mixed predicate", () => {
 
 describe("numbers", () => {
   test("arbitrary arrays", () => {
-    const result = sumBy([] as Array<unknown>, toNumber);
+    const result = sumBy([] as Array<unknown>, constant(1));
     expectTypeOf(result).toEqualTypeOf<number>();
   });
 
   test("arbitrary readonly arrays", () => {
-    const result = sumBy([] as ReadonlyArray<unknown>, toNumber);
+    const result = sumBy([] as ReadonlyArray<unknown>, constant(1));
     expectTypeOf(result).toEqualTypeOf<number>();
   });
 
   test("arbitrary non-empty arrays", () => {
-    const result = sumBy([1, 2] as [unknown, ...Array<unknown>], toNumber);
+    const result = sumBy([1, 2] as [unknown, ...Array<unknown>], constant(1));
     expectTypeOf(result).toEqualTypeOf<number>();
   });
 
   test("consts", () => {
-    const result = sumBy([1, 2, 3] as const, toNumber);
+    const result = sumBy([1, 2, 3] as const, constant(1));
     expectTypeOf(result).toEqualTypeOf<number>();
   });
 
   test("fixed-size tuples", () => {
-    const result = sumBy([1, 2] as [unknown, unknown], toNumber);
+    const result = sumBy([1, 2] as [unknown, unknown], constant(1));
     expectTypeOf(result).toEqualTypeOf<number>();
   });
 });
 
 describe("bigints", () => {
   test("arbitrary arrays", () => {
-    const result = sumBy([] as Array<unknown>, toBigInt);
+    const result = sumBy([] as Array<unknown>, constant(1n));
     expectTypeOf(result).toEqualTypeOf<bigint | 0>();
   });
 
   test("arbitrary readonly arrays", () => {
-    const result = sumBy([] as ReadonlyArray<unknown>, toBigInt);
+    const result = sumBy([] as ReadonlyArray<unknown>, constant(1n));
     expectTypeOf(result).toEqualTypeOf<bigint | 0>();
   });
 
   test("arbitrary non-empty arrays", () => {
-    const result = sumBy([1n, 2n] as [unknown, ...Array<unknown>], toBigInt);
+    const result = sumBy(
+      [1n, 2n] as [unknown, ...Array<unknown>],
+      constant(1n),
+    );
     expectTypeOf(result).toEqualTypeOf<bigint>();
   });
 
   test("consts", () => {
-    const result = sumBy([1n, 2n, 3n] as const, toBigInt);
+    const result = sumBy([1n, 2n, 3n] as const, constant(1n));
     expectTypeOf(result).toEqualTypeOf<bigint>();
   });
 
   test("fixed-size tuples", () => {
-    const result = sumBy([1n, 2n] as [unknown, unknown], toBigInt);
+    const result = sumBy([1n, 2n] as [unknown, unknown], constant(1n));
     expectTypeOf(result).toEqualTypeOf<bigint>();
   });
 });
 
 describe("dataLast", () => {
   test("numbers", () => {
-    const result = pipe([1, 2, 3] as const, sumBy(toNumber));
+    const result = pipe([1, 2, 3] as const, sumBy(constant(1)));
     expectTypeOf(result).toEqualTypeOf<number>();
   });
 
   test("bigints", () => {
-    const result = pipe([1n, 2n, 3n] as const, sumBy(toBigInt));
+    const result = pipe([1n, 2n, 3n] as const, sumBy(constant(1n)));
     expectTypeOf(result).toEqualTypeOf<bigint>();
   });
 
   test("empty array number", () => {
-    const result = pipe([] as const, sumBy(toNumber));
+    const result = pipe([] as const, sumBy(constant(1)));
     expectTypeOf(result).toEqualTypeOf<0>();
   });
 
   test("empty array bigint", () => {
-    const result = pipe([] as const, sumBy(toBigInt));
+    const result = pipe([] as const, sumBy(constant(1n)));
     expectTypeOf(result).toEqualTypeOf<0>();
   });
 });
