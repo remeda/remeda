cfdbb9d9cea69199ecb18fee5847b59c2ad9bce8
Grigoris Christainas <grigorisxristainas@gmail.com>
2024-10-28 16:55:09 +0200

    refactor(mean): drop `bigint` support (#898)
    Related to #874, I have also integrated changes based on recent comments
    about `median` (e.g., removed type-guard related tests and modified the
    `Mean` type)
    
    Please let me know if any additional adjustments are needed.

--- src/mean.test-d.ts
+++ src/mean.test-d.ts
@@ -1,5 +1,3 @@
-import { hasAtLeast } from "./hasAtLeast";
-import { isEmpty } from "./isEmpty";
 import { mean } from "./mean";
 import { pipe } from "./pipe";
 
@@ -8,7 +6,7 @@ test("empty arrays", () => {
   expectTypeOf(result).toEqualTypeOf<undefined>();
 });
 
-describe("numbers", () => {
+describe("dataFirst", () => {
   test("arbitrary arrays", () => {
     const result = mean([] as Array<number>);
     expectTypeOf(result).toEqualTypeOf<number | undefined>();
@@ -35,42 +33,10 @@ describe("numbers", () => {
   });
 });
 
-describe("bigints", () => {
-  test("arbitrary arrays", () => {
-    const result = mean([] as Array<bigint>);
-    expectTypeOf(result).toEqualTypeOf<number | undefined>();
-  });
-
-  test("arbitrary readonly arrays", () => {
-    const result = mean([] as ReadonlyArray<bigint>);
-    expectTypeOf(result).toEqualTypeOf<number | undefined>();
-  });
-
-  test("arbitrary non-empty arrays", () => {
-    const result = mean([1n, 2n] as [bigint, ...Array<bigint>]);
-    expectTypeOf(result).toEqualTypeOf<bigint>();
-  });
-
-  test("consts", () => {
-    const result = mean([1n, 2n, 3n] as const);
-    expectTypeOf(result).toEqualTypeOf<bigint>();
-  });
-
-  test("fixed-size tuples", () => {
-    const result = mean([1n, 2n] as [bigint, bigint]);
-    expectTypeOf(result).toEqualTypeOf<bigint>();
-  });
-});
-
 describe("dataLast", () => {
   test("numbers", () => {
     const result = pipe([1, 2, 3] as const, mean());
     expectTypeOf(result).toEqualTypeOf<number>();
-  });
-
-  test("bigints", () => {
-    const result = pipe([1n, 2n, 3n] as const, mean());
-    expectTypeOf(result).toEqualTypeOf<bigint>();
   });
 
   test("arbitrary number arrays", () => {
@@ -82,35 +48,4 @@ describe("dataLast", () => {
     const result = pipe([] as ReadonlyArray<number>, mean());
     expectTypeOf(result).toEqualTypeOf<number | undefined>();
   });
-
-  test("arbitrary bigint arrays", () => {
-    const result = pipe([] as Array<bigint>, mean());
-    expectTypeOf(result).toEqualTypeOf<number | undefined>();
-  });
-
-  test("arbitrary readonly bigint arrays", () => {
-    const result = pipe([] as ReadonlyArray<bigint>, mean());
-    expectTypeOf(result).toEqualTypeOf<number | undefined>();
-  });
-});
-
-describe("type-guards", () => {
-  it("narrows to `number` using `hasAtLeast`", () => {
-    const data = [] as Array<number>;
-    if (hasAtLeast(data, 1)) {
-      expectTypeOf(mean(data)).toEqualTypeOf<number>();
-    }
-  });
-
-  it("narrows to `undefined` using `isEmpty`", () => {
-    const data = [] as Array<number>;
-    if (isEmpty(data)) {
-      expectTypeOf(mean(data)).toEqualTypeOf<undefined>();
-    }
-  });
-});
-
-it("doesn't allow mixed arrays", () => {
-  // @ts-expect-error [ts2345] - Can't mean bigints and numbers...
-  mean([1, 2n]);
 });
