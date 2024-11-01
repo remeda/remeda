6a4bd9a5a0932eb31bb317f5927c3771337346aa
Eran Hirsch <eranhirsch@gmail.com>
2024-11-01 13:50:23 +0200

    testing

--- src/funnel.reference-batch.test.ts
+++ src/funnel.reference-batch.test.ts
@@ -1,15 +1,48 @@
-/* eslint-disable unicorn/no-object-as-default-parameter, @typescript-eslint/no-unused-vars, @typescript-eslint/explicit-function-return-type, @typescript-eslint/use-unknown-in-catch-callback-variable, @typescript-eslint/no-explicit-any, @typescript-eslint/prefer-readonly-parameter-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type,  @typescript-eslint/no-explicit-any,  @typescript-eslint/prefer-readonly-parameter-types, @typescript-eslint/require-await,  @typescript-eslint/use-unknown-in-catch-callback-variable, jsdoc/require-example, unicorn/no-object-as-default-parameter --
+ * These aren't useful for a reference implementation!
+ */
 
+import { fromKeys } from "./fromKeys";
 import { funnel } from "./funnel";
 
 type PromiseCallbacks<T> = Parameters<
   ConstructorParameters<typeof Promise<T>>[0]
 >;
 
-// @ts-expect-error [ts(6133)] -- soon...
-function batch<Params extends Array<any>, Response, Result>(
-  executor: (params: ReadonlyArray<Params>) => Promise<Response>,
-  extractor: (result: Response, ...params: Params) => Result,
+/**
+ * A reference implementation for a  batching utility function built on top of
+ * the `funnel` general purpose execution utility function. It will accumulate
+ * all params passed to an async `call` method within the defined timing policy
+ * and then use an async executor to process them in one invocation. It then
+ * extracts an individual result for each call which is used to resolve the
+ * original call.
+ *
+ * This allows synchronizing multiple async calls while keeping each call site
+ * isolated from the rest (for example, as react components).
+ *
+ * This reference implementation can be copied into your project as-is, or you
+ * can use it as the basis for a more complex implementation with additional
+ * features.
+ *
+ * @param executor - The main function that takes a batch and returns an
+ * aggregated response. The typing for the it's parameters will derive the
+ * typing for the extractor and the `call` method.
+ * @param extractor - A function that takes the aggregated response and extracts
+ * from it the result for each individual call. The function is called with both
+ * the index in the batch, and the params passed to the `call` method. This
+ * allows handling APIs that return batch results as both objects and arrays.
+ * @param timingPolicy - Same as the param for `funnel`, it defines the
+ * time frames where the batch will accumulate calls and when it would trigger.
+ * @returns A Funnel object with the `call` method augmented to support async
+ * response.
+ */
+function batch<Params extends Array<any>, BatchResponse, Result>(
+  executor: (requests: ReadonlyArray<Params>) => Promise<BatchResponse>,
+  extractor: (
+    response: BatchResponse,
+    index: number,
+    ...params: Params
+  ) => Result,
   timingPolicy: Parameters<typeof funnel>[2] = {
     invokedAt: "end",
     burstCoolDownMs: 0,
@@ -36,11 +69,14 @@ function batch<Params extends Array<any>, Response, Result>(
         // On success we iterate again over all calls and allow the extractor
         // to pull a value out of the aggregated response for each one.
         .then((response) => {
-          for (const {
-            params,
-            promiseCallbacks: [resolve],
-          } of batched) {
-            const result = extractor(response, ...params);
+          for (const [
+            index,
+            {
+              params,
+              promiseCallbacks: [resolve],
+            },
+          ] of batched.entries()) {
+            const result = extractor(response, index, ...params);
             resolve(result);
           }
         })
@@ -67,3 +103,81 @@ function batch<Params extends Array<any>, Response, Result>(
       }),
   };
 }
+
+describe("showcase", () => {
+  test("results as object", async () => {
+    const mockApi = vi.fn(
+      async (words: ReadonlyArray<string>): Promise<Record<string, number>> =>
+        fromKeys(words, (word) => word.length),
+    );
+
+    const countLettersApi = batch(
+      // We only need to type the `requests` param of the `executor` callback.
+      // All other types are derived from it.
+      async (requests: ReadonlyArray<[word: string]>) =>
+        await mockApi(requests.flat()),
+      (response, _, word) => response[word],
+    );
+
+    const prepared = [
+      countLettersApi.call("short"),
+      countLettersApi.call("medium"),
+      countLettersApi.call("loooooooooooooong"),
+    ];
+
+    expect(mockApi).toHaveBeenCalledTimes(0);
+
+    const [shortResult, mediumResult, longResult] = await Promise.all(prepared);
+
+    expect(mockApi).toHaveBeenCalledTimes(1);
+    expect(mockApi).toHaveBeenLastCalledWith([
+      "short",
+      "medium",
+      "loooooooooooooong",
+    ]);
+    expect(mockApi).toHaveLastResolvedWith({
+      short: 5,
+      medium: 6,
+      loooooooooooooong: 17,
+    });
+    expect(shortResult).toBe(5);
+    expect(mediumResult).toBe(6);
+    expect(longResult).toBe(17);
+  });
+
+  test("results as array", async () => {
+    const mockApi = vi.fn(
+      async (words: ReadonlyArray<string>): Promise<ReadonlyArray<number>> =>
+        words.map((word) => word.length),
+    );
+
+    const countLettersApi = batch(
+      // We only need to type the `requests` param of the `executor` callback.
+      // All other types are derived from it.
+      async (requests: ReadonlyArray<[word: string]>) =>
+        await mockApi(requests.flat()),
+      (response, index) => response[index],
+    );
+
+    const prepared = [
+      countLettersApi.call("short"),
+      countLettersApi.call("medium"),
+      countLettersApi.call("loooooooooooooong"),
+    ];
+
+    expect(mockApi).toHaveBeenCalledTimes(0);
+
+    const [shortResult, mediumResult, longResult] = await Promise.all(prepared);
+
+    expect(mockApi).toHaveBeenCalledTimes(1);
+    expect(mockApi).toHaveBeenLastCalledWith([
+      "short",
+      "medium",
+      "loooooooooooooong",
+    ]);
+    expect(mockApi).toHaveLastResolvedWith([5, 6, 17]);
+    expect(shortResult).toBe(5);
+    expect(mediumResult).toBe(6);
+    expect(longResult).toBe(17);
+  });
+});
