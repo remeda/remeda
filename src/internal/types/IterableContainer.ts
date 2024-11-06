f38bc155f0dbd6fe8b09febf7f04b9ea5dbc501b
Eran Hirsch <eranhirsch@gmail.com>
2024-11-06 13:39:22 +0200

    nits

--- src/internal/types/IterableContainer.ts
+++ src/internal/types/IterableContainer.ts
@@ -4,9 +4,10 @@
  * and of the form [x, ...y[]], etc...), and their readonly equivalent. This
  * allows us to be more inclusive to what functions can process.
  *
- * @example map<T extends ArrayLike>(items: T) { ... }
+ * @example
+ *   function map<T extends IterableContainer>(items: T) { ... }
  *
  * We would've named this `ArrayLike`, but that's already used by typescript...
  * @see This was inspired by the type-definition of Promise.all (https://github.com/microsoft/TypeScript/blob/1df5717b120cddd325deab8b0f2b2c3eecaf2b01/src/lib/es2015.promise.d.ts#L21)
  */
-export type IterableContainer<T = unknown> = ReadonlyArray<T> | readonly [];
+export type IterableContainer<T = unknown> = ReadonlyArray<T> | [];
