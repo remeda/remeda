3fc28da58af55bac0af5161748dee0da5346839f
Eran Hirsch <eranhirsch@gmail.com>
2024-11-15 20:13:31 +0200

    fix(pick): support optional keys (#923)
    Expanding our TupleParts utility to allow easy access to the optional
    part of the prefix, and then using this to improve the type of pick so
    that optional keys are marked as optional in the output.
    
    Closes: #911

--- src/internal/types/TupleParts.ts
+++ src/internal/types/TupleParts.ts
@@ -2,37 +2,53 @@ import type { IsEqual } from "type-fest";
 
 /**
  * Takes an array and returns the types that make up its parts. The prefix is
- * anything before the rest parameter (if any), the suffix is anything after
- * the rest parameter (if any), and the item is the type of the rest parameter.
+ * anything before the rest parameter (if any), split between it's required
+ * part, and it's optional part. The suffix is anything after the rest
+ * parameter (if any), and the item is the type of the rest parameter.
+ *
+ * NOTE: The prefix is split into 2 tuples where all items are non-optional so
+ * that types that rely on the presence of a specific element can be built more
+ * accurately.
+ *
+ * TODO: Some existing types use the `prefix` accessor which doesn't handle the optional part correctly. We need to fix each of those types before we can remove it.
  *
  * The output could be used to reconstruct the input: `[
- *   ...TupleParts<T>["prefix"],
- *   ...Array<TupleParts<T>["item"]>,
+ *   ...TupleParts<T>["required"],
+ *   ...Partial<TupleParts<T>["optional"]>,
+ *   ...CoercedArray<TupleParts<T>["item"]>,
  *   ...TupleParts<T>["suffix"],
  * ]`.
  */
 export type TupleParts<
   T,
-  Prefix extends Array<unknown> = [],
+  PrefixRequired extends Array<unknown> = [],
+  PrefixOptionals extends Array<unknown> = [],
   Suffix extends Array<unknown> = [],
 > = T extends readonly [infer Head, ...infer Tail]
-  ? TupleParts<Tail, [...Prefix, Head], Suffix>
+  ? TupleParts<Tail, [...PrefixRequired, Head], PrefixOptionals, Suffix>
   : T extends readonly [...infer Head, infer Tail]
-    ? TupleParts<Head, Prefix, [Tail, ...Suffix]>
+    ? TupleParts<Head, PrefixRequired, PrefixOptionals, [Tail, ...Suffix]>
     : // We need to distinguish between e.g. [number? ...Array<string>] and
       // Array<number | string>.
-      IsTupleRestOnly<T> extends false
-      ? // This is the [number? ...Array<string>] case.
-        T extends readonly [(infer MaybeHead)?, ...infer Tail]
-        ? TupleParts<Tail, [...Prefix, MaybeHead?], Suffix>
-        : never
-      : // This is the Array<number | string> case.
+      IsTupleRestOnly<T> extends true
+      ? // This is the Array<number | string> case.
         T extends ReadonlyArray<infer Item>
         ? {
-            prefix: Prefix;
+            prefix: [...PrefixRequired, ...Partial<PrefixOptionals>];
+            required: PrefixRequired;
+            optional: PrefixOptionals;
             item: Item;
             suffix: Suffix;
           }
+        : never
+      : // This is the [number? ...Array<string>] case.
+        T extends readonly [(infer OptionalHead)?, ...infer Tail]
+        ? TupleParts<
+            Tail,
+            PrefixRequired,
+            [...PrefixOptionals, OptionalHead],
+            Suffix
+          >
         : never;
 
 /**
