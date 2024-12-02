5f822a72c7a6f2c38b3f356845be52f045eedddf
Masyaka <maestroz89@gmail.com>
2024-12-02 15:36:23 +0200

    test: Added tests for standalone prop

--- src/prop.test-d.ts
+++ src/prop.test-d.ts
@@ -3,11 +3,27 @@ import { prop } from "./prop";
 import { sortBy } from "./sortBy";
 
 test("prop typing", () => {
-  const input = [{ a: 1 }];
+  const item = { a: 1 };
+  const input = [item];
 
   const works = sortBy(input, prop("a"));
   expectTypeOf(works).toEqualTypeOf<typeof input>();
 
   const doesntWork = pipe(input, sortBy(prop("a")));
   expectTypeOf(doesntWork).toEqualTypeOf<typeof input>();
+});
+
+test("prop typing standalone", () => {
+  const item = { a: 1 };
+  const input = [item, { a: 2 }];
+  const standAlonePropA = prop("a");
+  const valueA = standAlonePropA(item);
+  expectTypeOf(valueA).toEqualTypeOf<number>();
+  const valuesA = input.map(standAlonePropA);
+  expectTypeOf(valuesA).toEqualTypeOf<Array<number>>();
+
+  const standAlonePropB = prop("b");
+  // @ts-expect-error - b is not a key of typeof item
+  const valueB = standAlonePropB(item);
+  expectTypeOf(valueB).toEqualTypeOf<unknown>();
 });
--- src/prop.test.ts
+++ src/prop.test.ts
@@ -1,3 +1,4 @@
+import { indexBy } from "./indexBy";
 import { pipe } from "./pipe";
 import { prop } from "./prop";
 
@@ -5,4 +6,16 @@ test("prop", () => {
   const result = pipe({ foo: "bar" }, prop("foo"));
 
   expect(result).toBe("bar");
+});
+
+test("prop standalone", () => {
+  const input = [{ a: 1 }, { a: 2 }];
+  const standAlonePropA = prop("a");
+  const byPropA = pipe(input, indexBy(standAlonePropA));
+
+  expect(byPropA).eqls({ "1": { a: 1 }, "2": { a: 2 } });
+
+  const byPropADataFirst = indexBy(input, standAlonePropA);
+
+  expect(byPropADataFirst).eqls({ "1": { a: 1 }, "2": { a: 2 } });
 });
