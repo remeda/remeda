310bf2c4601dfc3efe32d7a0b073310efbbce7ba
Eran Hirsch <eranhirsch@gmail.com>
2024-12-16 18:06:09 +0200

    renames

--- src/countBy.ts
+++ src/countBy.ts
@@ -3,46 +3,52 @@ import type { ExactRecord } from "./internal/types/ExactRecord";
 
 /**
  * Categorize and count elements in an array using a defined callback function.
- * The transformation function applies to each element in the array to
- * determine its category and then counts how many elements fall into
- * each category.
+ * The callback function is applied to each element in the array to determine
+ * its category and then counts how many elements fall into each category.
  *
  * @param data - The array.
- * @param fn - The transformation function.
+ * @param categorizationFn - The categorization function.
  * @signature
- *   R.countBy(data, fn)
+ *   R.countBy(data, categorizationFn)
  * @example
  *    R.countBy(
  *      ["a", "b", "c", "B", "A", "a"],
  *      R.toLowerCase()
- *    ) // { a: 3, b: 2, c: 1 }
+ *    ); //=> { a: 3, b: 2, c: 1 }
  * @dataFirst
  * @category Array
  */
 export function countBy<T, K extends PropertyKey>(
   data: ReadonlyArray<T>,
-  fn: (value: T, index: number, data: ReadonlyArray<T>) => K | undefined,
+  categorizationFn: (
+    value: T,
+    index: number,
+    data: ReadonlyArray<T>,
+  ) => K | undefined,
 ): ExactRecord<K, number>;
 
 /**
  * Categorize and count elements in an array using a defined callback function.
- * The transformation function applies to each element in the array to
- * determine its category and then counts how many elements fall into
- * each category.
+ * The callback function is applied to each element in the array to determine
+ * its category and then counts how many elements fall into each category.
  *
- * @param fn - The transformation function.
+ * @param categorizationFn - The categorization function.
  * @signature
- *   R.countBy(fn)(data)
+ *   R.countBy(categorizationFn)(data)
  * @example
  *    R.pipe(
  *      ["a", "b", "c", "B", "A", "a"],
- *      R.countBy(R.toLowerCase())
- *    ) // { a: 3, b: 2, c: 1 }
+ *      R.countBy(R.toLowerCase()),
+ *    ); //=> { a: 3, b: 2, c: 1 }
  * @dataLast
  * @category Array
  */
 export function countBy<T, K extends PropertyKey>(
-  fn: (value: T, index: number, data: ReadonlyArray<T>) => K | undefined,
+  categorizationFn: (
+    value: T,
+    index: number,
+    data: ReadonlyArray<T>,
+  ) => K | undefined,
 ): (data: ReadonlyArray<T>) => ExactRecord<K, number>;
 
 export function countBy(...args: ReadonlyArray<unknown>): unknown {
@@ -51,7 +57,7 @@ export function countBy(...args: ReadonlyArray<unknown>): unknown {
 
 const countByImplementation = <T>(
   data: ReadonlyArray<T>,
-  fn: (
+  categorizationFn: (
     value: T,
     index: number,
     data: ReadonlyArray<T>,
@@ -60,10 +66,10 @@ const countByImplementation = <T>(
   const out: ExactRecord<PropertyKey, number> = {};
 
   for (const [index, item] of data.entries()) {
-    const key = fn(item, index, data);
-    if (key !== undefined) {
-      out[key] ??= 0;
-      out[key] += 1;
+    const category = categorizationFn(item, index, data);
+    if (category !== undefined) {
+      out[category] ??= 0;
+      out[category] += 1;
     }
   }
 
