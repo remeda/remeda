19a677d0f3d7b2277b615bc41e62bde401cb161e
tbui17 <86227911+tbui17@users.noreply.github.com>
2025-01-18 22:17:28 -0700

    change internal signature

--- src/mergeAll.ts
+++ src/mergeAll.ts
@@ -58,15 +58,12 @@ export function mergeAll<T extends object>(
 export function mergeAll<T extends IterableContainer<object>>(
   objects: T,
 ): MergeAll<T>;
-export function mergeAll<T extends IterableContainer<object>>(
-  objects: T,
-): MergeAll<T> {
+export function mergeAll(objects: ReadonlyArray<object>): object {
   let out = {};
 
   for (const item of objects) {
     out = { ...out, ...item };
   }
 
-  // @ts-expect-error the return type will work out - see the type tests.
   return out;
 }
