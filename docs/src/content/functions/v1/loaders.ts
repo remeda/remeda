5a5a70f8fbae6724b62e6fdc621b3155438df4dd
Eran Hirsch <eranhirsch@gmail.com>
2025-01-28 16:53:34 +0200

    throws

--- docs/src/content/functions/v1/loaders.ts
+++ docs/src/content/functions/v1/loaders.ts
@@ -5,6 +5,7 @@ import { evolve, isNullish, map, piped, prop, when } from "remeda";
 import invariant from "tiny-invariant";
 import { type JSONOutput } from "typedoc";
 import dataFilePath from "./functions.json?url";
+import { throws } from "@/lib/throws";
 
 const DATA_FILE = path.join(import.meta.dirname, path.basename(dataFilePath));
 
@@ -12,11 +13,12 @@ export const functionsLoader = file(DATA_FILE, {
   parser: piped(
     ($) => JSON.parse($) as JSONOutput.ProjectReflection,
     prop("children"),
-    when(isNullish, () => {
-      throw new Error(
+    when(
+      isNullish,
+      throws(
         `Data file ${DATA_FILE} is missing any declarations or references`,
-      );
-    }),
+      ),
+    ),
     map(evolve({ id: toString() })),
   ),
 });
@@ -25,9 +27,7 @@ export const categoriesLoader = file(DATA_FILE, {
   parser: piped(
     ($) => JSON.parse($) as JSONOutput.ProjectReflection,
     prop("categories"),
-    when(isNullish, () => {
-      throw new Error(`Data file ${DATA_FILE} is missing any categories`);
-    }),
+    when(isNullish, throws(`Data file ${DATA_FILE} is missing any categories`)),
     map(({ title: id, children }) => {
       invariant(children !== undefined, `Category ${id} has no children?!`);
       // Astro expects reference types to be strings although it allows ids to be `numbers` :(
