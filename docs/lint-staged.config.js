dd27a10d3ac42661959ef8d22b8896c38e9c3eb8
Eran Hirsch <eran.ext@darrow.ai>
2025-02-03 08:08:56 +0200

    also astro

--- docs/lint-staged.config.js
+++ docs/lint-staged.config.js
@@ -6,12 +6,12 @@ export default {
   "*.@(ts|tsx|astro)": () => "npm run check",
 
   // Javascript and Typescript (including commonJs and esm variants)
-  "*.@(js|jsx|ts|tsx|cjs|mjs)": ["eslint --fix", "prettier --write"],
+  "*.@(js|jsx|ts|tsx|cjs|mjs|astro)": ["eslint --fix", "prettier --write"],
 
   // _try_ prettier on anything, it supports a lot of things! If this is touching a file
   // you don't want add it to .prettierignore.
   // lint-staged runs *all* matching rules and doesn't have an "anything else" rule, so
   // instead, we add all previous file extensions to the glob here so it doesn't run on
   // any of those files.
-  "!(*.@(js|jsx|ts|tsx|cjs|mjs))": "prettier --ignore-unknown --write",
+  "!(*.@(js|jsx|ts|tsx|cjs|mjs|astro))": "prettier --ignore-unknown --write",
 };
