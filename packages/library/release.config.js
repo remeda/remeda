0364ae6d1e544d90801ab5e7f80e4cad45412a7a
Eran Hirsch <eranhirsch@gmail.com>
2025-02-20 20:38:53 +0200

    fix release

--- packages/library/release.config.js
+++ packages/library/release.config.js
@@ -1,4 +1,10 @@
 /** @type {Partial<import('semantic-release').GlobalConfig>} */
 export default {
   branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", { pkgName: "remeda" }],
+    "@semantic-release/github",
+  ],
 };
