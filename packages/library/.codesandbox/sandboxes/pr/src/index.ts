b49c134e3b92ec552a35f75daaf5a6114893daa9
Eran Hirsch <eranhirsch@gmail.com>
2025-02-25 17:31:07 +0200

    move sandbox

---
+++ packages/library/.codesandbox/sandboxes/pr/src/index.ts
@@ -0,0 +1,12 @@
+import "./styles.css";
+import * as R from "@remeda/library";
+
+const DATA = ["a", "b", "c"];
+
+const result = R.map(DATA, (x) => x.toUpperCase());
+
+document.querySelector("#app")!.innerHTML = `
+<h1>Hello Remeda 👋</h1>
+<code>
+  ${JSON.stringify(result)}
+</code>`;
