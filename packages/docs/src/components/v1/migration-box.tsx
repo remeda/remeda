dd34e973c1ff5b17327c509ad30afd3e1dfd777d
Eran Hirsch <eranhirsch@gmail.com>
2025-02-26 21:14:38 +0200

    codemod

--- packages/docs/src/components/v1/migration-box.tsx
+++ packages/docs/src/components/v1/migration-box.tsx
@@ -13,7 +13,7 @@ export function MigrationBox({
   readonly children: ReactNode;
 }): ReactNode {
   return (
-    <Collapsible className="flex flex-col gap-8 rounded-md border border-sky-100 bg-gradient-to-tl from-sky-50 to-sky-100 p-3 shadow-sm dark:border-sky-900 dark:from-sky-900 dark:to-sky-950">
+    <Collapsible className="flex flex-col gap-8 rounded-md border border-sky-100 bg-linear-to-tl from-sky-50 to-sky-100 p-3 shadow-xs dark:border-sky-900 dark:from-sky-900 dark:to-sky-950">
       <div className="relative flex items-center">
         <h6 className="text-sm font-semibold uppercase tracking-wider text-neutral-800 dark:text-neutral-200">
           Breaking changes in v2
