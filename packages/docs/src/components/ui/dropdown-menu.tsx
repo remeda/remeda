80a9f3c84c673903ddaf25e1bd69982ce00715f9
Eran Hirsch <eranhirsch@gmail.com>
2025-02-27 12:25:09 +0200

    implicit

--- packages/docs/src/components/ui/dropdown-menu.tsx
+++ packages/docs/src/components/ui/dropdown-menu.tsx
@@ -84,7 +84,6 @@ function DropdownMenuItem({
 function DropdownMenuCheckboxItem({
   className,
   children,
-  checked,
   ...props
 }: ComponentProps<typeof DropdownMenuPrimitive.CheckboxItem>) {
   return (
@@ -94,7 +93,6 @@ function DropdownMenuCheckboxItem({
         "focus:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-sm py-1.5 pr-2 pl-8 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
-      checked={checked}
       {...props}
     >
       <span className="pointer-events-none absolute left-2 flex size-3.5 items-center justify-center">
