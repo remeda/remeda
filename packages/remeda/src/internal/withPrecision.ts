a1d160ff29e44c32c337f283b6acd704fa3e085c
Eran Hirsch <eranhirsch@gmail.com>
2025-03-12 16:22:23 +0200

    nit

--- packages/remeda/src/internal/withPrecision.ts
+++ packages/remeda/src/internal/withPrecision.ts
@@ -1,3 +1,7 @@
+// ECMAScript doesn't support more decimal places than 15 anyways,
+// so supporting higher values doesn't make a lot of sense,
+// Considering Number.MAX_SAFE_INTEGER < 10**16, we can use -15
+// for the negative precision limit, too.
 const MAX_PRECISION = 15;
 
 const RADIX = 10;
