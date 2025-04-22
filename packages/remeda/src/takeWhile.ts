6223c373f0c0997fe4647b2b6a14ee6f4a8439eb
Alex Vukadinov <alexvuka1@gmail.com>
2025-04-22 12:37:45 +0100

    feat: improve takeWhile type inference

--- packages/remeda/src/takeWhile.ts
+++ packages/remeda/src/takeWhile.ts
@@ -13,6 +13,10 @@ import { purry } from "./purry";
  * @dataFirst
  * @category Array
  */
+export function takeWhile<T extends IterableContainer, S extends T>(
+  data: T, 
+  predicate: (item: T[number], index: number, data: T) => item is S[number]
+): Array<S[number]>;
 export function takeWhile<T extends IterableContainer>(
   data: T,
   predicate: (item: T[number], index: number, data: T) => boolean,
@@ -29,9 +33,13 @@ export function takeWhile<T extends IterableContainer>(
  * @dataLast
  * @category Array
  */
+export function takeWhile<T extends IterableContainer, S extends T>(
+  predicate: (item: T[number], index: number, data: T) => item is S[number]
+): (array: T) => Array<S[number]>;
 export function takeWhile<T extends IterableContainer>(
   predicate: (item: T[number], index: number, data: T) => boolean,
 ): (array: T) => Array<T[number]>;
+
 
 export function takeWhile(...args: ReadonlyArray<unknown>): unknown {
   return purry(takeWhileImplementation, args);
