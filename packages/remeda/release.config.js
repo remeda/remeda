f512dca9d7d401f8f4d78f74d2adfc8188bc43b1
Eran Hirsch <eranhirsch@gmail.com>
2025-05-26 10:14:43 +0300

    fix

--- packages/remeda/release.config.js
+++ packages/remeda/release.config.js
@@ -1,4 +1,19 @@
 /** @type {Partial<import('semantic-release').GlobalConfig>} */
 export default {
   branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
 };
