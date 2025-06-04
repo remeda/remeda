78fab668a1a0608be74e67240c68229b6933b760
Eran Hirsch <eranhirsch@gmail.com>
2025-06-04 10:06:30 +0300

    nits

---
+++ packages/docs/src/lib/route-navbar-entries.ts
@@ -0,0 +1,38 @@
+import { forNavbar, getFunctions } from "./functions/entries";
+import {
+  getMigrationMappings,
+  forNavbar as migrationMappingsForNavBar,
+} from "./mapping/entries";
+import {
+  getV1Functions,
+  forNavbar as v1FunctionsForNavbar,
+} from "./v1/entries";
+
+/**
+ * The mobile navbar is part of the header which is part of the general layout
+ * and doesn't have access to the data in the specific pages. This means we need
+ * to sync the logic for what entries it should show with the routing logic for
+ * our pages.
+ */
+export async function getRouteNavbarEntries(
+  pathname: string,
+  migrationLibrary: string | undefined,
+) {
+  if (migrationLibrary !== undefined) {
+    return migrationMappingsForNavBar(
+      await getMigrationMappings(migrationLibrary),
+    );
+  }
+
+  switch (pathname) {
+    // Keep in sync with the routes available in the pages directory.
+    case "/v1":
+      return v1FunctionsForNavbar(await getV1Functions());
+
+    case "/docs":
+      return forNavbar(await getFunctions());
+
+    default:
+      return [];
+  }
+}
