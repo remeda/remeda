4e88d44adb8aa3b3dfe0c04f916a30ed7b51595c
Eran Hirsch <eranhirsch@gmail.com>
2025-06-04 11:17:23 +0300

    update

--- packages/docs/src/components/ui/dropdown-menu.tsx
+++ packages/docs/src/components/ui/dropdown-menu.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { cn } from "@/lib/utils";
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { CheckIcon, ChevronRightIcon, CircleIcon } from "lucide-react";
