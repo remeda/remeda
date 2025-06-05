c1d22ecbd18442de817b00af7940c3b27b4a491b
Eran Hirsch <eranhirsch@gmail.com>
2025-06-05 15:29:39 +0300

    attempt 2

---
+++ packages/stackblitz-template/README.md
@@ -0,0 +1,1 @@
+Testing out a new template for stackblitz PRs.
---
+++ packages/stackblitz-template/prettier.config.js
@@ -0,0 +1,4 @@
+/** @type {import('prettier').Config} */
+export default {
+  plugins: ["prettier-plugin-pkg", "prettier-plugin-sh"],
+};
