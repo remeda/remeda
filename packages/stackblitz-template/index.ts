406292d8591b84eee15e88f61ed7b8060144a147
Eran Hirsch <eranhirsch@gmail.com>
2025-06-06 11:13:12 +0300

    wip

--- packages/stackblitz-template/index.ts
+++
@@ -1,8 +0,0 @@
-import { map } from "remeda";
-
-const DATA = [1, 2, 3] as const;
-
-const result = map(DATA, (x) => x * 2);
-//    ^?
-
-console.log(result);
---
+++ packages/stackblitz-template/playground.ts
@@ -0,0 +1,3 @@
+import * as R from "remeda";
+
+R.map([1, 2, 3] as const, (x) => x * 2); // =>
