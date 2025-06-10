5e550d5d67f353faca4581ef11af68362ff12e4f
Eran Hirsch <eranhirsch@gmail.com>
2025-06-10 13:34:45 +0300

    with playground

--- packages/stackblitz-template/playground.ts
+++
@@ -1,3 +0,0 @@
-import * as R from "remeda";
-
-R.map([1, 2, 3] as const, (x) => x * 2); // =>
---
+++ packages/stackblitz-template/src/playground.ts
@@ -0,0 +1,16 @@
+// Welcome to the Remeda TypeScript Playground. Use this file to test out how
+// Remeda utilities enforce input parameters and how it shapes the output.
+
+// This file is typechecked with the same TypeScript configuration that the
+// TypeScript Playground at https://typescriptlang.org/play uses; this is not
+// the same as the configuration used internally by Remeda, and it isn't our
+// recommended configuration; we highly recommend enabling
+// `exactOptionalPropertyTypes`. The configuration can be modified in
+// `tsconfig.playground.json` in the root of this project.
+
+import { filter } from "remeda";
+
+const DATA = ["hello", 123, "world", 456] as const;
+
+const result = filter(DATA, ($) => typeof $ === "string");
+//    ^?
