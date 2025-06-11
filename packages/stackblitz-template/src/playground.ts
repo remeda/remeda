21bf0d7d9da3fff2fb039fe9b0591ab5fc17c2c9
Eran Hirsch <eranhirsch@gmail.com>
2025-06-11 15:11:46 +0300

    fixes

--- packages/stackblitz-template/src/playground.ts
+++ packages/stackblitz-template/src/playground.ts
@@ -7,9 +7,10 @@
  * This file is typechecked with the same TypeScript configuration that the
  * [TypeScript's Playground](https://typescriptlang.org/play) uses; this is not
  * the same as the configuration used internally by Remeda, and it isn't our
- * recommended configuration; we highly recommend enabling
- * `exactOptionalPropertyTypes`. The configuration can be modified in
+ * recommended configuration. The configuration can be modified in
  * `tsconfig.playground.json` in the root of this project.
+ *
+ * !We highly recommend enabling `exactOptionalPropertyTypes`!
  */
 
 import { filter } from "remeda";
@@ -17,4 +18,3 @@ import { filter } from "remeda";
 const DATA = ["hello", 123, "world", 456] as const;
 
 const result = filter(DATA, ($) => typeof $ === "string");
-//    ^?
