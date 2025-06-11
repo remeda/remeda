f6d4c3beb526f3348890cda9fa1d832c74eb9d8c
Eran Hirsch <eranhirsch@gmail.com>
2025-06-11 10:20:39 +0300

    cleanup

--- packages/pr-sandbox/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { doSomething } from "./do-something";
-import "./styles.css";
-
-document.querySelector("#app")!.innerHTML = `
-  <h1>Hello Remeda 👋</h1>
-  <code>${JSON.stringify(doSomething())}</code>
-`;
