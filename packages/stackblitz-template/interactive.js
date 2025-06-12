748f1a1e38342b8c3cc4cf7161415bb9deeeeed3
Eran Hirsch <eranhirsch@gmail.com>
2025-06-12 10:30:11 +0300

    even less lines

--- packages/stackblitz-template/interactive.js
+++ packages/stackblitz-template/interactive.js
@@ -2,16 +2,12 @@ import { start } from "node:repl";
 
 const repl = start({ prompt: "" });
 
-console.log("Loading the Remeda utilities into the global scope...");
+console.log("⏳ Loading the Remeda utilities into the global scope...");
 import("remeda").then((remeda) => {
   Object.assign(repl.context, remeda);
-  console.log("✅ Done!");
-  console.log("");
   console.log(
-    "You can now use Remeda utilities directly in the console, e.g.,",
+    "✅ Done! You can now use Remeda utilities directly in the console, e.g., `map([1, 2, 3], add(4));`",
   );
-  console.log("");
-  console.log("\tmap([1, 2, 3], add(4));");
   console.log("");
 
   repl.setPrompt("Remeda> ");
