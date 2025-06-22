d31af52cc34da4f7fcc8ca8aa850eacca1ecd5b4
Eran Hirsch <eranhirsch@gmail.com>
2025-06-22 21:00:29 +0300

    even more doc

--- packages/remeda/src/internal/types/EnumerableStringKeyedValueOf.ts
+++ packages/remeda/src/internal/types/EnumerableStringKeyedValueOf.ts
@@ -11,7 +11,10 @@ export type EnumerableStringKeyedValueOf<T> =
     ? IfNever<
         Exclude<keyof T, symbol>,
         // If the only keys in T are symbols then there are no enumerable values
-        // in the object.
+        // in the object. We rely on this `IfNever` construct because it turns
+        // out that Record<PropertyKey, "something">[never] results in
+        // `"something"` and not `never`?!
+        // (@see https://www.typescriptlang.org/play/?#code/C4TwDgpgBAGlC8UBKEDGB7ATgEwDwAVN1JNQBpCEAGigCIBndAWwmAAsBLAOwHNaA+ANpcIANwiYAugG4AUAHp5UZQD0A-LNmhIUAJoJkaLHlpsIAG3PpaNWgHcs57AOFiJMhUtVqgA)
         never,
         // Otherwise we take all possible values from all remaining keys. We use
         // Required to make sure we don't include `undefined` in the result if
