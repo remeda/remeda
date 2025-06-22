18793d6770f5fb4cd1ee2b97f518d21bcfd26602
Eran Hirsch <eranhirsch@gmail.com>
2025-06-22 16:05:18 +0300

    simplify code

--- packages/remeda/src/internal/types/EnumerableStringKeyedValueOf.ts
+++ packages/remeda/src/internal/types/EnumerableStringKeyedValueOf.ts
@@ -1,21 +1,13 @@
-import type { EmptyObject } from "type-fest";
+import type { IfNever } from "type-fest";
 
 /**
  * A union of all values of properties in T which are not keyed by a symbol,
  * following the definition of `Object.values` and `Object.entries`.
  */
-export type EnumerableStringKeyedValueOf<T> = ValuesOf<{
-  [K in keyof T]-?: K extends symbol ? never : T[K];
-}>;
-
-/**
- * Extracts the value type from an object type T.
- */
-type ValuesOf<T> =
-  // EmptyObject is used to infer value type as never instead of unknown
-  // for an empty object
-  T extends EmptyObject
-    ? T[keyof T]
-    : T extends Record<PropertyKey, infer V>
-      ? V
-      : never;
+export type EnumerableStringKeyedValueOf<T> = T extends unknown
+  ? IfNever<
+      Exclude<keyof T, symbol>,
+      never,
+      Required<T>[Exclude<keyof T, symbol>]
+    >
+  : never;
