db925c539c1467fb03673c098f27295b41ac9884
Eran Hirsch <eranhirsch@gmail.com>
2025-07-02 11:04:58 +0300

    woops

--- packages/remeda/src/internal/types/If.ts
+++ packages/remeda/src/internal/types/If.ts
@@ -1,6 +1,6 @@
 import type { IsNever } from "type-fest";
 
-// TODO [type-fest@>=5] -- This type is shipping in type-fest v5 and standardizes all IfXXX types. We backported it so that we can already use it because v5 is shipped.
+// TODO [type-fest@>=5] -- This type is shipping in type-fest v5 and standardizes all IfXXX types. We backported it so that we can already use it before v5 is shipped.
 export type If<Type extends boolean, IfBranch, ElseBranch> =
   IsNever<Type> extends true
     ? ElseBranch
