228a60d8761f141ecfba06c1d7e938bfb4e2fc2a
Eran Hirsch <eranhirsch@gmail.com>
2025-07-13 17:38:56 +0300

    add test to pathOr

---
+++ packages/remeda/src/pathOr.test-d.ts
@@ -0,0 +1,12 @@
+import { describe, expectTypeOf, test } from "vitest";
+import { stringToPath } from "./stringToPath";
+import { pathOr } from "./pathOr";
+
+// @see https://github.com/remeda/remeda/issues/779
+describe("examples from lodash migration (issue #779)", () => {
+  test("using stringToPath", () => {
+    expectTypeOf(
+      pathOr({ a: [{ b: 123 }] }, stringToPath("a[0].b"), 456),
+    ).toEqualTypeOf<number>();
+  });
+});
