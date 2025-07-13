ab6fc0a41f24e610c7f2f9fbdcb07af90d60c847
Eran Hirsch <eranhirsch@gmail.com>
2025-07-13 17:23:59 +0300

    docs

--- packages/remeda/src/stringToPath.ts
+++ packages/remeda/src/stringToPath.ts
@@ -15,23 +15,54 @@ type StringToPath<S> = If<
 >;
 
 type StringToPathImpl<S> =
+  // We start by checking the 2 quoted variants of the square bracket access
+  // syntax. We do this in a single check and not in a subsequent check that
+  // would only extract the quoted part so that we can catch cases where the
+  // quoted part itself contains square brackets. This allows TypeScript to be
+  // "greedy" about what it infers into Quoted and DoubleQuoted.
   S extends `${infer Head}['${infer Quoted}']${infer Tail}`
     ? [...StringToPath<Head>, Quoted, ...StringToPath<Tail>]
     : S extends `${infer Head}["${infer DoubleQuoted}"]${infer Tail}`
       ? [...StringToPath<Head>, DoubleQuoted, ...StringToPath<Tail>]
-      : S extends `${infer Head}[${infer Unquoted}]${infer Tail}`
+      : // If we have an unquoted property access we also need to run the
+        // contents recursively too (unlike the quoted variants above).
+        S extends `${infer Head}[${infer Unquoted}]${infer Tail}`
         ? [
             ...StringToPath<Head>,
             ...StringToPath<Unquoted>,
             ...StringToPath<Tail>,
           ]
-        : S extends `${infer Head}.${infer Tail}`
+        : // Finally, we process any dots one after the other from left to
+          // right. TypeScript will be non-greedy here, putting *everything*
+          // after the first dot into the Tail.
+          S extends `${infer Head}.${infer Tail}`
           ? [...StringToPath<Head>, ...StringToPath<Tail>]
-          : "" extends S
-            ? []
-            : S extends `${infer N extends number}`
-              ? [If<IsNumericLiteral<N>, N, S>]
-              : [S];
+          : // Finally we need to handle the few cases of simple literals.
+            "" extends S
+            ? // There are some edge-cases where Lodash will try to access an
+              // empty property, but those seem nonsensical in practice so we
+              // prefer just skipping these cases.
+              []
+            : // We differ from Lodash in the way we handle numbers. Lodash
+              // returns everything in the path as a string, and relies on JS to
+              // coerce array accessors to numbers (or the other way around in
+              // practice, e.g., `myArray[123] === `myArray[123]`), but from a
+              // typing perspective the two are not the same and we need the
+              // path to be accurate about it.
+              S extends `${infer N extends number}`
+              ? [
+                  If<
+                    // TypeScript considers " 123 " to still extend `${number}`,
+                    // but would type is as `string` instead of a literal. We
+                    // can use that fact to make sure we only consider simple
+                    // number literals as numbers, and take the rest as strings.
+                    IsNumericLiteral<N>,
+                    N,
+                    S
+                  >,
+                ]
+              : // This simplest form of a path is just a single string literal.
+                [S];
 
 /**
  * Converts a path string to an array of string keys (including array index
