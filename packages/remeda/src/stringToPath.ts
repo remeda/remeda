c32a2fb3ffbd5089ca63756a81e8a1b50125af9b
Eran Hirsch <eranhirsch@gmail.com>
2025-07-14 12:40:23 +0300

    docs + codeql + restructure

--- packages/remeda/src/stringToPath.ts
+++ packages/remeda/src/stringToPath.ts
@@ -1,8 +1,32 @@
 import type { IsNumericLiteral, IsStringLiteral } from "type-fest";
 import type { If } from "./internal/types/If";
 
+// In the stringToPath implementation we consume the path from left to right,
+// each time chopping off a candidate segment for addition to the path by
+// matching against **one of** the following named groups:
+// - `propName`: is used to parse dot-notation paths, e.g. foo.bar.baz, we allow
+// multiple sequential dots because our type allows them, but they are
+// semantically meaningless. Note that this would also allow strings starting
+// with a dot, e.g. `.foo`, this is fine because the dot itself is just used as
+// a separator and is ignored in the final path. Note that we limit the number
+// of dots to some arbitrary large number (100); this is to avoid a concern that
+// regular expression unbounded prefix matching can cause non-linear performance
+// issues.
+// - `quoted`, `doubleQuoted`: used within square bracket notation to prevent
+// any recursive parsing of their contents. As for the type itself, we only
+// allow quote symbols to appear immediately after the opening square bracket
+// and immediately before the closing square bracket, this allows us to handle
+// more gracefully cases where a quote symbol might appear within the quoted
+// part.
+// - `unquoted`: If the contents of the square brackets are not quoted they will
+// match this group (this is way the order is important here!). Contents of this
+// group get parsed recursively.
+//
+// Note that this Regular Expression is anchored to the start of the string, but
+// not to the end, it is assumed that there will be more contents after the
+// matched part; this content will be parsed recursively.
 const PATH_PARTS_RE =
-  /^(?:\.*(?<propName>[^.[\]]+)|\['(?<quoted>.*?)'\]|\["(?<doubleQuoted>.*?)"\]|\[(?<unquoted>.*?)\])(?<rest>.*)$/u;
+  /^(?:\.{0,100}(?<propName>[^.[\]]+)|\['(?<quoted>.*?)'\]|\["(?<doubleQuoted>.*?)"\]|\[(?<unquoted>.*?)\])/u;
 
 const NON_NEGATIVE_INTEGER_RE = /^[0-9]+$/u;
 
@@ -104,24 +128,33 @@ export function stringToPath<const Path extends string>(
   if (match?.groups === undefined) {
     return [path];
   }
+
   const {
-    groups: { propName, quoted, doubleQuoted, unquoted, rest },
+    groups: { propName, quoted, doubleQuoted, unquoted },
   } = match;
 
-  if (quoted !== undefined || doubleQuoted !== undefined) {
-    // @ts-expect-error [ts2322] -- This is OK, TypeScript can't infer this
-    // automatically.
-    return [quoted ?? doubleQuoted, ...stringToPath(rest!)];
-  }
-
-  if (unquoted !== undefined) {
-    return [...stringToPath(unquoted), ...stringToPath(rest!)];
-  }
-
-  // @ts-expect-error [ts2322] -- This is OK, TypeScript can't infer this
-  // automatically.
   return [
-    NON_NEGATIVE_INTEGER_RE.test(propName!) ? Number(propName) : propName,
-    ...stringToPath(rest!),
+    ...(unquoted === undefined
+      ? [
+          propName === undefined
+            ? // We either have quoted or doubleQuoted contents at this point,
+              // they are semantically equivalent and just need to be added to
+              // the path, as-is.
+              (quoted ?? doubleQuoted!)
+            : // simple property names are the only ones we need to further
+              // check and parse their content, in order to handle array index
+              // accessors correctly (by parsing them as numbers), but obviously
+              // we only do this if they are an non-negative integer.
+              NON_NEGATIVE_INTEGER_RE.test(propName)
+              ? Number(propName)
+              : propName,
+        ]
+      : // We have unquoted square bracket contents, we need to recursively
+        // parse it and add it to the path.
+        stringToPath(unquoted)),
+    // Our regular expression only handles a single segment at a time, we need
+    // to recursively parse the remainder of the path and append it to the
+    // result.
+    ...stringToPath(path.slice(match[0].length)),
   ];
 }
