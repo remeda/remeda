c306b10c1c0e814215610d8d92bae06b2c4f1db3
Eran Hirsch <eranhirsch@gmail.com>
2025-07-28 21:03:37 +0300

    docs

--- packages/remeda/src/defaultTo.ts
+++ packages/remeda/src/defaultTo.ts
@@ -14,15 +14,52 @@ type FallbackOf<T> = If<
       metadata: T;
     }
   >,
-  NonNullable<T>
+  T
 >;
 
-export function defaultTo<T, Fallback extends FallbackOf<T>>(
+/**
+ * A stricter wrapper around the [Nullish coalescing operator `??`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator)
+ * that ensures that the fallback matches the type of the data, and that the
+ * data is nullish (`null` or `undefined`).
+ *
+ * Notice that `Number.NaN` is not nullish and would not result in returning the
+ * fallback!
+ *
+ * @param data - A nullish value.
+ * @param fallback - A value of the same type as `data` that would be returned
+ * when `data` is nullish.
+ * @signature
+ *   R.defaultTo(data, fallback);
+ * @example
+ *   R.defaultTo("hello" as string | undefined, "world"); //=> "hello"
+ *   R.defaultTo(undefined as string | undefined, "world"); //=> "world"
+ * @dataFirst
+ * @category Other
+ */
+export function defaultTo<T, const Fallback extends FallbackOf<T>>(
   data: T,
   fallback: Fallback,
 ): NonNullable<T> | Fallback;
 
-export function defaultTo<T, Fallback extends FallbackOf<T>>(
+/**
+ * A stricter wrapper around the [Nullish coalescing operator `??`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator)
+ * that ensures that the fallback matches the type of the data, and that the
+ * data is nullish (`null` or `undefined`).
+ *
+ * Notice that `Number.NaN` is not nullish and would not result in returning the
+ * fallback!
+ *
+ * @param fallback - A value of the same type as `data` that would be returned
+ * when `data` is nullish.
+ * @signature
+ *   R.defaultTo(fallback)(data);
+ * @example
+ *   R.pipe("hello" as string | undefined, R.defaultTo("world")); //=> "hello"
+ *   R.pipe(undefined as string | undefined, R.defaultTo("world")); //=> "world"
+ * @dataLast
+ * @category Other
+ */
+export function defaultTo<T, const Fallback extends FallbackOf<T>>(
   fallback: Fallback,
 ): (data: T) => NonNullable<T> | Fallback;
 
