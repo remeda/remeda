4940c6f647cf148373d15cacd787255ca452d366
Eran Hirsch <eranhirsch@gmail.com>
2025-07-30 13:17:13 +0300

    revert partially

--- packages/remeda/src/isFunction.ts
+++ packages/remeda/src/isFunction.ts
@@ -1,13 +1,7 @@
 import type { NarrowedTo } from "./internal/types/NarrowedTo";
 
-// Contrary to intuition, because TypeScript compares function params using
-// contra-variance and not co-variance, in order to accept any function we need
-// the most "limiting" type for the argument list, and not the most permissive
-// one (which would be `readonly unknown[]`); now when any function is compared
-// against this signature it will always match because `never` **everything**
-// extends everything!
-// @see https://www.typescriptlang.org/play/?#code/C4TwDgpgBA6glsAFgJQgQwCYHsB2AbEAVRwGscsB3HAHgBUA+KAXilqggA9gIcMBnKAAoAdKLQAnAOZ8AXFHHps+EFACupclQDaAXQCUzRurKUcAKChQA-FGDjVEC1DkAzNHj4QA3GbOhIsAiIAII4IHSMLGyc3LwCImJSslBoYQZMRhqm1rb20K7unj5+4NDwSAByEABuEOIRzKzsXDz8QqLCEtJyODV16ZkmVDl2Ds5Qbh7evv7QtBB8wABi6gDGjYJoPaoAtgBG-YZQi+JwOJLFs1AAGgAMjeUoirgExEM084srOKv0PgD0-0sUAAelYZqUbgBGB5BULhT7LNZ-MyA4FgiEBa4AJlhlT69UR31+AKBljBQA
-type StrictFunction = (...args: never) => unknown;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Contrary to intuition, because TypeScript compares function params using contra-variance and not co-variance, in order to accept all functions we **can't** use `readonly unknown[]` because it will only accept functions with an actual variadic param. On the other hand we can't use `never` either (although `never` extends **everything** and thus would catch all cases, @see https://www.typescriptlang.org/play/?#code/C4TwDgpgBA6glsAFgJQgQwCYHsB2AbEAVRwGscsB3HAHgBUA+KAXilqggA9gIcMBnKAAoAdKLQAnAOZ8AXFHHps+EFACupclQDaAXQCUzRurKUcAKChQA-FGDjVEC1DkAzNHj4QA3GbOhIsAiIAII4IHSMLGyc3LwCImJSslBoYQZMRhqm1rb20K7unj5+4NDwSAByEABuEOIRzKzsXDz8QqLCEtJyODV16ZkmVDl2Ds5Qbh7evv7QtBB8wABi6gDGjYJoPaoAtgBG-YZQi+JwOJLFs1AAGgAMjeUoirgExEM084srOKv0PgD0-0sUAAelYZqUbgBGB5BULhT7LNZ-MyA4FgiEBa4AJlhlT69UR31+AKBljBQA) because it breaks any function that handles functions generically and that use `Parameters<F>`, because it would be converted to `never` *too early*, losing it's relation to F (similar to this issue: https://github.com/microsoft/TypeScript/issues/61750).
+type StrictFunction = (...args: any) => unknown;
 
 /**
  * A function that checks if the passed parameter is a Function and narrows its type accordingly.
