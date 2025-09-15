34be062858d44207ce1b29cf73a79a87eb267217
Eran Hirsch <eranhirsch@gmail.com>
2025-09-15 17:27:14 +0300

    even better

--- packages/remeda/src/capitalize.ts
+++ packages/remeda/src/capitalize.ts
@@ -3,12 +3,12 @@ import { purry } from "./purry";
 /**
  * Makes the first character of a string uppercase while leaving the rest
  * unchanged. It uses the built-in [`String.prototype.toUpperCase`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
- * for the runtime, and the built-in [`Capitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#capitalizestringtype)
- * utility type for typing.
+ * for the runtime and the built-in [`Capitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#capitalizestringtype)
+ * utility type for typing and thus shares their [limitations](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLocaleUpperCase#description).
  *
  * For display purposes, prefer using the CSS pseudo-element [`::first-letter`](https://developer.mozilla.org/en-US/docs/Web/CSS/::first-letter) to target
  * just the first letter of the word, and [`text-transform: uppercase`](https://developer.mozilla.org/en-US/docs/Web/CSS/text-transform#uppercase)
- * to capitalize it.
+ * to capitalize it. This transformation **is** locale-aware.
  *
  * For other case manipulations see: `toUpperCase`, `toLowerCase`,
  * `uncapitalize`, `toCamelCase`, `toKebabCase`, and `toSnakeCase`.
@@ -26,12 +26,12 @@ export function capitalize<T extends string>(data: T): Capitalize<T>;
 /**
  * Makes the first character of a string uppercase while leaving the rest
  * unchanged. It uses the built-in [`String.prototype.toUpperCase`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
- * for the runtime, and the built-in [`Capitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#capitalizestringtype)
- * utility type for typing.
+ * for the runtime and the built-in [`Capitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#capitalizestringtype)
+ * utility type for typing and thus shares their [limitations](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLocaleUpperCase#description).
  *
  * For display purposes, prefer using the CSS pseudo-element [`::first-letter`](https://developer.mozilla.org/en-US/docs/Web/CSS/::first-letter) to target
  * just the first letter of the word, and [`text-transform: uppercase`](https://developer.mozilla.org/en-US/docs/Web/CSS/text-transform#uppercase)
- * to capitalize it.
+ * to capitalize it. This transformation **is** locale-aware.
  *
  * For other case manipulations see: `toUpperCase`, `toLowerCase`,
  * `uncapitalize`, `toCamelCase`, `toKebabCase`, and `toSnakeCase`.
--- packages/remeda/src/uncapitalize.ts
+++ packages/remeda/src/uncapitalize.ts
@@ -3,12 +3,12 @@ import { purry } from "./purry";
 /**
  * Makes the first character of a string lowercase while leaving the rest
  * unchanged. It uses the built-in [`String.prototype.toLowerCase`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase)
- * for the runtime, and the built-in [`Uncapitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#Uncapitalizestringtype)
- * utility type for typing.
+ * for the runtime and the built-in [`Uncapitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#Uncapitalizestringtype)
+ * utility type for typing and thus shares their [limitations](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLocaleLowerCase#description).
  *
  * For display purposes, prefer using the CSS pseudo-element [`::first-letter`](https://developer.mozilla.org/en-US/docs/Web/CSS/::first-letter) to target
  * just the first letter of the word, and [`text-transform: lowercase`](https://developer.mozilla.org/en-US/docs/Web/CSS/text-transform#lowercase)
- * to lowercase it.
+ * to lowercase it. This transformation **is** locale-aware.
  *
  * For other case manipulations see: `toUpperCase`, `toLowerCase`, `capitalize`,
  * `toCamelCase`, `toKebabCase`, and `toSnakeCase`.
@@ -26,12 +26,12 @@ export function uncapitalize<T extends string>(data: T): Uncapitalize<T>;
 /**
  * Makes the first character of a string lowercase while leaving the rest
  * unchanged. It uses the built-in [`String.prototype.toLowerCase`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase)
- * for the runtime, and the built-in [`Uncapitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#Uncapitalizestringtype)
- * utility type for typing.
+ * for the runtime and the built-in [`Uncapitalize`](https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html#Uncapitalizestringtype)
+ * utility type for typing and thus shares their [limitations](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLocaleLowerCase#description).
  *
  * For display purposes, prefer using the CSS pseudo-element [`::first-letter`](https://developer.mozilla.org/en-US/docs/Web/CSS/::first-letter) to target
  * just the first letter of the word, and [`text-transform: lowercase`](https://developer.mozilla.org/en-US/docs/Web/CSS/text-transform#lowercase)
- * to lowercase it.
+ * to lowercase it. This transformation **is** locale-aware.
  *
  * For other case manipulations see: `toUpperCase`, `toLowerCase`, `capitalize`,
  * `toCamelCase`, `toKebabCase`, and `toSnakeCase`.
