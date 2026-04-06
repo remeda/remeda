58e57f05ba763e69a2bafecd435eff785604c735
Eran Hirsch <eranhirsch@gmail.com>
2026-04-06 16:53:43 +0300

    chore(release): flip `feat` and `fix` conventional label semantics (#1315)
    It doesn't make sense for us to release a minor version every time we
    add a utility or functionality, as those don't impact existing users at
    all. Instead, fixes to existing utilities are! This is why we decided to
    flip the two semantically.

--- packages/remeda/release.config.js
+++ packages/remeda/release.config.js
@@ -2,7 +2,19 @@
 export default {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          // Flipped from the default config! New features don't pose a risk to
+          // existing code and thus shouldn't prevent users from upgrading, but
+          // fixes to existing utilities can impact how they work and might
+          // cause compile or runtime errors for users.
+          { type: "feat", release: "patch" },
+          { type: "fix", release: "minor" },
+        ],
+      },
+    ],
 
     "@semantic-release/release-notes-generator",
 
