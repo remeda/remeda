5e75bd440119b294249138e40c429c24779505ee
Eran Hirsch <eranhirsch@gmail.com>
2026-05-05 22:53:50 +0300

    pedantic

--- packages/docs/src/components/h3-card-title.tsx
+++ packages/docs/src/components/h3-card-title.tsx
@@ -8,7 +8,7 @@ export function H3CardTitle({
   return (
     // We can't inline this within an astro component because in order for the
     // component to be able to pass props on to the raw html element via the
-    // `asChild` prop it needs to happen within the context of a React island.
+    // `asChild` prop it needs to happen within the context of React rendering.
     <CardTitle asChild {...props}>
       <h3>{children}</h3>
     </CardTitle>
--- packages/docs/src/components/header-card-header.tsx
+++ packages/docs/src/components/header-card-header.tsx
@@ -8,7 +8,7 @@ export function HeaderCardHeader({
   return (
     // We can't inline this within an astro component because in order for the
     // component to be able to pass props on to the raw html element via the
-    // `asChild` prop it needs to happen within the context of a React island.
+    // `asChild` prop it needs to happen within the context of React rendering.
     <CardHeader asChild {...props}>
       <header>{children}</header>
     </CardHeader>
