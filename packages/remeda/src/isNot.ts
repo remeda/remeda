02565d4442aea99e1b999cec34ac25e224d92de3
Eran Hirsch <eranhirsch@gmail.com>
2026-08-24 22:52:09 +0300

    green

--- packages/remeda/src/isNot.ts
+++ packages/remeda/src/isNot.ts
@@ -1,3 +1,5 @@
+type Predicate<T> = (data: T) => boolean;
+
 /**
  * A function that takes a guard function as predicate and returns a guard that negates it.
  *
@@ -11,11 +13,12 @@
  * @dataLast
  * @category Guard
  */
-export function isNot<T, S extends T>(
-  predicate: (data: T) => data is S,
-): (data: T) => data is Exclude<T, S>;
-export function isNot<T>(predicate: (data: T) => boolean): (data: T) => boolean;
+export function isNot<T, Narrow extends T>(
+  predicate: (data: T) => data is Narrow,
+): <Wide extends T>(data: Wide) => data is Exclude<Wide, Narrow>;
 
-export function isNot<T>(predicate: (data: T) => boolean) {
-  return (data: T): boolean => !predicate(data);
+export function isNot<T>(predicate: Predicate<T>): Predicate<T>;
+
+export function isNot<T>(predicate: Predicate<T>): Predicate<T> {
+  return (data) => !predicate(data);
 }
