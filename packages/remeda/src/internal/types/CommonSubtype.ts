5f4ceebd9e550fa61daffa21b6b3ea316ca85e3a
Eran Hirsch <eranhirsch@gmail.com>
2026-08-26 13:27:24 +0300

    handle any

--- packages/remeda/src/internal/types/CommonSubtype.ts
+++ packages/remeda/src/internal/types/CommonSubtype.ts
@@ -1,16 +1,21 @@
-import type { IsNever } from "type-fest";
+import type { IsAny, IsNever } from "type-fest";
 
 /**
  * Find the common subtype which T0 and T1 both extend. Similar to the built-in
  * `Extract` utility, but allows either T0 or T1 to be wider than the other.
  */
-export type CommonSubtype<T0, T1> = T0 extends T1
-  ? T0
-  : T1 extends T0
+export type CommonSubtype<T0, T1> =
+  IsAny<T0> extends true
     ? T1
-    : // Our types are incomparable (neither one extends the other) but they
-      // might still share some common props.
-      NonEmptyIntersection<T0, T1>;
+    : IsAny<T1> extends true
+      ? T0
+      : T0 extends T1
+        ? T0
+        : T1 extends T0
+          ? T1
+          : // Our types are incomparable (neither one extends the other) but they
+            // might still share some common props.
+            NonEmptyIntersection<T0, T1>;
 
 type NonEmptyIntersection<T0, T1> =
   IsNonArrayObject<T0> extends true
