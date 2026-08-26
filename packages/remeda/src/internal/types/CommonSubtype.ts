90519b842d57bf0f00e79bb964ed908ebd621d2b
Eran Hirsch <eranhirsch@gmail.com>
2026-08-26 16:38:09 +0300

    better docs

--- packages/remeda/src/internal/types/CommonSubtype.ts
+++ packages/remeda/src/internal/types/CommonSubtype.ts
@@ -1,21 +1,26 @@
 import type { IsAny, IsNever } from "type-fest";
 
 /**
- * Find the common subtype which T0 and T1 both extend. Similar to the built-in
- * `Extract` utility, but allows either T0 or T1 to be wider than the other.
+ * Find the widest subtype which extends both T0 and T1 (
+ * `CommonSubtype<T0, T1> extends T0` and `CommonSubtype<T0, T1> extends T1`);
+ * similar to the built-in `Extract`, but allows either T0 or T1 to be wider \
+ * than the other.
  */
 export type CommonSubtype<T0, T1> =
   IsAny<T0> extends true
-    ? T1
-    : IsAny<T1> extends true
+    ? // `any` extends and doesn't extend any type at the same time, resulting
+      // in a union of both branches, which is not semantically correct for
+      // this type. The opposite case where T1 is `any` would be handled
+      // correctly implicitly when we check if T0 extends T1 because all types
+      // extends `any`.
+      T1
+    : T0 extends T1
       ? T0
-      : T0 extends T1
-        ? T0
-        : T1 extends T0
-          ? T1
-          : // Our types are incomparable (neither one extends the other) but they
-            // might still share some common props.
-            NonEmptyIntersection<T0, T1>;
+      : T1 extends T0
+        ? T1
+        : // Our types are incomparable (neither one extends the other) but they
+          // might still share some common props.
+          NonEmptyIntersection<T0, T1>;
 
 type NonEmptyIntersection<T0, T1> =
   IsNonArrayObject<T0> extends true
